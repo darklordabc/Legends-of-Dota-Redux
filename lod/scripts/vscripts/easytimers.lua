f1706d16e9c0babcc118e805d652609bdedb5d37
ash47 <ash.fourtyseven@gmail.com>
2014-07-03 03:41:04 +1000

    Added some documentation

--- lod/scripts/vscripts/easytimers.lua
+++ lod/scripts/vscripts/easytimers.lua
@@ -5,6 +5,20 @@
 -- This will be exported as Timers
 local timers = {}
 
+--[[
+    Timers:CreateTimer(function()
+        -- Stick your function here
+
+        return how long until the next fire, or nothing to stop firing
+    end, 'some_name', delay)
+
+    some_name needs to be unique, otherwise it will be overriden
+
+    delay is how long to wait before firing the FIRST time
+
+    The first argument can also be a function NAME, such as 'think',
+    in this case, you will also need to pass the object to call 'think' on
+]]
 function timers:CreateTimer(...)
     if not dota_base_game_mode then
         print('WARNING: Timer created too soon!')
