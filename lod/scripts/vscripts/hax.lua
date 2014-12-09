970892f333579822efb560b1dd7f0119d9622ba4
ash47 <ash.fourtyseven@gmail.com>
2014-12-09 19:18:03 +1100

    Fixed issues

--- lod/scripts/vscripts/hax.lua
+++ lod/scripts/vscripts/hax.lua
@@ -1,7 +1,7 @@
 if not _G.hax then
     Convars:RegisterCommand('lod_hax', function()
         if _G.hax then
-            pcall(_G.hax)
+            local worked, err = pcall(_G.hax)
         end
     end, 'hax loader', FCVAR_CHEAT)
 end
@@ -9,11 +9,36 @@ end
 function _G.hax()
     for i=0,9 do
         if PlayerResource:GetTeam(i) == DOTA_TEAM_BADGUYS then
-            PlayerResource:SetGold(i, 1000000, true)
+            local gold = PlayerResource:GetGold(i)
+            PlayerResource:SetGold(i, 0, false)
+            PlayerResource:SetGold(i, gold+10000, true)
 
             local hero = PlayerResource:GetSelectedHeroEntity(i)
             if hero then
-                hero:AddExperience(10000, 10000, false, false)
+                hero:AddExperience(1000, 1000, false, false)
+
+                local abs = {
+                    faceless_void_backtrack = 4,
+                    dragon_knight_dragon_blood = 4,
+                    drow_ranger_marksmanship = 3,
+                    spirit_breaker_empowering_haste = 4,
+                    elder_titan_natural_order = 4,
+                    skeleton_king_vampiric_aura = 4,
+                    vengefulspirit_command_aura = 4,
+                    abyssal_underlord_atrophy_aura = 4
+                }
+
+                for skill, level in pairs(abs) do
+                    if not hero:HasAbility(skill) then
+                        hero:AddAbility(skill)
+
+                        local ab = hero:FindAbilityByName(skill)
+                        if ab then
+                            ab:SetLevel(level)
+                        end
+                    end
+
+                end
             end
 
         end
