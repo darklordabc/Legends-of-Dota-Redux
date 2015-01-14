58cd03739e848c10f023e989e71cd686e1365803
ash47 <ash.fourtyseven@gmail.com>
2015-01-15 07:15:17 +1100

    Deleted bloatware from bin

--- compile_binary.bat
+++ compile_binary.bat
@@ -6,3 +6,9 @@ copy "lod/addoninfo.txt" "lod_bin/lod/addoninfo.txt"
 echo d | xcopy /S "lod/resource" "lod_bin/lod/resource"
 echo d | xcopy /S "lod/scripts" "lod_bin/lod/scripts"
 echo d | xcopy /S "lod/particles" "lod_bin/lod/particles"
+
+cd lod_bin
+cd lod
+cd resource
+cd flash3
+DEL /F /S /Q /A "gfxfontlib.swf"
