6be4ee9825f9bb0d2ad3ca080562df97eee70c15
ash47 <ash.fourtyseven@gmail.com>
2015-01-15 07:44:13 +1100

    Recompiled

--- compile_binary.bat
+++ compile_binary.bat
@@ -2,13 +2,13 @@ rmdir /S /Q "lod_bin"
 mkdir "lod_bin"
 mkdir "lod_bin/lod"
 copy "manifest.kv" "lod_bin/manifest.kv"
-copy "lod/addoninfo.txt" "lod_bin/lod/addoninfo.txt"
 echo d | xcopy /S "lod/resource" "lod_bin/lod/resource"
 echo d | xcopy /S "lod/scripts" "lod_bin/lod/scripts"
 echo d | xcopy /S "lod/particles" "lod_bin/lod/particles"
 
-cd lod_bin
 cd lod
-cd resource
-cd flash3
+copy "addoninfo.txt" "../lod_bin/lod/addoninfo.txt"
+cd ..
+
+cd lod_bin/lod/resource/flash3
 DEL /F /S /Q /A "gfxfontlib.swf"
