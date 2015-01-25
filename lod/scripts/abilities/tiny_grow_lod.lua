2f4f4fad66bc14043f0e62a42a6dc67bb41e0731
ash47 <ash.fourtyseven@gmail.com>
2015-01-26 04:22:04 +1100

    Remove debug

--- lod/scripts/abilities/tiny_grow_lod.lua
+++ lod/scripts/abilities/tiny_grow_lod.lua
@@ -3,12 +3,8 @@ function onTinyGrowUpgrade(keys)
     local hero = keys.caster
     if not hero then return end
 
-    print('inside')
-
     -- Ensure it is leveled
     if keys.ability:GetLevel() <= 0 then return end
-
-    print('Leveled!')
 
     -- Remove old modifiers
     hero:RemoveModifierByName('tiny_grow_lod')
@@ -18,11 +14,9 @@ function onTinyGrowUpgrade(keys)
     if hero:HasScepter() then
         -- Give scepter version
         keys.ability:OnChannelFinish(true)
-        print('scepter')
     else
         -- Give normal version
         keys.ability:OnSpellStart()
-        print('regular')
     end
 
     -- Set the model
@@ -36,7 +30,6 @@ ListenToGameEvent('dota_item_purchased', function(keys)
         if hero:HasAbility('tiny_grow_lod') and hero:HasScepter() then
             local ab = hero:FindAbilityByName('tiny_grow_lod')
             if ab then
-                print('running upgrade')
                 ab:OnUpgrade()
             end
         end
