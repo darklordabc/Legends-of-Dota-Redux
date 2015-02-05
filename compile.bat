aa28064053e0348d2031a1a6ce161666f50d8c9c
ash47 <ash.fourtyseven@gmail.com>
2015-02-05 22:25:08 +1100

    Moved maps to seperate folder

--- compile.bat
+++ compile.bat
@@ -21,10 +21,10 @@ echo d | xcopy /S "lod/scripts" "lod_s2_bin/scripts"
 
 cd lod
 copy "addoninfo.txt" "../lod_s2_bin/addoninfo.txt"
-cd maps
-copy "dota_pvp.vpk" "../../lod_s2_bin/maps/dota_pvp.vpk"
+cd ../maps
+copy "dota_pvp.vpk" "../lod_s2_bin/maps/dota_pvp.vpk"
 
-cd ../../lod_s2_bin/resource/flash3
+cd ../lod_s2_bin/resource/flash3
 del gfxfontlib.swf
 
 cd ../../..
