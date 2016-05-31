99c9e4b67b32ce2b2cb0eec808f645a9523ab505
Myrl Hex <Myrl.0xf@gmail.com>
2016-06-01 02:06:54 +0800

    Added tinker rearm script

---
+++ src/scripts/abilities/tinker_rearm_lod.lua
@@ -0,0 +1,46 @@
+--[[
+	Author: kritth
+	Date: 7.1.2015.
+	Put modifier to override animation on cast
+]]
+function rearm_start( keys )
+	local caster = keys.caster
+	local ability = keys.ability
+	local abilityLevel = ability:GetLevel()
+	ability:ApplyDataDrivenModifier( caster, caster, "modifier_rearm_level_" .. abilityLevel .. "_datadriven", {} )
+end
+--[[
+	Author: kritth
+	Date: 7.1.2015.
+	Refresh cooldown
+]]
+function rearm_refresh_cooldown( keys )
+	local caster = keys.caster
+	
+	-- Reset cooldown for abilities that is not rearm
+	for i = 0, caster:GetAbilityCount() - 1 do
+		local ability = caster:GetAbilityByIndex( i )
+		if ability and ability ~= keys.ability then
+			ability:EndCooldown()
+		end
+	end
+	
+	-- Put item exemption in here
+	local exempt_table = { 
+        item_recipe_black_king_bar = true,
+        item_recipe_arcane_boots = true,
+        item_helm_of_the_dominator = true,
+        item_refresher = true,
+        item_sphere = true,
+        item_bottle = true,
+        item_recipe_necronomicon = true
+    }
+	
+	-- Reset cooldown for items
+	for i = 0, 5 do
+		local item = caster:GetItemInSlot( i )
+		if item and not exempt_table[ item:GetAbilityName() ] then
+			item:EndCooldown()
+		end
+	end
+end
