df7938f9ed37e08945db73a8094c08cb34fe865f
Wouterz90 <wouter.zegers@gmail.com>
2016-06-11 16:52:48 +0200

    Create flesh_heap_int.lua

---
+++ src/scripts/vscripts/lua_spells/flesh_heap_int.lua
@@ -0,0 +1,49 @@
+flesh_heap_int = class({})
+LinkLuaModifier( "modifier_flesh_heap_int", "lua_spells/modifiers/modifier_flesh_heap_int.lua" ,LUA_MODIFIER_MOTION_NONE )
+
+
+function flesh_heap_int:GetIntrinsicModifierName()
+	return "modifier_flesh_heap_int"
+end
+
+--------------------------------------------------------------------------------
+
+function flesh_heap_int:OnHeroDiedNearby( hVictim, hKiller, kv )
+	if hVictim == nil or hKiller == nil then
+		return
+	end
+
+	if hVictim:GetTeamNumber() ~= self:GetCaster():GetTeamNumber() and self:GetCaster():IsAlive() then
+		self.flesh_heap_range = self:GetSpecialValueFor( "flesh_heap_range" )
+		local vToCaster = self:GetCaster():GetOrigin() - hVictim:GetOrigin()
+		local flDistance = vToCaster:Length2D()
+		if hKiller == self:GetCaster() or self.flesh_heap_range >= flDistance then
+			if self.nKills == nil then
+				self.nKills = 0
+			end
+
+			self.nKills = self.nKills + 1
+
+			local hBuff = self:GetCaster():FindModifierByName( "modifier_flesh_heap_int" )
+			if hBuff ~= nil then
+				hBuff:SetStackCount( self.nKills )
+				self:GetCaster():CalculateStatBonus()
+			end
+
+			local nFXIndex = ParticleManager:CreateParticle( "particles/units/heroes/hero_pudge/pudge_fleshheap_count.vpcf", PATTACH_OVERHEAD_FOLLOW, self:GetCaster() )
+			ParticleManager:SetParticleControl( nFXIndex, 1, Vector( 1, 0, 0 ) )
+			ParticleManager:ReleaseParticleIndex( nFXIndex )
+		end
+	end
+end
+
+--------------------------------------------------------------------------------
+
+function flesh_heap_int:GetFleshHeapKills()
+	if self.nKills == nil then
+		self.nKills = 0
+	end
+	return self.nKills
+end
+ 
+--------------------------------------------------------------------------------
