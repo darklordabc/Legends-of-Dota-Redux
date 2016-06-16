1f4cc98e4d0a343ee820c0f3e9b203f054f8eb65
Wouterz90 <wouter.zegers@gmail.com>
2016-06-16 20:04:20 +0200

    Update pudge_flesh_heap_agi.lua

--- src/scripts/abilities/pudge_flesh_heap_agi.lua
+++ src/scripts/abilities/pudge_flesh_heap_agi.lua
@@ -1,6 +1,6 @@
 flesh_heap_agi = class({})
 
-LinkLuaModifier( "modifier_flesh_heap_agi", "lua_spells/modifiers/modifier_flesh_heap_agi.lua" ,LUA_MODIFIER_MOTION_NONE )
+LinkLuaModifier( "modifier_flesh_heap_agi", "scripts/vscripts/../abilities/modifiers/modifier_flesh_heap_agi.lua" ,LUA_MODIFIER_MOTION_NONE )
 
 
 function flesh_heap_agi:GetIntrinsicModifierName()
