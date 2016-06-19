0196bba6b7d80254e82ccb79ac0c352e15e61d25
Wouterz90 <wouter.zegers@gmail.com>
2016-06-19 15:18:53 +0200

    Fixing the script issues

--- src/scripts/abilities/pudge_flesh_heap_int.lua
+++ src/scripts/abilities/pudge_flesh_heap_int.lua
@@ -1,4 +1,4 @@
-flesh_heap_int = class({})
+pudge_flesh_heap_int = class({})
 LinkLuaModifier( "modifier_flesh_heap_int", "scripts/vscripts/../abilities/modifiers/modifier_flesh_heap_agi.lua" ,LUA_MODIFIER_MOTION_NONE )
 
 
