0eecea86cc2b27b9bb16ff893b578c1a5703e86c
Yahnich <y.slagmeulder@hotmail.com>
2016-06-28 03:24:46 +0200

    Add files via upload

---
+++ src/scripts/abilities/custom_cast_range.lua
@@ -0,0 +1,9 @@
+function SetCastRange(keys)
+	local caster = keys.caster
+	if keys.ability:GetLevel() <= 0 then return end
+	-- Remove old cast range
+	caster:RemoveModifierByName("modifier_item_aether_lens")
+	-- Replace cast range
+	caster:AddNewModifier(caster,ability,"modifier_item_aether_lens",{})
+	-- Incompatible with aether lens for now for obvious reasons, fix this if desired once proper cast range support exists
+end
