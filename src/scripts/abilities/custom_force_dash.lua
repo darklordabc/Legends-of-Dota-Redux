16098de73f4c0ade196729f5ae8aab28812c343c
Yahnich <y.slagmeulder@hotmail.com>
2016-06-29 11:01:04 +0200

    Add files via upload

--- src/scripts/abilities/custom_force_dash.lua
+++ src/scripts/abilities/custom_force_dash.lua
@@ -1,11 +1,12 @@
 function ForceDash(keys)
 	local caster = keys.caster
+	local target = keys.target
 
 	local ability = keys.ability
 	local modifier = keys.modifier
 	-- Distance calculations
 	local distance = ability:GetLevelSpecialValueFor("distance", (ability:GetLevel() - 1))
-	local direction = caster:GetForwardVector()
+	local direction = target:GetForwardVector()
 	local duration = ability:GetLevelSpecialValueFor("duration", (ability:GetLevel() - 1))
 	local speed = distance/duration
 
@@ -15,19 +16,19 @@ function ForceDash(keys)
 	ability.direction = direction
 	ability.traveled_distance = 0
 	
-	ability:ApplyDataDrivenModifier(caster, caster, modifier, {duration = duration})
+	ability:ApplyDataDrivenModifier(caster, target, modifier, {duration = duration})
 end
 
 function ForceMotion( keys )
-	local caster = keys.target
+	local target = keys.target
 	local ability = keys.ability
-	-- Move the caster while the distance traveled is less than the original distance upon cast
+	-- Move the target while the distance traveled is less than the original distance upon cast
 	if ability.traveled_distance < ability.distance then
-		caster:SetAbsOrigin(caster:GetAbsOrigin() + ability.direction * ability.speed)
+		target:SetAbsOrigin(target:GetAbsOrigin() + ability.direction * ability.speed)
 		ability.traveled_distance = ability.traveled_distance + ability.speed
 	else
 		-- Remove the motion controller once the distance has been traveled
-		caster:InterruptMotionControllers(false)
-		caster:RemoveModifierByName("modifier_force_dash")
+		target:InterruptMotionControllers(false)
+		target:RemoveModifierByName("modifier_force_dash")
 	end
 end
