3445cb0c72605ca650abd9a5ecbed2ece8c65812
Yahnich <y.slagmeulder@hotmail.com>
2016-07-09 13:46:33 +0200

    Update changelog.js

--- src/content/panorama/scripts/custom_game/changelog.js
+++ src/content/panorama/scripts/custom_game/changelog.js
@@ -1,34 +1,41 @@
 "use strict";
 
 function toggleChangelog(arg){
-	$("#changelogDisplay").visible = !($("#changelogDisplay").visible)
-	
+	$("#changelogDisplay").visible = !($("#changelogDisplay").visible)	
 	$("#descriptionDisplay").visible = true
-	$("#updateDisplay").visible = false
+	$("#updateDisplay").visible = false	
 	$("#creditsDisplay").visible = false
+	$("#OptionTextDescription").AddClass("displaySelected");
+	$("#OptionTextUpdates").RemoveClass("displaySelected");
+	$("#OptionTextCredits").RemoveClass("displaySelected");
 }
 
 function toggleDescription(arg){
 	$("#descriptionDisplay").visible = true
 	$("#updateDisplay").visible = false
 	$("#creditsDisplay").visible = false
+	$("#OptionTextDescription").AddClass("displaySelected");
+	$("#OptionTextUpdates").RemoveClass("displaySelected");
+	$("#OptionTextCredits").RemoveClass("displaySelected");
 }
 
 function toggleUpdates(arg){
 	$("#descriptionDisplay").visible = false
-
 	$("#updateDisplay").visible = true
-
 	$("#creditsDisplay").visible = false
+	$("#OptionTextDescription").RemoveClass("displaySelected");
+	$("#OptionTextUpdates").AddClass("displaySelected");
+	$("#OptionTextCredits").RemoveClass("displaySelected");
 
 }
 
 function toggleCredits(arg){
 	$("#descriptionDisplay").visible = false
-
 	$("#updateDisplay").visible = false
-
 	$("#creditsDisplay").visible = true
+	$("#OptionTextDescription").RemoveClass("displaySelected");
+	$("#OptionTextUpdates").RemoveClass("displaySelected");
+	$("#OptionTextCredits").AddClass("displaySelected");
 
 }
 
