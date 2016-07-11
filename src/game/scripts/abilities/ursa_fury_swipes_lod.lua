625c62e2a22a814c4ae2d7b8e04059ddcb139814
Yahnich <y.slagmeulder@hotmail.com>
2016-07-11 20:39:09 +0200

    Update ursa_fury_swipes_lod.lua

--- src/game/scripts/abilities/ursa_fury_swipes_lod.lua
+++ src/game/scripts/abilities/ursa_fury_swipes_lod.lua
@@ -18,41 +18,34 @@ function fury_swipes_attack( keys )
 	
 	-- Necessary value from KV
 	local duration = ability:GetLevelSpecialValueFor( "bonus_reset_time", ability:GetLevel() - 1 )
-	local damage_per_stack = ability:GetLevelSpecialValueFor( "damage_per_stack", ability:GetLevel() - 1 ) /(1+caster:GetIntellect()/1600)
+	local damage_per_stack = ability:GetLevelSpecialValueFor( "damage_per_stack", ability:GetLevel() - 1 )
 	-- Modifies damage bonus if ranged attacker
 	if caster:IsRangedAttacker() then damage_per_stack = ability:GetLevelSpecialValueFor( "damage_per_stack_ranged", ability:GetLevel() - 1 ) end
-
+	-- Filters out int scaling
+	damage_per_stack = damage_per_stack/(1+caster:GetIntellect()/1600)
 
 	if target:GetName() == exceptionName then	-- Put exception here
 		duration = ability:GetLevelSpecialValueFor( "bonus_reset_time_roshan", ability:GetLevel() - 1 )
 	end
 	
-	-- Check if unit already have stack
-	if target:HasModifier( modifierName ) then
-		local current_stack = target:GetModifierStackCount( modifierName, ability )
+	-- Check for current stacks
+	local current_stack = target:GetModifierStackCount( modifierName, ability )
+	if current_stack == nil then current_stack = 0 end
 		
-		-- Deal damage
-		local damage_table = {
-			victim = target,
-			attacker = caster,
-			damage = damage_per_stack * current_stack,
-			damage_type = damageType
+	-- Reset duration
+	ability:RemoveModifierByName(modifierName)
+	ability:ApplyDataDrivenModifier( caster, target, modifierName, { Duration = duration } )
+	
+	-- Update current stacks
+	current_stack = current_stack + 1
+	target:SetModifierStackCount( modifierName, ability, current_stack)
+		
+	-- Deal damage
+	local damage_table = {
+		victim = target,
+		attacker = caster,
+		damage = damage_per_stack * current_stack,
+		damage_type = damageType
 		}
-		ApplyDamage( damage_table )
-		
-		ability:ApplyDataDrivenModifier( caster, target, modifierName, { Duration = duration } )
-		target:SetModifierStackCount( modifierName, ability, current_stack + 1 )
-	else
-		ability:ApplyDataDrivenModifier( caster, target, modifierName, { Duration = duration } )
-		target:SetModifierStackCount( modifierName, ability, 1 )
-		
-		-- Deal damage
-		local damage_table = {
-			victim = target,
-			attacker = caster,
-			damage = damage_per_stack,
-			damage_type = damageType
-		}
-		ApplyDamage( damage_table )
-	end
+	ApplyDamage( damage_table )
 end
