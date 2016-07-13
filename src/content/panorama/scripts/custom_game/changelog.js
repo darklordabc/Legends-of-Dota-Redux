c263757150dff7080576a19c2dc39bc0cb0e77e4
TideSofDarK <tidesmain@gmail.com>
2016-07-13 22:56:41 +0500

    Dev messages/notifications

--- src/content/panorama/scripts/custom_game/changelog.js
+++ src/content/panorama/scripts/custom_game/changelog.js
@@ -1,45 +1,61 @@
 "use strict";
 
+// TODO: Back-end
+var messages = [];
+messages[0] = {"steamid" : 76561198054179075, "message" : "Testing messages"}
+messages[1] = {"steamid" : 76561198054179075, "message" : "Testing messages x2"}
+messages[2] = {"steamid" : 76561198001376044, "message" : "Testing messages x3"}
+
 function toggleChangelog(arg){
 	$("#changelogDisplay").SetHasClass("changelogDisplayHidden", !$("#changelogDisplay").BHasClass("changelogDisplayHidden"))
-	$("#descriptionDisplay").visible = true
-	$("#updateDisplay").visible = false	
-	$("#creditsDisplay").visible = false
-	// $("#OptionTextDescription").AddClass("displaySelected");
-	// $("#OptionTextUpdates").RemoveClass("displaySelected");
-	// $("#OptionTextCredits").RemoveClass("displaySelected");
+
+	if (arg) { //shortcut to open panel and select specific tab
+		arg();
+	}
 }
 
 function toggleDescription(arg){
 	$("#descriptionDisplay").visible = true
 	$("#updateDisplay").visible = false
 	$("#creditsDisplay").visible = false
-	// $("#OptionTextDescription").AddClass("displaySelected");
-	// $("#OptionTextUpdates").RemoveClass("displaySelected");
-	// $("#OptionTextCredits").RemoveClass("displaySelected");
+	$("#showDescriptionButton").checked = true;
 }
 
 function toggleUpdates(arg){
 	$("#descriptionDisplay").visible = false
 	$("#updateDisplay").visible = true
 	$("#creditsDisplay").visible = false
-	// $("#OptionTextDescription").RemoveClass("displaySelected");
-	// $("#OptionTextUpdates").AddClass("displaySelected");
-	// $("#OptionTextCredits").RemoveClass("displaySelected");
-
+	$("#showUpdatesButton").checked = true;
 }
 
 function toggleCredits(arg){
 	$("#descriptionDisplay").visible = false
 	$("#updateDisplay").visible = false
 	$("#creditsDisplay").visible = true
-	// $("#OptionTextDescription").RemoveClass("displaySelected");
-	// $("#OptionTextUpdates").RemoveClass("displaySelected");
-	// $("#OptionTextCredits").AddClass("displaySelected");
+	$("#showCreditsButton").checked = true;
+}
+
+function setMessagesNumber() {
+	var messageCount = messages.length;
+
+	$("#changelogNotificationLabel").text = messageCount;
+}
+
+function decrementLabelNumber(panel) {
+	var number = parseInt(panel.text);
+	number--;
+	if (number <= 0) {
+		panel.GetParent().visible = false;
+	} else {
+		panel.text = number;
+	}
+}
+
+function openMessage(args) {
 
 }
 
-(function() {
+function setupCredits() {
 	var panel = $("#creditsPanel");
       
 	for (var steamID3 in GameUI.CustomUIConfig().premiumData) {
@@ -47,12 +63,42 @@ function toggleCredits(arg){
 			var userPic = $.CreatePanel("Panel", panel, steamID3);
 			userPic.BLoadLayoutSnippet("userPic");
 
-			userPic.FindChildTraverse("avatar").steamid = GameUI.CustomUIConfig().premiumData[steamID3]["steamID64"];
+			var steamID64 = GameUI.CustomUIConfig().premiumData[steamID3]["steamID64"]
+
+			for (var message in messages) {
+				(function () {
+					var messageTable = messages[message];
+
+					if (messageTable.steamid == steamID64) {
+						var devMessagePanel = $.CreatePanel("Panel", userPic.FindChildTraverse("userPicMessages"), "devMessage_"+steamID64);
+						devMessagePanel.BLoadLayoutSnippet("devMessage");
+						devMessagePanel.FindChildTraverse("devMessageLabel").text = "!";
+
+						devMessagePanel.SetPanelEvent('onactivate', function(){
+							var messageText = messageTable.message;
+							Game.EmitSound( "ui.click_alt" );
+
+							decrementLabelNumber($("#changelogNotificationLabel"))
+							devMessagePanel.visible = false;
+
+							$.DispatchEvent( "UIShowCustomLayoutPopupParameters", "CustomPopupTest", "file://{resources}/layout/custom_game/dev_message.xml", "popupvalue="+messageText);
+						});
+					}
+				})();
+			}
+
+			userPic.FindChildTraverse("avatar").steamid = steamID64;
 
 			userPic.FindChildTraverse("userPicName").text = $.Localize(steamID3.toString());
 			userPic.FindChildTraverse("userPicDescription").text = $.Localize(steamID3.toString()+ "_Description");
 		})();
 	}
+}
 
+(function() {
+	setMessagesNumber()
+	setupCredits()
+
+	$("#descriptionDisplay").visible = true
 	$("#showDescriptionButton").checked = true;
 })();
