84485a080ba2293c381746401c0524f62c5c6794
Yahnich <y.slagmeulder@hotmail.com>
2016-07-16 23:35:11 +0200

    fixed precache issues and tinkered with ability removal

--- src/game/scripts/abilities/true_random_lod.lua
+++ src/game/scripts/abilities/true_random_lod.lua
@@ -1,16 +1,34 @@
 local Timers = require('easytimers')
 local util = require('util')
+local skillManager = require('skillmanager')
 
 function RandomGet(keys)
 	local caster = keys.caster
 	local ability = keys.ability
+	skillManager:precacheSkill(caster.randomAb, function() return 1 end) -- dynamic caching
+	print(caster.randomAb, "wtf")
 	local randomAb = caster:FindAbilityByName(caster.randomAb)
-	if not IsValidEntity(randomAb) then
+	if not randomAb then
 		randomAb = caster:AddAbility(caster.randomAb)
 		if caster.subAb then
 		    subAb = caster:AddAbility(caster.subAb)
 		end
 	end
+	
+	-- SAFETY MEASURE UNTIL WEIRD NIL BUG IS FOUNF
+	if not randomAb then
+		local picker = math.random(#ability.randomSelection)
+		caster.randomAb = ability.randomSelection[picker]
+		if ability.subList[caster.randomAb] then caster.subAb = ability.subList[caster.randomAb] end
+		local randomAb = caster:FindAbilityByName(caster.randomAb)
+		if not randomAb then
+			randomAb = caster:AddAbility(caster.randomAb)
+			if caster.subAb then
+				subAb = caster:AddAbility(caster.subAb)
+			end
+		end
+	end
+	--------- REMOVE ONCE BUG FOUND -------------
 	local maxLevel = randomAb:GetMaxLevel()
 	PrecacheItemByNameAsync(randomAb:GetName(),function()
 		return randomAb
@@ -51,83 +69,98 @@ function RandomRemove(keys)
 	local ability = keys.ability
 	local randomAb = caster:FindAbilityByName(caster.randomAb)
 	if caster.subAb and caster.subActivated then randomAb = caster:FindAbilityByName(caster.subAb) end
-	
+	print(randomAb:GetName())
 	-- Level main ability if random ability is leveled
 	if randomAb:GetLevel() > ability:GetLevel() then ability:SetLevel(randomAb:GetLevel()) end
 	if IsValidEntity(randomAb) then
 		caster:SwapAbilities(ability:GetName(), randomAb:GetName(), true, false)
 		randomAb:SetHidden(true) -- double check for flyout
 	end
-	if caster.safeRemoveList then 
+	if ability.safeRemoveList then 
 		-- caster:RemoveAbility(caster.lastAbility)
 		local abName = caster.randomAb
-		if caster.randomKv["safe"][abName] then
+		if ability.randomKv["safe"..ability.type][abName] then
 			caster:RemoveAbility(abName)
-			if caster.subList[abName] then caster.subAb = caster.subList[abName] end
+			if ability.subList[abName] then caster.subAb = ability.subList[abName] end
 			if caster.subAb then
 			    subAb = caster:RemoveAbility(caster.subAb)
 			end
 		else
-			caster.safeRemoveList[abName] = false
+			ability.safeRemoveList[abName] = false
 		    local setdelay = randomAb:GetCooldown(-1)
 		    local buffer = 2
             local timer = getAbilityDuration(randomAb, setdelay, buffer)
 			Timers:CreateTimer(function()
-				caster.safeRemoveList[abName] = true
+				ability.safeRemoveList[abName] = true
+				for k,v in pairs(ability.safeRemoveList) do
+					if v == true and caster:FindAbilityByName(k) and caster:FindAbilityByName(k):IsHidden() == true then
+						caster:RemoveAbility(k)
+						if ability.subList[k] then caster:RemoveAbility(ability.subList[k]) end
+						print(k, ability.subList[k])
+					end
+				end
 	        	return nil
 	    	end, abName..math.random(99999), timer)
 		end
-
-		for k,v in pairs(caster.safeRemoveList) do
-	    	if v == true and caster:FindAbilityByName(k) and caster:FindAbilityByName(k):IsHidden() == true then
-	    		caster:RemoveAbility(k)
-	    	end
-	    end
 	end
 
-	local picker = math.random(#caster.randomSelection)
-	caster.randomAb = caster.randomSelection[picker]
+	local picker = math.random(#ability.randomSelection)
+	caster.randomAb = ability.randomSelection[picker]
 	if 15 < GetAbilityCount(caster) then
-		picker = math.random(#caster.randomSafeSelection)
-		caster.randomAb = caster.randomSafeSelection[picker]
+		picker = math.random(#ability.randomSafeSelection )
+		local pickedSkill = ability.randomSafeSelection [picker]
+		if not caster.ownedSkill[pickedSkill] then
+			caster.randomAb = pickedSkill
+			print(picker,pickedSkill)
+		else
+			local pickedSkill
+			while caster.ownedSkill[pickedSkill] do
+				picker = math.random(#ability.randomSafeSelection )
+				pickedSkill = ability.randomSafeSelection [picker]
+				print(picker,pickedSkill)
+			end
+			caster.randomAb = pickedSkill
+		end
 	end
-	caster.subAb = caster.subList[caster.randomAb]	
+	caster.subAb = ability.subList[caster.randomAb]	
 end
 
 function RandomInit(keys)
 	local ability = keys.ability
 	local caster = keys.caster
-	caster.randomKv = LoadKeyValues('scripts/kv/randompicker.kv')
-	caster.safeRemoveList = {}
+	ability.type = keys.value
+	ability.randomKv = LoadKeyValues('scripts/kv/randompicker.kv')
+	ability.safeRemoveList = {}
 	caster.random = ability
-	caster.subList = LoadKeyValues('scripts/kv/abilityDeps.kv')
+	ability.subList = LoadKeyValues('scripts/kv/abilityDeps.kv')
 	-- check currently owned and visible skills to prevent repeats
-	local ownedSkill={}
+	caster.ownedSkill={}
 	for i = 0, GetAbilityCount(caster)-1 do
 		local exclusion = caster:GetAbilityByIndex(i):GetName()
 		if not caster:FindAbilityByName(exclusion):IsHidden() then
-			ownedSkill[exclusion] = true
+			caster.ownedSkill[exclusion] = true
 		end
 	end
 	-- find desired flags
-	for k, v in pairs( caster.randomKv ) do
+	for k, v in pairs( ability.randomKv ) do
+		local x = {} -- xclusion
 		if k == keys.value then
 			-- change values to ascending sequence
 			local i = 1
 			for l,m in pairs(v) do
-				if not ownedSkill[l] then -- do not add already owned skills to possible set
-					v[l] = i
+				if not caster.ownedSkill[l] then -- do not add already owned skills to possible set
+					x[l] = i
 					i = i + 1
 				end
 			end
 			-- invert keys and values to make ability names the value; second step to turning table into array
 			local s={}
-			for l,m in pairs(v) do
+			for l,m in pairs(x) do
 				s[m]=l
 			end
-			caster.randomSelection = s
+			ability.randomSelection = s
 		end
-		if k == "safe" then
+		if k == "safe"..ability.type then
 			-- change values to ascending sequence
 			local i = 1
 			for l,m in pairs(v) do
@@ -139,10 +172,10 @@ function RandomInit(keys)
 			for l,m in pairs(v) do
 				s[m]=l
 			end
-			caster.randomSafeSelection = s
+			ability.randomSafeSelection  = s
 		end
 	end
-	local picker = math.random(#caster.randomSelection)
-	caster.randomAb = caster.randomSelection[picker]
-	if caster.subList[caster.randomAb] then caster.subAb = caster.subList[caster.randomAb] end
+	local picker = math.random(#ability.randomSelection)
+	caster.randomAb = ability.randomSelection[picker]
+	if ability.subList[caster.randomAb] then caster.subAb = ability.subList[caster.randomAb] end
 end
