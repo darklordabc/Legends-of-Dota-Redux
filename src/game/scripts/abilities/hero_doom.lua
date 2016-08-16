33a5c77de0a8f30adc7ba4f1aa96d7bcd7ed8677
darklordabc <curtisjdubois@hotmail.com>
2016-08-16 15:56:17 +1000

    no message

--- src/game/scripts/abilities/hero_doom.lua
+++ src/game/scripts/abilities/hero_doom.lua
@@ -3,19 +3,37 @@ function EatCreep ( keys )
 	local ability = keys.ability
 	local target = keys.target
 
-	if target:IsValidEntity(true) and not target:IsAncient() and not caster:HasModifier("modifier_creep_eaten") then
-		local damage = target:GetHealth()
-		ApplyDamage({victim = target, attacker = caster, damage = damage, damage_type = DAMAGE_TYPE_PURE})
-		ability:ApplyDataDrivenModifier(caster, target, "modifier_creep_eaten", {duration = damage / 20})
+	if IsValidEntity(target) then
+		local health = target:GetHealth()
+		target:Kill(ability, caster)
+		ability:ApplyDataDrivenModifier(caster, caster, "modifier_creep_eaten", {duration = health / 20})
 	end
 end
 
 function CreepGold ( keys )
 	local caster = keys.caster
 	local ability = keys.ability
-	local gold = ability:GetLevelSpecialValueFor("devour_gold", ability:GetLevel) - 1
+	local gold = ability:GetLevelSpecialValueFor("devour_gold", ability:GetLevel()) - 1
 
 	if caster:IsAlive() then
 	caster:ModifyGold(gold, false, 0)
 	end 
+end
+
+--[[Author: igo95862, Noya
+	Used by: Pizzalol
+	Date: 27.01.2016.
+	Disallows eating another unit while Devour is in progress]]
+function DevourCheck( keys )
+	local caster = keys.caster
+	local modifier = keys.modifier
+	local player = caster:GetPlayerOwner()
+	local pID = caster:GetPlayerOwnerID()
+
+	if caster:HasModifier(modifier) then
+		caster:Interrupt()
+
+		-- Play Error Sound
+		EmitSoundOnClient("General.CastFail_InvalidTarget_Hero", player)
+	end
 end
