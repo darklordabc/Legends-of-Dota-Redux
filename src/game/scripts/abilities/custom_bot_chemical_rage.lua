0f160684d35bb90d0ddd746443fc03e5c4479fe3
SwordBacon <swordofbacon@gmail.com>
2016-08-29 12:44:46 -0600

    Add files via upload

---
+++ src/game/scripts/abilities/custom_bot_chemical_rage.lua
@@ -0,0 +1,23 @@
+--[[
+	Author: Noya (modified by SwordBacon)
+	Date: 9.1.2015.
+	Checks if the caster HP dropped below the threshold
+]]
+function ChemicalRageActivate( event )
+	-- Variables
+	local caster = event.caster
+	local ability = event.ability
+	local cooldown = ability:GetCooldown( ability:GetLevel() )
+
+	-- Cast the spell
+	if caster:GetHealthPercent() < 90 and ability:GetCooldownTimeRemaining() == 0 then
+		caster:CastAbilityImmediately(ability, caster:GetPlayerOwnerID())
+		ability:StartCooldown( cooldown )
+		caster:Stop()
+		caster:EmitSound("Hero_Alchemist.ChemicalRage.Cast")
+	end
+end
+
+function RemoveLoop( keys )
+	keys.caster:StopSound("Hero_Alchemist.ChemicalRage")
+end
