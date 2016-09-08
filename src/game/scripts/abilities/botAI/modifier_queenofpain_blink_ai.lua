5b5624403ede92ba1e30d07b18e96ce35cc77b66
SwordBacon <swordofbacon@gmail.com>
2016-09-07 21:22:26 -0600

    Update modifier_queenofpain_blink_ai.lua

--- src/game/scripts/abilities/botAI/modifier_queenofpain_blink_ai.lua
+++ src/game/scripts/abilities/botAI/modifier_queenofpain_blink_ai.lua
@@ -42,7 +42,7 @@ function modifier_queenofpain_blink_ai:OnIntervalThink()
 		elseif caster:GetHealthPercent() < 30 and not target and not caster:HasModifier("modifier_bloodseeker_rupture") then
 			shouldBlink = true
 			--print("blink retreat")
-		elseif GridNav:IsTraversable(caster:GetAbsOrigin()) then
+		elseif not GridNav:IsTraversable(caster:GetAbsOrigin()) then
 			shouldBlink = true
 			--print("blocked!")
 		end
