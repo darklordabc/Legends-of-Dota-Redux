6fd1258dd27672c23162a4eaba77fc234a54eeda
Wouterz90 <wouter.zegers@gmail.com>
2016-09-16 20:28:47 +0200

    Update npc_dota_hero_puck_perk.lua

--- src/game/scripts/abilities/hero_perks/npc_dota_hero_puck_perk.lua
+++ src/game/scripts/abilities/hero_perks/npc_dota_hero_puck_perk.lua
@@ -4,7 +4,7 @@
 --    Perk: 
 --
 --------------------------------------------------------------------------------------------------------
---LinkLuaModifier( "modifier_npc_dota_hero_puck_perk", "scripts/vscripts/../abilities/hero_perks/npc_dota_hero_puck_perk.lua" ,LUA_MODIFIER_MOTION_NONE )
+LinkLuaModifier( "modifier_npc_dota_hero_puck_perk", "scripts/vscripts/../abilities/hero_perks/npc_dota_hero_puck_perk.lua" ,LUA_MODIFIER_MOTION_NONE )
 --------------------------------------------------------------------------------------------------------
 if npc_dota_hero_puck_perk == nil then npc_dota_hero_puck_perk = class({}) end
 --------------------------------------------------------------------------------------------------------
