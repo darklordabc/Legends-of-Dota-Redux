cdc15a33cb120d69a01f66db08675f2e45be3104
Wouterz90 <wouter.zegers@gmail.com>
2016-09-16 16:55:20 +0200

    Added perk for Storm.

--- src/game/scripts/abilities/hero_perks/npc_dota_hero_storm_spirit_perk.lua
+++ src/game/scripts/abilities/hero_perks/npc_dota_hero_storm_spirit_perk.lua
@@ -1,25 +1,34 @@
 --------------------------------------------------------------------------------------------------------
 --
---		Hero: storm_spirit
---		Perk: 
+--    Hero: storm
+--    Perk: Gives 1 mana everytime the OnUnitMoved event triggers
 --
 --------------------------------------------------------------------------------------------------------
 LinkLuaModifier( "modifier_npc_dota_hero_storm_spirit_perk", "scripts/vscripts/../abilities/hero_perks/npc_dota_hero_storm_spirit_perk.lua" ,LUA_MODIFIER_MOTION_NONE )
 --------------------------------------------------------------------------------------------------------
 if npc_dota_hero_storm_spirit_perk == nil then npc_dota_hero_storm_spirit_perk = class({}) end
 --------------------------------------------------------------------------------------------------------
---		Modifier: modifier_npc_dota_hero_storm_spirit_perk				
+--    Modifier: modifier_npc_dota_hero_storm_perk        
 --------------------------------------------------------------------------------------------------------
 if modifier_npc_dota_hero_storm_spirit_perk == nil then modifier_npc_dota_hero_storm_spirit_perk = class({}) end
 --------------------------------------------------------------------------------------------------------
 function modifier_npc_dota_hero_storm_spirit_perk:IsPassive()
-	return true
+  return true
 end
 --------------------------------------------------------------------------------------------------------
 function modifier_npc_dota_hero_storm_spirit_perk:IsHidden()
-	return true
+  return false
 end
 --------------------------------------------------------------------------------------------------------
 -- Add additional functions
 --------------------------------------------------------------------------------------------------------
+function modifier_npc_dota_hero_storm_spirit_perk:DeclareFunctions()
+    local funcs = {
+        MODIFIER_EVENT_ON_UNIT_MOVED,
+    }
+    return funcs
+end
 
+function modifier_npc_dota_hero_storm_spirit_perk:OnUnitMoved(keys)
+  self:GetParent():GiveMana(1)
+end
