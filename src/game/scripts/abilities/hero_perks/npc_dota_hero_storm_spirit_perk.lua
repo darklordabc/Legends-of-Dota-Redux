7f88cf134fae3c3b8fa19be95679c320c3aea78d
Wouterz90 <wouter.zegers@gmail.com>
2016-09-16 22:23:05 +0200

    Update npc_dota_hero_storm_spirit_perk.lua

--- src/game/scripts/abilities/hero_perks/npc_dota_hero_storm_spirit_perk.lua
+++ src/game/scripts/abilities/hero_perks/npc_dota_hero_storm_spirit_perk.lua
@@ -1,7 +1,7 @@
 --------------------------------------------------------------------------------------------------------
 --
 --    Hero: storm
---    Perk: Gives mana based on the difference measured between the old and new position in one second intervals
+--    Perk: Gives 1 mana everytime the OnUnitMoved event triggers
 --
 --------------------------------------------------------------------------------------------------------
 LinkLuaModifier( "modifier_npc_dota_hero_storm_spirit_perk", "scripts/vscripts/../abilities/hero_perks/npc_dota_hero_storm_spirit_perk.lua" ,LUA_MODIFIER_MOTION_NONE )
@@ -27,18 +27,21 @@ function modifier_npc_dota_hero_storm_spirit_perk:OnCreated()
 end
 
 function modifier_npc_dota_hero_storm_spirit_perk:OnIntervalThink()
+  local manaGiven = (1/25)
+  local maxRange = 3000
+
   if IsServer() then
     local currTime = GameRules:GetGameTime()
     if not self:GetCaster().position then self:GetCaster().position = {} end
     if not self:GetCaster().position[math.floor(currTime)-1] then self:GetCaster().position[math.floor(currTime)-1] = self:GetCaster():GetAbsOrigin() end
     self:GetCaster().position[math.floor(currTime)] = self:GetCaster():GetAbsOrigin()
 
-    if (self:GetCaster().position[math.floor(currTime)] - self:GetCaster().position[math.floor(currTime)-1]):Length2D() > 2000 then
+    if (self:GetCaster().position[math.floor(currTime)] - self:GetCaster().position[math.floor(currTime)-1]):Length2D() > maxRange then
       self.distanceMoved = 0
     else
       self.distanceMoved =  (self:GetCaster().position[math.floor(currTime)] - self:GetCaster().position[math.floor(currTime)-1]):Length2D()
     end
-    self:GetCaster():GiveMana(self.distanceMoved/25)
+    self:GetCaster():GiveMana(self.distanceMoved/manaGiven)
     for t, pos in pairs(self:GetCaster().position) do
       if (currTime-t) > 1 then
         self:GetCaster().position[t] = nil
