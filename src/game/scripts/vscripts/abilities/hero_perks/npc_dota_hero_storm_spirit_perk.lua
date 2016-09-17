b0ff0dcddc69e2481a34edb62b764aec8b758df9
SwordBacon <swordofbacon@gmail.com>
2016-09-17 12:50:41 -0600

    Update npc_dota_hero_storm_spirit_perk.lua

--- src/game/scripts/vscripts/abilities/hero_perks/npc_dota_hero_storm_spirit_perk.lua
+++ src/game/scripts/vscripts/abilities/hero_perks/npc_dota_hero_storm_spirit_perk.lua
@@ -1,10 +1,11 @@
 --------------------------------------------------------------------------------------------------------
 --
---    Hero: storm
---    Perk: Gives 1 mana everytime the OnUnitMoved event triggers
+--    Hero: Storm Spirit
+--    Perk: Restores mana when Storm Spirit travels at high speeds. 
+--    Function: Compares position every 0.1 second between the previous position, after the initial 50 units the unit will get its unit moved/10 mana
 --
 --------------------------------------------------------------------------------------------------------
-LinkLuaModifier( "modifier_npc_dota_hero_storm_spirit_perk", "abilities/hero_perks/npc_dota_hero_storm_spirit_perk.lua" ,LUA_MODIFIER_MOTION_NONE )
+LinkLuaModifier( "modifier_npc_dota_hero_storm_spirit_perk", "scripts/vscripts/../abilities/hero_perks/npc_dota_hero_storm_spirit_perk.lua" ,LUA_MODIFIER_MOTION_NONE )
 --------------------------------------------------------------------------------------------------------
 if npc_dota_hero_storm_spirit_perk == nil then npc_dota_hero_storm_spirit_perk = class({}) end
 --------------------------------------------------------------------------------------------------------
@@ -27,25 +28,28 @@ function modifier_npc_dota_hero_storm_spirit_perk:OnCreated()
 end
 
 function modifier_npc_dota_hero_storm_spirit_perk:OnIntervalThink()
-  local manaGiven = (1/10)
-  local maxRange = 400
+  local manaGiven = 10
+  local maxRange = 4000
   local startPenalty = 50
 
   if IsServer() then
     local currTime = GameRules:GetGameTime()
     if not self:GetCaster().position then self:GetCaster().position = {} end
-    if not self:GetCaster().position[math.floor(currTime)-1] then self:GetCaster().position[math.floor(currTime)-1] = self:GetCaster():GetAbsOrigin() end
-    self:GetCaster().position[math.floor(currTime)] = self:GetCaster():GetAbsOrigin()
-
-    if (self:GetCaster().position[math.floor(currTime)] - self:GetCaster().position[math.floor(currTime)-1]):Length2D() > maxRange then
+    if not self:GetCaster().position[(math.floor(currTime*10)/10)-0.1] then self:GetCaster().position[(math.floor(currTime*10)/10)-0.1] = self:GetCaster():GetAbsOrigin() end
+    
+    self:GetCaster().position[math.floor(currTime*10)/10] = self:GetCaster():GetAbsOrigin()
+    
+    if (self:GetCaster().position[math.floor(currTime*10)/10] - self:GetCaster().position[(math.floor(currTime*10)/10)-0.1]):Length2D() > maxRange then
       self.distanceMoved = 0
     else
-      self.distanceMoved =  ((self:GetCaster().position[math.floor(currTime)] - self:GetCaster().position[math.floor(currTime)-1]):Length2D() -startPenalty)
+      self.distanceMoved =  ((self:GetCaster().position[math.floor(currTime*10)/10] - self:GetCaster().position[(math.floor(currTime*10)/10)-0.1]):Length2D() -startPenalty)
     end
+
     if self.distanceMoved < 0 then
       self.distanceMoved = 0
     end
     self:GetCaster():GiveMana(self.distanceMoved/manaGiven)
+    --print(self.distanceMoved.." "..(self.distanceMoved/manaGiven))
     for t, pos in pairs(self:GetCaster().position) do
       if (currTime-t) > 1 then
         self:GetCaster().position[t] = nil
