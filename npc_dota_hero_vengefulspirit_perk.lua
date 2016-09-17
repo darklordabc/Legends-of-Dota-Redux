a719eabc0893cb9d575d2441739acc6753eb9ff9
SwordBacon <swordofbacon@gmail.com>
2016-09-16 18:14:44 -0600

    Add files via upload

---
+++ npc_dota_hero_vengefulspirit_perk.lua
@@ -0,0 +1,25 @@
+--------------------------------------------------------------------------------------------------------
+--
+--		Hero: vengefulspirit
+--		Perk: 
+--
+--------------------------------------------------------------------------------------------------------
+LinkLuaModifier( "modifier_npc_dota_hero_vengefulspirit_perk", "abilities/hero_perks/npc_dota_hero_vengefulspirit_perk.lua" ,LUA_MODIFIER_MOTION_NONE )
+--------------------------------------------------------------------------------------------------------
+if npc_dota_hero_vengefulspirit_perk == nil then npc_dota_hero_vengefulspirit_perk = class({}) end
+--------------------------------------------------------------------------------------------------------
+--		Modifier: modifier_npc_dota_hero_vengefulspirit_perk				
+--------------------------------------------------------------------------------------------------------
+if modifier_npc_dota_hero_vengefulspirit_perk == nil then modifier_npc_dota_hero_vengefulspirit_perk = class({}) end
+--------------------------------------------------------------------------------------------------------
+function modifier_npc_dota_hero_vengefulspirit_perk:IsPassive()
+	return true
+end
+--------------------------------------------------------------------------------------------------------
+function modifier_npc_dota_hero_vengefulspirit_perk:IsHidden()
+	return true
+end
+--------------------------------------------------------------------------------------------------------
+-- Add additional functions
+--------------------------------------------------------------------------------------------------------
+
