4558aeb9a6f923d60dc263089044812c03b7ff3c
SwordBacon <swordofbacon@gmail.com>
2016-09-16 18:19:38 -0600

    Update npc_dota_hero_enigma_perk.lua

--- src/game/scripts/abilities/hero_perks/npc_dota_hero_enigma_perk.lua
+++ src/game/scripts/abilities/hero_perks/npc_dota_hero_enigma_perk.lua
@@ -4,7 +4,7 @@
 --		Perk: 
 --
 --------------------------------------------------------------------------------------------------------
-LinkLuaModifier( "modifier_npc_dota_hero_enigma_perk", "scripts/vscripts/../abilities/hero_perks/npc_dota_hero_enigma_perk.lua" ,LUA_MODIFIER_MOTION_NONE )
+LinkLuaModifier( "modifier_npc_dota_hero_enigma_perk", "abilities/hero_perks/npc_dota_hero_enigma_perk.lua" ,LUA_MODIFIER_MOTION_NONE )
 --------------------------------------------------------------------------------------------------------
 if npc_dota_hero_enigma_perk == nil then npc_dota_hero_enigma_perk = class({}) end
 --------------------------------------------------------------------------------------------------------
