1683bc8431b6fe975c26ca7b1eb8157188f8e5a7
Wouterz90 <wouter.zegers@gmail.com>
2016-09-18 10:38:46 +0200

    Moved the hero specific function to their file

--- src/game/scripts/vscripts/abilities/hero_perks/hero_perks_filters.lua
+++ src/game/scripts/vscripts/abilities/hero_perks/hero_perks_filters.lua
@@ -1,5 +1,39 @@
 function heroPerksDamageFilter(filterTable)
+  local targetIndex = filterTable["entindex_target_const"]
+  local target = EntIndexToHScript(targetIndex)
+  local casterIndex = filterTable["entindex_source_const"]
+  local caster = EntIndexToHScript(casterIndex)
+  local abilityIndex = filterTable["entindex_ability_const"]
+  local ability = EntIndexToHScript(abilityIndex)
+  
+  -- Perk for Puck
+  local puckPerk = require('scripts/vscripts/abilities/hero_perks/npc_dota_hero_puck_perk')
+  PerkPuckReflectSpell(caster,target,ability)
+  
+  local targetPerk = target:FindAbilityByName(target:GetName() .. "_perk")
+  if targetPerk and targetPerks_projectile[targetPerk:GetName()] then
+    target.perkTarget = caster
+    target.perkAbility = ability
+  end
+  -- Returning the filterTable
+  return filterTable
 end
 
-function heroPerksProjectileFilter(filterTable)
+function heroPerksModifierFilter(filterTable)
+  local parent_index = filterTable["entindex_parent_const"]
+  local caster_index = filterTable["entindex_caster_const"]
+  local ability_index = filterTable["entindex_ability_const"]
+  if not parent_index or not caster_index or not ability_index then
+      return true
+  end
+  local parent = EntIndexToHScript( parent_index )
+  local caster = EntIndexToHScript( caster_index )
+  local ability = EntIndexToHScript( ability_index )
+  
+  -- Perk for Dragon Knight
+  local puckDragonKnight = require('scripts/vscripts/abilities/hero_perks/npc_dota_hero_puck_dragon_knight')
+  PerkDragonKnight(filterTable)
+  
+  -- Returning the filterTable
+  return filterTable
 end
