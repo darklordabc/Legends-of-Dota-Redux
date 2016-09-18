3bc5e64b9713c265b1169e27ad6ad0672c060bcd
SwordBacon <swordofbacon@gmail.com>
2016-09-17 18:21:23 -0600

    Add SF perk

--- src/game/scripts/vscripts/abilities/hero_perks/npc_dota_hero_nevermore_perk.lua
+++ src/game/scripts/vscripts/abilities/hero_perks/npc_dota_hero_nevermore_perk.lua
@@ -1,7 +1,7 @@
 --------------------------------------------------------------------------------------------------------
 --
---		Hero: nevermore
---		Perk: 
+--		Hero: Shadow Fiend
+--		Perk: At the start of the game, Shadow Fiend gains a free level of Necromastery, whether he has it or not.
 --
 --------------------------------------------------------------------------------------------------------
 LinkLuaModifier( "modifier_npc_dota_hero_nevermore_perk", "abilities/hero_perks/npc_dota_hero_nevermore_perk.lua" ,LUA_MODIFIER_MOTION_NONE )
@@ -22,4 +22,19 @@ end
 --------------------------------------------------------------------------------------------------------
 -- Add additional functions
 --------------------------------------------------------------------------------------------------------
+function modifier_npc_dota_hero_nevermore_perk:OnCreated(keys)
+	local caster = self:GetCaster()
 
+	local necromastery = caster:FindAbilityByName("nevermore_necromastery") or nil
+
+	if necromastery then
+		necromastery:UpgradeAbility(false)
+	else 
+		necromastery = caster:AddAbility("nevermore_necromastery")
+		necromastery:SetHidden(true)
+		necromastery:SetLevel(1)
+	end
+
+	return keys
+end
+--------------------------------------------------------------------------------------------------------
