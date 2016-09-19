60e714a7984c67176a8b3c522a6a9c026c27ed7a
Yahnich <y.slagmeulder@hotmail.com>
2016-09-19 22:44:28 +0200

    changed mist coil handling

--- src/game/scripts/vscripts/abilities/hero_perks/npc_dota_hero_abaddon_perk.lua
+++ src/game/scripts/vscripts/abilities/hero_perks/npc_dota_hero_abaddon_perk.lua
@@ -47,36 +47,43 @@ end
 function modifier_npc_dota_hero_abaddon_perk:OnIntervalThink()
 	if not self.activated then
 		local shield = self:GetParent():FindAbilityByName("abaddon_aphotic_shield")
-		if shield:GetLevel() > 0 then
-			if shield and shield:GetLevel() > 0 then
-				self:GetParent():AddNewModifier(self:GetParent(), shield, "modifier_charges",
-					{
-						max_count = 2,
-						start_count = 1,
-						replenish_time = shield:GetCooldown(-1)
-					}
-				)
-			end
-			self.activated = true
+		if shield and shield:GetLevel() > 0 then
+			self:GetParent():AddNewModifier(self:GetParent(), shield, "modifier_charges",
+				{
+					max_count = 2,
+					start_count = 1,
+					replenish_time = shield:GetCooldown(-1)
+				}
+			)
 		end
+		self.activated = true
 	end
 end
 
 local Timers = require('easytimers')
 
+function PerkAbaddon(filterTable)
+	local victim_index = filterTable["entindex_victim_const"]
+    local attacker_index = filterTable["entindex_attacker_const"]
+    local ability_index = filterTable["entindex_inflictor_const"]
+    if not victim_index or not attacker_index or not ability_index then
+        return true
+    end
+	print("perk")
+    local attacker = EntIndexToHScript( victim_index )
+    local victim = EntIndexToHScript( attacker_index )
+    local ability = EntIndexToHScript( ability_index )
+	local targetPerk = attacker:FindAbilityByName(attacker:GetName() .. "_perk")
+	if targetPerk and targetPerks_damage[targetPerk:GetName()] then
+		if ability and ability:GetName() == "abaddon_death_coil" and attacker == victim then
+			filterTable["damage"] = 0
+			attacker:Heal(ability:GetSpecialValueFor("self_damage"), attacker)
+		end
+	end
+ end
+
 function modifier_npc_dota_hero_abaddon_perk:OnAbilityExecuted(params)
 	if params.unit == self:GetParent() then
-		if params.ability:GetName() == "abaddon_death_coil" then
-			local coil = params.ability
-			self:GetParent():Heal(coil:GetSpecialValueFor("self_damage")*2, self:GetParent())
-			Timers:CreateTimer(function()
-				if not coil:IsCooldownReady() then
-					self:GetParent():Heal(coil:GetSpecialValueFor("self_damage")*2, self:GetParent())
-				else
-					return 0.01
-				end
-			end, DoUniqueString('abbadonSelfHeal'), 0.01)
-		end
 		if params.ability:GetName() == "abaddon_aphotic_shield" then
 			local shield = params.ability
 			local stacks = self:GetParent():GetModifierStackCount("modifier_charges", self:GetParent())
