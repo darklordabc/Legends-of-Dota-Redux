69860f9b77a4813c09460504d1978733d7784614
SwordBacon <swordofbacon@gmail.com>
2016-09-20 11:25:09 -0600

    Update npc_dota_hero_storm_spirit_perk.lua

--- src/game/scripts/vscripts/abilities/hero_perks/npc_dota_hero_storm_spirit_perk.lua
+++ src/game/scripts/vscripts/abilities/hero_perks/npc_dota_hero_storm_spirit_perk.lua
@@ -24,9 +24,9 @@ end
 -- Add additional functions
 --------------------------------------------------------------------------------------------------------
 function modifier_npc_dota_hero_storm_spirit_perk:OnCreated()
-  if IsServer() then
-		self:StartIntervalThink(0.1)
-	end
+    if IsServer() then
+        self:StartIntervalThink(0.1)
+    end
 end
 
 function modifier_npc_dota_hero_storm_spirit_perk:OnIntervalThink()
