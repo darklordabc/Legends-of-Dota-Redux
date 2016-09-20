83f4b64d6d3af412c59176d00a45abc676344e20
Yahnich <y.slagmeulder@hotmail.com>
2016-09-20 15:00:53 +0200

    added death prevention to make sure perk triggers

--- src/game/scripts/vscripts/abilities/hero_perks/npc_dota_hero_phoenix_perk.lua
+++ src/game/scripts/vscripts/abilities/hero_perks/npc_dota_hero_phoenix_perk.lua
@@ -29,15 +29,28 @@ end
 function modifier_npc_dota_hero_phoenix_perk:DeclareFunctions()
 	local funcs = {
 		MODIFIER_EVENT_ON_TAKEDAMAGE,
+		MODIFIER_PROPERTY_MIN_HEALTH,
 	}
 	return funcs
 end
+if IsServer() then
+	function modifier_npc_dota_hero_phoenix_perk:OnCreated()
+		self.egg = self:GetParent():FindAbilityByName("phoenix_supernova")
+	end
+	
+	function modifier_npc_dota_hero_phoenix_perk:OnTakeDamage(params)
+		if params.unit == self:GetParent() then
+			if params.damage > self:GetParent():GetHealth() and self.egg and self.egg:IsCooldownReady() then
+				self:GetParent():CastAbilityNoTarget(self.egg, self:GetParent():GetPlayerID())
+			end
+		end
+	end
 
-function modifier_npc_dota_hero_phoenix_perk:OnTakeDamage(params)
-	if params.unit == self:GetParent() then
-		local egg = self:GetParent():FindAbilityByName("phoenix_supernova")
-		if params.damage > self:GetParent():GetHealth() and egg and egg:IsCooldownReady() then
-			self:GetParent():CastAbilityNoTarget(egg, self:GetParent():GetPlayerID())
+	function modifier_npc_dota_hero_phoenix_perk:GetMinHealth(params)
+		if self.egg and self.egg:GetLevel() > 0 and self.egg:IsCooldownReady() then
+			return 1
+		else
+			return 0
 		end
 	end
 end
