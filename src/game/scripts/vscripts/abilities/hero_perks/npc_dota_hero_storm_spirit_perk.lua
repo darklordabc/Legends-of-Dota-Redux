ee3a2da5a1a224e1f97e2b15080f71e17316852e
Wouterz90 <wouter.zegers@gmail.com>
2016-09-20 20:03:37 +0200

    Added onabilitystart
    Minor improvement in consistency

--- src/game/scripts/vscripts/abilities/hero_perks/npc_dota_hero_storm_spirit_perk.lua
+++ src/game/scripts/vscripts/abilities/hero_perks/npc_dota_hero_storm_spirit_perk.lua
@@ -9,7 +9,7 @@ LinkLuaModifier( "modifier_npc_dota_hero_storm_spirit_perk", "abilities/hero_per
 --------------------------------------------------------------------------------------------------------
 if npc_dota_hero_storm_spirit_perk == nil then npc_dota_hero_storm_spirit_perk = class({}) end
 --------------------------------------------------------------------------------------------------------
---    Modifier: modifier_npc_dota_hero_storm_perk        
+--    Modifier: modifier_npc_dota_hero_storm_spirit_perk        
 --------------------------------------------------------------------------------------------------------
 if modifier_npc_dota_hero_storm_spirit_perk == nil then modifier_npc_dota_hero_storm_spirit_perk = class({}) end
 --------------------------------------------------------------------------------------------------------
@@ -24,41 +24,80 @@ end
 -- Add additional functions
 --------------------------------------------------------------------------------------------------------
 function modifier_npc_dota_hero_storm_spirit_perk:OnCreated()
-    if IsServer() then
-        self:StartIntervalThink(0.1)
-    end
+  if IsServer() then
+    self:StartIntervalThink(0.1)
+    self:GetCaster().position = {}
+  end
 end
 
+function modifier_npc_dota_hero_storm_spirit_perk:DeclareFunctions()
+  local funcs = {
+    MODIFIER_EVENT_ON_ABILITY_START,
+  }
+  return funcs
+end
+
+function modifier_npc_dota_hero_storm_spirit_perk:OnAbilityStart(keys)
+  if IsServer() then
+    if not keys.ability:HasAbilityFlag("teleport") then
+      print(keys.ability:GetAbilityName())
+      self.oldLocation  = self:GetParent():GetAbsOrigin()
+    end
+  end
+end
+
+
+
+  
+
 function modifier_npc_dota_hero_storm_spirit_perk:OnIntervalThink()
-  local manaGiven = 10
-  local maxRange = 4000
+  
+  local distanceFactor = 0.1
   local startPenalty = 50
+  local caster = self:GetParent()
 
   if IsServer() then
-    local currTime = GameRules:GetGameTime()
-    if not self:GetCaster().position then self:GetCaster().position = {} end
-    if not self:GetCaster().position[(math.floor(currTime*10)/10)-0.1] then self:GetCaster().position[(math.floor(currTime*10)/10)-0.1] = self:GetCaster():GetAbsOrigin() end
-    
-    self:GetCaster().position[math.floor(currTime*10)/10] = self:GetCaster():GetAbsOrigin()
-    --print("OldPosition")
-    --print(self:GetCaster().position[(math.floor(currTime*10)/10)-0.1])
-    --print("NewPosition")
-    --print(self:GetCaster():GetAbsOrigin())
-    if (self:GetCaster():GetAbsOrigin() - self:GetCaster().position[(math.floor(currTime*10)/10)-0.1]):Length2D() > maxRange then
-      self.distanceMoved = 0
+    local currTime = math.floor(GameRules:GetGameTime()*100)/100 -- We can't be comparing floats but we don't want whole numbers either.
+
+    caster.position[currTime] = caster:GetAbsOrigin() -- Storing the location for the next interval
+    if self.oldLocation then -- oldLocation gets stored whenever an ability get's started and should overwrite the values because blink like spells may happen between the intervals
+      caster.position[currTime-0.1] = self.oldLocation
+      caster.position[currTime-0.2] = self.oldLocation
+      self.blink = true
+      self.oldLocation = nil -- Resetting the location stored from the ability because we don't want it to get reused.
+    end
+
+
+    if not caster.position[currTime-0.1] then -- If this is empty, which seems to happen we use the position before, if that is nil we are starting and use the current position
+      caster.position[currTime-0.1] = caster.position[currTime-0.2]
+      if not caster.position[currTime-0.1] then
+        caster.position[currTime-0.1] = caster:GetAbsOrigin()
+      end
+    end
+
+    local distanceMoved = (caster.position[currTime-0.1] - caster:GetAbsOrigin()):Length2D()
+    local distanceMovedMinusPenalty = distanceMoved - startPenalty
+
+    if distanceMovedMinusPenalty < 1 then 
+      distanceMovedMinusPenalty = 0
+      self.manaGiven = distanceFactor
     else
-      self.distanceMoved =  ((self:GetCaster():GetAbsOrigin() - self:GetCaster().position[(math.floor(currTime*10)/10)-0.1]):Length2D() -startPenalty)
+      if not self.manaGiven then self.manaGiven = distanceFactor end -- Just to make sure this is not nil
+      caster:GiveMana(distanceMovedMinusPenalty*self.manaGiven)
+      if (distanceMovedMinusPenalty*self.manaGiven) ~= 0 then
+        SendOverheadEventMessage(nil, OVERHEAD_ALERT_MANA_ADD, caster, distanceMovedMinusPenalty*self.manaGiven, nil)
+        print(distanceMovedMinusPenalty*self.manaGiven)
+        if self.blink == true then
+          self.manaGiven = 0 -- To prevent double amounts of mana given out due to missing vectors/wrong time rounding
+        end
+      end
     end
-
-    if self.distanceMoved < 0 then
-      self.distanceMoved = 0
-    end
-    self:GetCaster():GiveMana(self.distanceMoved/manaGiven)
-
-    --print(self.distanceMoved.." "..(self.distanceMoved/manaGiven))
-    for t, pos in pairs(self:GetCaster().position) do
-      if (currTime-t) > 1 then
-        self:GetCaster().position[t] = nil
+    
+    
+    
+    for t, pos in pairs(caster.position) do -- Clearing the position values we no longer use
+      if (currTime-t) > 0.5 then
+       caster.position[t] = nil
       else
         break
       end
