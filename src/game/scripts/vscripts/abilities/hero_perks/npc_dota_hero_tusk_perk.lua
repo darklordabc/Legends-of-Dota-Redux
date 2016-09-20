f4c710b543d913c62da8cd43857206f25e1705ea
SwordBacon <swordofbacon@gmail.com>
2016-09-20 16:56:58 -0600

    Add Tusk perk
    Abilities are hard-coded because they are clearly defined in the perk.

--- src/game/scripts/vscripts/abilities/hero_perks/npc_dota_hero_tusk_perk.lua
+++ src/game/scripts/vscripts/abilities/hero_perks/npc_dota_hero_tusk_perk.lua
@@ -1,7 +1,7 @@
 --------------------------------------------------------------------------------------------------------
 --
---		Hero: tusk
---		Perk: 
+--		Hero: Tusk
+--		Perk: Walrus Kick and Walrus Punch will refund their manacost when used by Tusk. 
 --
 --------------------------------------------------------------------------------------------------------
 LinkLuaModifier( "modifier_npc_dota_hero_tusk_perk", "abilities/hero_perks/npc_dota_hero_tusk_perk.lua" ,LUA_MODIFIER_MOTION_NONE )
@@ -23,3 +23,23 @@ end
 -- Add additional functions
 --------------------------------------------------------------------------------------------------------
 
+--------------------------------------------------------------------------------------------------------
+function modifier_npc_dota_hero_tusk_perk:DeclareFunctions()
+	return { MODIFIER_EVENT_ON_ABILITY_FULLY_CAST }
+end
+--------------------------------------------------------------------------------------------------------
+function modifier_npc_dota_hero_tusk_perk:OnAbilityFullyCast(keys)
+	if IsServer() then
+		if self:GetCaster() == keys.unit then
+			if keys.ability:GetName() == "tusk_walrus_kick" then
+				keys.ability:RefundManaCost()
+			elseif keys.ability:GetName() == "tusk_walrus_punch" then
+				-- GetManaCost() and RefundManaCost() do not work for Walrus Punch, so this is a loosy goosy workaround
+				self:GetCaster():GiveMana(25 + (25 * keys.ability:GetLevel()))
+			end
+		end
+	end
+
+	return true
+end
+--------------------------------------------------------------------------------------------------------
