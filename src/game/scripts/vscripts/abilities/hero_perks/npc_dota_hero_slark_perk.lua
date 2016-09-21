8f035b67f1e62d232c9358f2263271432b5359be
SwordBacon <swordofbacon@gmail.com>
2016-09-21 15:42:57 -0600

    Add Slark perk

--- src/game/scripts/vscripts/abilities/hero_perks/npc_dota_hero_slark_perk.lua
+++ src/game/scripts/vscripts/abilities/hero_perks/npc_dota_hero_slark_perk.lua
@@ -1,7 +1,7 @@
 --------------------------------------------------------------------------------------------------------
 --
---		Hero: slark
---		Perk: 
+--		Hero: Slark
+--		Perk: Slark gains +3 agility for each level put in an mobility/escape skill. 
 --
 --------------------------------------------------------------------------------------------------------
 LinkLuaModifier( "modifier_npc_dota_hero_slark_perk", "abilities/hero_perks/npc_dota_hero_slark_perk.lua" ,LUA_MODIFIER_MOTION_NONE )
@@ -20,6 +20,27 @@ function modifier_npc_dota_hero_slark_perk:IsHidden()
 	return true
 end
 --------------------------------------------------------------------------------------------------------
--- Add additional functions
+
+function modifier_npc_dota_hero_slark_perk:DeclareFunctions()
+	return {
+		MODIFIER_PROPERTY_STATS_AGILITY_BONUS,
+	}
+end
+--------------------------------------------------------------------------------------------------------
+function modifier_npc_dota_hero_slark_perk:GetModifierBonusStats_Agility(params)
+	local caster = self:GetCaster()
+
+	local agility_value = 3
+	local bonusAgility = 0
+
+	for i = 0, 15 do
+		local ability = caster:GetAbilityByIndex(i)
+		if ability and ability:HasAbilityFlag("mobility") then
+			local level = ability:GetLevel()
+			bonusAgility = bonusAgility + (level * agility_value)	
+		end
+	end
+	return bonusAgility
+end
 --------------------------------------------------------------------------------------------------------
 
