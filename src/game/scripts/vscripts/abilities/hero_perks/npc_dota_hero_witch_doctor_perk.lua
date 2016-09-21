00632668e56b4285ded7b91482ed0a7d712017a3
SwordBacon <swordofbacon@gmail.com>
2016-09-20 19:39:15 -0600

    Add WD perk
    vscript error occurs sometimes but doesn't affect mechanics.

--- src/game/scripts/vscripts/abilities/hero_perks/npc_dota_hero_witch_doctor_perk.lua
+++ src/game/scripts/vscripts/abilities/hero_perks/npc_dota_hero_witch_doctor_perk.lua
@@ -1,7 +1,7 @@
 --------------------------------------------------------------------------------------------------------
 --
---		Hero: witch_doctor
---		Perk: 
+--		Hero: Witch Doctor
+--		Perk: Increases Witch Doctor's healing effectiveness by 25%. 
 --
 --------------------------------------------------------------------------------------------------------
 LinkLuaModifier( "modifier_npc_dota_hero_witch_doctor_perk", "abilities/hero_perks/npc_dota_hero_witch_doctor_perk.lua" ,LUA_MODIFIER_MOTION_NONE )
@@ -20,6 +20,37 @@ function modifier_npc_dota_hero_witch_doctor_perk:IsHidden()
 	return true
 end
 --------------------------------------------------------------------------------------------------------
+function modifier_npc_dota_hero_witch_doctor_perk:RemoveOnDeath()
+	return false
+end
+--------------------------------------------------------------------------------------------------------
 -- Add additional functions
 --------------------------------------------------------------------------------------------------------
+function modifier_npc_dota_hero_witch_doctor_perk:DeclareFunctions()
+	return { MODIFIER_EVENT_ON_HEAL_RECEIVED }
+end
+--------------------------------------------------------------------------------------------------------
+function modifier_npc_dota_hero_witch_doctor_perk:OnHealReceived(keys)
+	if IsServer() then
+		local caster = self:GetCaster()
+		local inflictor = keys.inflictor -- Heal ability
+		local unit = keys.unit 
+		local amount = keys.gain -- Amount healed
 
+		if inflictor then
+			-- vscript error occurs saying this is nil, but it is not
+			local healer = inflictor:GetCaster()
+			if healer then
+				if healer == caster then
+					amount = amount * 0.25
+					if unit:GetHealthPercent() < 100 then
+						unit:PopupNumbers(unit, "heal", Vector(10, 255, 10), 3.0, math.floor(amount), 0, nil)
+					end
+					unit:Heal(amount, caster)
+				end
+			end
+		end	
+	end
+	return true
+end
+--------------------------------------------------------------------------------------------------------
