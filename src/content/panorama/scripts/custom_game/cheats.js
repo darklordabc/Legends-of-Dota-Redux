5829ce2edb4ff8d3a47e3ed6a02ed4fbeca63b99
lcd1232 <malexey1984@gmail.com>
2016-09-21 04:09:10 +0300

    Added new files

---
+++ src/content/panorama/scripts/custom_game/cheats.js
@@ -0,0 +1,67 @@
+"use strict";
+var cheat_list = [
+	{
+		name = 'com_lvl_up_1',
+		command = '-lvlup 1',
+	},
+	{
+		name = 'com_lvl_bots_1',
+		command = '-levelbots 1',
+	},
+	{
+		name = 'com_lvl_max',
+		command = '-lvlmax',
+	},
+	{
+		name = 'com_gold_max',
+		command = '-gold 999999',
+	},
+	{
+		name = 'com_refresh',
+		command = '-refresh',
+	},
+	{
+		name = 'com_respawn',
+		command = '-respawn',
+	},
+	{
+		name = 'com_start_game',
+		command = '-startgame',
+	},
+	{
+		name = 'com_spawn_neutrals',
+		command = '-spawnneutrals',
+	},
+	{
+		name = 'com_wtf_unwtf',
+		command = ['-wtf', '-unwtf'],
+	},
+	{
+		name = 'com_vision',
+		command = ['-allvision', '-normalvision'],
+	},
+	{
+		name = 'com_teleport',
+		command = '-teleport',
+	},
+	{
+		name = 'com_item_1',
+		command = '-item item_travel_boots_1',
+	},
+	{
+		name = 'com_item_2',
+		command = '-item item_heart',
+	},
+	{
+		name = 'com_item_3',
+		command = '-item item_radiance',
+	},
+	{
+		name = 'com_item_4',
+		command = '-item item_blink',
+	},
+];
+
+function toggleCheats(args){
+
+}
