81af22f37558d3e6b19a99b4c5742b400bd8753a
SwordBacon <swordofbacon@gmail.com>
2016-09-21 18:14:46 -0600

    Add Clinkz perk

--- src/game/scripts/vscripts/abilities/hero_perks/npc_dota_hero_clinkz_perk.lua
+++ src/game/scripts/vscripts/abilities/hero_perks/npc_dota_hero_clinkz_perk.lua
@@ -1,7 +1,7 @@
 --------------------------------------------------------------------------------------------------------
 --
---		Hero: clinkz
---		Perk: 
+--		Hero: Clinkz
+--		Perk: Clinkz will receive a free level in his first non-ultimate Autocast ability at the start of the game. 
 --
 --------------------------------------------------------------------------------------------------------
 LinkLuaModifier( "modifier_npc_dota_hero_clinkz_perk", "abilities/hero_perks/npc_dota_hero_clinkz_perk.lua" ,LUA_MODIFIER_MOTION_NONE )
@@ -22,4 +22,17 @@ end
 --------------------------------------------------------------------------------------------------------
 -- Add additional functions
 --------------------------------------------------------------------------------------------------------
-
+function modifier_npc_dota_hero_clinkz_perk:OnCreated(keys)
+	if IsServer() then
+		local caster = self:GetCaster()
+		
+		for i = 0, 15 do 
+			local ability = caster:GetAbilityByIndex(i)
+			if ability and ability:HasAbilityFlag("autocast_basic") then
+				ability:UpgradeAbility(true)
+				break
+			end
+		end
+	end
+end
+--------------------------------------------------------------------------------------------------------
