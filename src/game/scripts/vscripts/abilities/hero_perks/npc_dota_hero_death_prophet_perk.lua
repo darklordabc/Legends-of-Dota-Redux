f6c61fd4145fed3a4198d9f364d606f3fbc4e41d
Wouterz90 <wouter.zegers@gmail.com>
2016-09-22 13:55:54 +0200

    Added the silene = mute perk for dp

--- src/game/scripts/vscripts/abilities/hero_perks/npc_dota_hero_death_prophet_perk.lua
+++ src/game/scripts/vscripts/abilities/hero_perks/npc_dota_hero_death_prophet_perk.lua
@@ -1,7 +1,7 @@
 --------------------------------------------------------------------------------------------------------
 --
 --		Hero: death_prophet
---		Perk: 
+--		Perk: Any silence from Death Prophet is also a mute
 --
 --------------------------------------------------------------------------------------------------------
 LinkLuaModifier( "modifier_npc_dota_hero_death_prophet_perk", "abilities/hero_perks/npc_dota_hero_death_prophet_perk.lua" ,LUA_MODIFIER_MOTION_NONE )
@@ -22,4 +22,44 @@ end
 --------------------------------------------------------------------------------------------------------
 -- Add additional functions
 --------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------
+LinkLuaModifier( "modifier_npc_dota_hero_death_prophet_perk_mute", "abilities/hero_perks/npc_dota_hero_death_prophet_perk.lua" ,LUA_MODIFIER_MOTION_NONE )
+--------------------------------------------------------------------------------------------------------
+--    Modifier: modifier_npc_dota_hero_death_prophet_perk_mute       
+--------------------------------------------------------------------------------------------------------
+if modifier_npc_dota_hero_death_prophet_perk_mute == nil then modifier_npc_dota_hero_death_prophet_perk_mute = class({}) end
+--------------------------------------------------------------------------------------------------------
+-- Add additional functions
+--------------------------------------------------------------------------------------------------------
 
+function modifier_npc_dota_hero_death_prophet_perk_mute:CheckState()
+  local state = {
+    [MODIFIER_STATE_MUTED] = true,
+  }
+
+  return state
+end
+
+function modifier_npc_dota_hero_death_prophet_perk_mute:GetTexture()
+  return death_prophet_silence
+end
+
+function perkDeathProphet(filterTable)  --ModifierGainedFilter
+  local parent_index = filterTable["entindex_parent_const"]
+  local caster_index = filterTable["entindex_caster_const"]
+  local ability_index = filterTable["entindex_ability_const"]
+  if not parent_index or not caster_index or not ability_index then
+    return true
+  end
+  local parent = EntIndexToHScript( parent_index )
+  local caster = EntIndexToHScript( caster_index )
+  local ability = EntIndexToHScript( ability_index )
+  if ability then
+    if caster:HasModifier("modifier_npc_dota_hero_death_prophet_perk") then
+      if ability:HasAbilityFlag("silencing") then
+        local modifierDuration = filterTable["duration"]
+        parent:AddNewModifier(caster,nil,"function modifier_npc_dota_hero_death_prophet_perk_mute",{duration = modifierDuration})
+      end
+    end  
+  end  
+end
