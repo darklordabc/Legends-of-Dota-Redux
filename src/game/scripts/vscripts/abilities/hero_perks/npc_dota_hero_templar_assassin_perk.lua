161a0ce5587d88bf184ca1e0f12b07143725d78d
SwordBacon <swordofbacon@gmail.com>
2016-09-21 18:04:19 -0600

    Update TA perk
    Delay increased from 2 to 5.

--- src/game/scripts/vscripts/abilities/hero_perks/npc_dota_hero_templar_assassin_perk.lua
+++ src/game/scripts/vscripts/abilities/hero_perks/npc_dota_hero_templar_assassin_perk.lua
@@ -1,7 +1,7 @@
 --------------------------------------------------------------------------------------------------------
 --
 --      Hero: Templar Assassin
---      Perk: Templar Assassin turns invisible when not moving for 2 seconds. 
+--      Perk: Templar Assassin turns invisible when not moving for 5 seconds. 
 --
 --------------------------------------------------------------------------------------------------------
 LinkLuaModifier( "modifier_npc_dota_hero_templar_assassin_perk", "abilities/hero_perks/npc_dota_hero_templar_assassin_perk.lua" ,LUA_MODIFIER_MOTION_NONE )
@@ -24,7 +24,8 @@ end
 function modifier_npc_dota_hero_templar_assassin_perk:OnCreated(keys)
     if IsServer() then
         self.check = false
-        self:GetCaster():AddNewModifier(self:GetCaster(), self:GetAbility(), "modifier_npc_dota_hero_templar_assassin_invis_break", {Duration = 2})
+        self.invisDelay = 5
+        self:GetCaster():AddNewModifier(self:GetCaster(), self:GetAbility(), "modifier_npc_dota_hero_templar_assassin_invis_break", {Duration = self.invisDelay})
     end
     return true
 end
@@ -55,7 +56,7 @@ end
 function modifier_npc_dota_hero_templar_assassin_perk:OnUnitMoved(keys)
     if IsServer() then
         if keys.unit and self:GetCaster() == keys.unit then
-            self:GetCaster():AddNewModifier(self:GetCaster(), self:GetAbility(), "modifier_npc_dota_hero_templar_assassin_invis_break", {Duration = 2})
+            self:GetCaster():AddNewModifier(self:GetCaster(), self:GetAbility(), "modifier_npc_dota_hero_templar_assassin_invis_break", {Duration = self.invisDelay})
         end
     end
     return true
@@ -64,7 +65,7 @@ end
 function modifier_npc_dota_hero_templar_assassin_perk:OnAttack(keys)
     if IsServer() then
         if keys.attacker and self:GetCaster() == keys.attacker then
-            self:GetCaster():AddNewModifier(self:GetCaster(), self:GetAbility(), "modifier_npc_dota_hero_templar_assassin_invis_break", {Duration = 2})
+            self:GetCaster():AddNewModifier(self:GetCaster(), self:GetAbility(), "modifier_npc_dota_hero_templar_assassin_invis_break", {Duration = self.invisDelay})
         end
     end
     return true
