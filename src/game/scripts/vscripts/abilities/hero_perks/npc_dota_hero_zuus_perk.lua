585f5cdbe27a7427b98d48e86c89c27bc1796e49
SwordBacon <swordofbacon@gmail.com>
2016-09-21 19:23:20 -0600

    Add zeus perk

--- src/game/scripts/vscripts/abilities/hero_perks/npc_dota_hero_zuus_perk.lua
+++ src/game/scripts/vscripts/abilities/hero_perks/npc_dota_hero_zuus_perk.lua
@@ -1,7 +1,7 @@
 --------------------------------------------------------------------------------------------------------
 --
---		Hero: zuus
---		Perk: 
+--		Hero: Zeus
+--		Perk: Reduces the manacost of all Lightning spells by 20%. 
 --
 --------------------------------------------------------------------------------------------------------
 LinkLuaModifier( "modifier_npc_dota_hero_zuus_perk", "abilities/hero_perks/npc_dota_hero_zuus_perk.lua" ,LUA_MODIFIER_MOTION_NONE )
@@ -20,6 +20,29 @@ function modifier_npc_dota_hero_zuus_perk:IsHidden()
 	return true
 end
 --------------------------------------------------------------------------------------------------------
+function modifier_npc_dota_hero_zuus_perk:OnCreated(keys)
+	self.cooldownPercentReduction = 20
+	self.cooldownReduction = 1-(self.cooldownPercentReduction / 100)
+	return true
+end
+--------------------------------------------------------------------------------------------------------
 -- Add additional functions
 --------------------------------------------------------------------------------------------------------
-
+function modifier_npc_dota_hero_zuus_perk:DeclareFunctions()
+  local funcs = {
+	MODIFIER_EVENT_ON_ABILITY_FULLY_CAST
+  }
+  return funcs
+end
+--------------------------------------------------------------------------------------------------------
+function modifier_npc_dota_hero_zuus_perk:OnAbilityFullyCast(keys)
+  if IsServer() then
+	local hero = self:GetCaster()
+	local target = keys.target
+	local ability = keys.ability
+	if hero == keys.unit and ability and ability:HasAbilityFlag("lightning") then
+	  ability:EndCooldown()
+	  ability:StartCooldown(ability:GetCooldown(ability:GetLevel() - 1) * self.cooldownReduction)
+	end
+  end
+end
