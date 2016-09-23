d307f6b0f8c010f806a5eb9d8b963b24b54fe9ce
Wouterz90 <wouter.zegers@gmail.com>
2016-09-23 13:13:11 +0200

    Added EShaker Perk

--- src/game/scripts/vscripts/abilities/hero_perks/npc_dota_hero_earthshaker_perk.lua
+++ src/game/scripts/vscripts/abilities/hero_perks/npc_dota_hero_earthshaker_perk.lua
@@ -1,7 +1,7 @@
 --------------------------------------------------------------------------------------------------------
 --
 --		Hero: earthshaker
---		Perk: 
+--		Perk: Heals for (2)% of earthshakers max hp when using an earth spell
 --
 --------------------------------------------------------------------------------------------------------
 LinkLuaModifier( "modifier_npc_dota_hero_earthshaker_perk", "abilities/hero_perks/npc_dota_hero_earthshaker_perk.lua" ,LUA_MODIFIER_MOTION_NONE )
@@ -22,4 +22,22 @@ end
 --------------------------------------------------------------------------------------------------------
 -- Add additional functions
 --------------------------------------------------------------------------------------------------------
+function modifier_npc_dota_hero_earthshaker_perk:DeclareFunctions()
+  local funcs = {
+    MODIFIER_EVENT_ON_ABILITY_FULLY_CAST
+  }
+  return funcs
+end
 
+function modifier_npc_dota_hero_earthshaker_perk:OnAbilityFullyCast(keys)
+  local healPercent = 2
+  healPercent = 0.01 * healPercent
+
+  if IsServer() then
+    if keys.unit == self:GetParent() then
+      if keys.ability:HasAbilityFlag("earth") then
+        keys.unit:Heal(keys.caster:GetMaxHealth() * healPercent ,keys.unit)
+      end
+    end
+  end
+end
