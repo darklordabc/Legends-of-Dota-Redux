954973a442330b507b8e18563165945524888614
SwordBacon <swordofbacon@gmail.com>
2016-09-23 16:51:55 -0600

    Add Underlord perk

--- src/game/scripts/vscripts/abilities/hero_perks/npc_dota_hero_abyssal_underlord_perk.lua
+++ src/game/scripts/vscripts/abilities/hero_perks/npc_dota_hero_abyssal_underlord_perk.lua
@@ -1,7 +1,7 @@
 --------------------------------------------------------------------------------------------------------
 --
---		Hero: Abyssal Underlord
---		Perk: 
+--		Hero: Underlord
+--		Perk: Underlord gains +1 to all stats for each level put in a custom ability.
 --
 --------------------------------------------------------------------------------------------------------
 LinkLuaModifier( "modifier_npc_dota_hero_abyssal_underlord_perk", "abilities/hero_perks/npc_dota_hero_abyssal_underlord_perk.lua" ,LUA_MODIFIER_MOTION_NONE )
@@ -20,6 +20,49 @@ function modifier_npc_dota_hero_abyssal_underlord_perk:IsHidden()
 	return true
 end
 --------------------------------------------------------------------------------------------------------
+function modifier_npc_dota_hero_abyssal_underlord_perk:IsHidden()
+	return true
+end
+--------------------------------------------------------------------------------------------------------
 -- Add additional functions
+--------------------------------------------------------------------------------------------------------
+function modifier_npc_dota_hero_abyssal_underlord_perk:DeclareFunctions()
+	return { 
+	MODIFIER_PROPERTY_STATS_INTELLECT_BONUS,
+	MODIFIER_PROPERTY_STATS_AGILITY_BONUS,
+	MODIFIER_PROPERTY_STATS_STRENGTH_BONUS  
+	}
+end
+--------------------------------------------------------------------------------------------------------
+function modifier_npc_dota_hero_abyssal_underlord_perk:OnCreated()
+	if IsServer() then
+		self.bonusPerLevel = 1
+		self.bonusAmount = 0
+	end
+	return true
+end
+--------------------------------------------------------------------------------------------------------
+function modifier_npc_dota_hero_abyssal_underlord_perk:GetModifierBonusStats_Intellect(params)
+	local caster = self:GetCaster()
+	local bonusIntellect = 0
+
+	for i = 0, 15 do
+		local ability = caster:GetAbilityByIndex(i)
+		if ability and ability:HasAbilityFlag("custom") then
+			local level = ability:GetLevel()
+			bonusIntellect = bonusIntellect + (level * self.bonusPerLevel)	
+		end
+	end
+	self.bonusAmount = bonusIntellect
+	return self.bonusAmount
+end
+--------------------------------------------------------------------------------------------------------
+function modifier_npc_dota_hero_abyssal_underlord_perk:GetModifierBonusStats_Agility(params)
+	return self.bonusAmount
+end
+--------------------------------------------------------------------------------------------------------
+function modifier_npc_dota_hero_abyssal_underlord_perk:GetModifierBonusStats_Strength(params)
+	return self.bonusAmount
+end
 --------------------------------------------------------------------------------------------------------
 
