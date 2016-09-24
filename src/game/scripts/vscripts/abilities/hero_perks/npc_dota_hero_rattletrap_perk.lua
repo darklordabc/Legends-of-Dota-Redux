4b741c83cf9ffaba8455556e147a22dc9e42a5f0
SwordBacon <swordofbacon@gmail.com>
2016-09-24 12:52:43 -0600

    Update clockwerk perk
    Added modifier to display interval

--- src/game/scripts/vscripts/abilities/hero_perks/npc_dota_hero_rattletrap_perk.lua
+++ src/game/scripts/vscripts/abilities/hero_perks/npc_dota_hero_rattletrap_perk.lua
@@ -5,6 +5,7 @@
 --
 --------------------------------------------------------------------------------------------------------
 LinkLuaModifier( "modifier_npc_dota_hero_rattletrap_perk", "abilities/hero_perks/npc_dota_hero_rattletrap_perk.lua" ,LUA_MODIFIER_MOTION_NONE )
+LinkLuaModifier( "modifier_npc_dota_hero_rattletrap_flare_delay", "abilities/hero_perks/npc_dota_hero_rattletrap_perk.lua" ,LUA_MODIFIER_MOTION_NONE )
 --------------------------------------------------------------------------------------------------------
 if npc_dota_hero_rattletrap_perk == nil then npc_dota_hero_rattletrap_perk = class({}) end
 --------------------------------------------------------------------------------------------------------
@@ -25,10 +26,14 @@ end
 function modifier_npc_dota_hero_rattletrap_perk:OnCreated(keys)
     if IsServer() then
         local caster = self:GetCaster()
+
         self.flare = caster:AddAbility("rattletrap_rocket_flare_perk")
+        self.thinkInterval = 15
+
         self.flare:SetHidden(true)
         self.flare:UpgradeAbility(false)
-        self:StartIntervalThink(15)
+        self:StartIntervalThink(self.thinkInterval)
+        caster:AddNewModifier(caster, self.flare, "modifier_npc_dota_hero_rattletrap_flare_delay", {Duration = self.thinkInterval})
     end
 end
 --------------------------------------------------------------------------------------------------------
@@ -42,19 +47,26 @@ function modifier_npc_dota_hero_rattletrap_perk:OnIntervalThink(keys)
         	flare:SetLevel(rocketFlare:GetLevel() + 1)
         end
 
-        if flare then
+        if flare and caster:IsAlive() then
         	local findFowLocation = false
         	while findFowLocation == false do
 	        	local randomX = RandomInt(-7136, 7136)
 	        	local randomY = RandomInt(-7136, 7136)
 	        	local vector = Vector(randomX, randomY, 0)
-	        	local cooldown = flare:GetCooldownTimeRemaining()
-	        	flare:EndCooldown()
 	        	caster:CastAbilityOnPosition(vector, flare, caster:GetPlayerID())
+                caster:AddNewModifier(caster, flare, "modifier_npc_dota_hero_rattletrap_flare_delay", {Duration = self.thinkInterval})
 	        	findFowLocation = true
 	        end
         end
         return true
     end
+end
+--------------------------------------------------------------------------------------------------------
+--      Modifier: modifier_npc_dota_hero_rattletrap_flare_delay                
+--------------------------------------------------------------------------------------------------------
+if modifier_npc_dota_hero_rattletrap_flare_delay == nil then modifier_npc_dota_hero_rattletrap_flare_delay = class({}) end
+--------------------------------------------------------------------------------------------------------
+function modifier_npc_dota_hero_rattletrap_flare_delay:IsHidden()
+    return false
 end
 --------------------------------------------------------------------------------------------------------
