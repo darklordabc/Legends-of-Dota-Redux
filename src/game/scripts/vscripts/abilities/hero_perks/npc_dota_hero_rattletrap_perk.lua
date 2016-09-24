68e36a13f9a6403f2cd8723004b91a0b1771e5b9
SwordBacon <swordofbacon@gmail.com>
2016-09-23 19:56:32 -0600

    Add clockwork perk

--- src/game/scripts/vscripts/abilities/hero_perks/npc_dota_hero_rattletrap_perk.lua
+++ src/game/scripts/vscripts/abilities/hero_perks/npc_dota_hero_rattletrap_perk.lua
@@ -1,7 +1,7 @@
 --------------------------------------------------------------------------------------------------------
 --
---		Hero: rattletrap
---		Perk: 
+--		Hero: Clockwork
+--		Perk: Fires a random flare to any part of the map every 15 seconds. Scales with Rocket Flare.
 --
 --------------------------------------------------------------------------------------------------------
 LinkLuaModifier( "modifier_npc_dota_hero_rattletrap_perk", "abilities/hero_perks/npc_dota_hero_rattletrap_perk.lua" ,LUA_MODIFIER_MOTION_NONE )
@@ -22,4 +22,40 @@ end
 --------------------------------------------------------------------------------------------------------
 -- Add additional functions
 --------------------------------------------------------------------------------------------------------
+function modifier_npc_dota_hero_rattletrap_perk:OnCreated(keys)
+    if IsServer() then
+        local caster = self:GetCaster()
+        self.flare = caster:AddAbility("rattletrap_rocket_flare_perk")
+        self.flare:SetHidden(true)
+        self.flare:UpgradeAbility(false)
+        self:StartIntervalThink(5)
+    end
+end
+--------------------------------------------------------------------------------------------------------
+function modifier_npc_dota_hero_rattletrap_perk:OnIntervalThink(keys)
+    if IsServer() then
+        local caster = self:GetCaster()
+        local flare = self.flare
+        local rocketFlare = caster:FindAbilityByName("rattletrap_rocket_flare")
 
+        if rocketFlare then
+        	flare:SetLevel(rocketFlare:GetLevel() + 1)
+        end
+
+        if flare then
+        	local findFowLocation = false
+        	while findFowLocation == false do
+	        	local randomX = RandomInt(-7136, 7136)
+	        	local randomY = RandomInt(-7136, 7136)
+	        	local vector = Vector(randomX, randomY, 0)
+	        	local cooldown = flare:GetCooldownTimeRemaining()
+	        	flare:EndCooldown()
+	        	caster:CastAbilityOnPosition(vector, flare, caster:GetPlayerID())
+	        	findFowLocation = true
+	        	print(caster:GetAbsOrigin())
+	        end
+        end
+        return true
+    end
+end
+--------------------------------------------------------------------------------------------------------
