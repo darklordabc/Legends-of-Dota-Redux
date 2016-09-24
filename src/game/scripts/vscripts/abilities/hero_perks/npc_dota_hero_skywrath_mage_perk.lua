8674444e46b0356d168e94e9144bea5aff95f85e
SwordBacon <swordofbacon@gmail.com>
2016-09-24 13:29:04 -0600

    Add Skywrath perk

--- src/game/scripts/vscripts/abilities/hero_perks/npc_dota_hero_skywrath_mage_perk.lua
+++ src/game/scripts/vscripts/abilities/hero_perks/npc_dota_hero_skywrath_mage_perk.lua
@@ -20,6 +20,29 @@ function modifier_npc_dota_hero_skywrath_mage_perk:IsHidden()
 	return true
 end
 --------------------------------------------------------------------------------------------------------
+function modifier_npc_dota_hero_skywrath_mage_perk:OnCreated(keys)
+	self.cooldownThreshold = 7
+    self.manaPercentReduction = 15
+    self.manaReduction = self.manaPercentReduction / 100
+	return true
+end
+--------------------------------------------------------------------------------------------------------
 -- Add additional functions
 --------------------------------------------------------------------------------------------------------
-
+function modifier_npc_dota_hero_skywrath_mage_perk:DeclareFunctions()
+  local funcs = {
+    MODIFIER_EVENT_ON_ABILITY_FULLY_CAST
+  }
+  return funcs
+end
+--------------------------------------------------------------------------------------------------------
+function modifier_npc_dota_hero_skywrath_mage_perk:OnAbilityFullyCast(keys)
+  if IsServer() then
+    local hero = self:GetCaster()
+    local target = keys.target
+    local ability = keys.ability
+    if hero == keys.unit and ability and ability:GetCooldownTimeRemaining() < self.cooldownThreshold then
+      hero:GiveMana(ability:GetManaCost(ability:GetLevel() - 1) * self.manaReduction)
+    end
+  end
+end
