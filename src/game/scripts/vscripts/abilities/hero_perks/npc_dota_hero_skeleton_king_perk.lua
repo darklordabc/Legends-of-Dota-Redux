52cf98e0f0657efb200e8797256593a755f8ce39
darklordabc <curtisjdubois@hotmail.com>
2016-09-27 22:31:58 +1000

    Wraith King Perk

--- src/game/scripts/vscripts/abilities/hero_perks/npc_dota_hero_skeleton_king_perk.lua
+++ src/game/scripts/vscripts/abilities/hero_perks/npc_dota_hero_skeleton_king_perk.lua
@@ -1,7 +1,8 @@
+local Timers = require('easytimers')
 --------------------------------------------------------------------------------------------------------
 --
---		Hero: skeleton_king
---		Perk: 
+--		Hero: Wraith King
+--		Perk: Wraith King starts the game with an Aegis of the Immortal.
 --
 --------------------------------------------------------------------------------------------------------
 LinkLuaModifier( "modifier_npc_dota_hero_skeleton_king_perk", "abilities/hero_perks/npc_dota_hero_skeleton_king_perk.lua" ,LUA_MODIFIER_MOTION_NONE )
@@ -19,7 +20,21 @@ end
 function modifier_npc_dota_hero_skeleton_king_perk:IsHidden()
 	return true
 end
+
+function modifier_npc_dota_hero_skeleton_king_perk:RemoveOnDeath()
+	return false
+end
 --------------------------------------------------------------------------------------------------------
 -- Add additional functions
+function modifier_npc_dota_hero_skeleton_king_perk:OnCreated(keys)
+	if IsServer() then
+		local caster = self:GetCaster()
+		
+		Timers:CreateTimer(function()
+			caster:AddItemByName('item_aegis')
+        return
+    end, DoUniqueString('give_wk_aegis'), 1)
+	end
+end
 --------------------------------------------------------------------------------------------------------
 
