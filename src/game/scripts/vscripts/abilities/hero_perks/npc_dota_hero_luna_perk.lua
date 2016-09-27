91b14a8c5c52fd812b50decb2483b7a8a444c4db
SwordBacon <swordofbacon@gmail.com>
2016-09-27 11:28:51 -0600

    Fixed luna bug

--- src/game/scripts/vscripts/abilities/hero_perks/npc_dota_hero_luna_perk.lua
+++ src/game/scripts/vscripts/abilities/hero_perks/npc_dota_hero_luna_perk.lua
@@ -20,6 +20,10 @@ function modifier_npc_dota_hero_luna_perk:IsHidden()
 	return true
 end
 --------------------------------------------------------------------------------------------------------
+function modifier_npc_dota_hero_luna_perk:RemoveOnDeath()
+    return false
+end
+--------------------------------------------------------------------------------------------------------
 -- Add additional functions
 --------------------------------------------------------------------------------------------------------
 function modifier_npc_dota_hero_luna_perk:OnCreated(keys)
