35e2540fcae76e4ab63b97eef8208495230f0e67
darklordabc <curtisjdubois@hotmail.com>
2016-09-28 16:10:44 +1000

    Beastmaster perk, added icon, and stacks, improved logic to think

--- src/game/scripts/vscripts/abilities/hero_perks/npc_dota_hero_beastmaster_perk.lua
+++ src/game/scripts/vscripts/abilities/hero_perks/npc_dota_hero_beastmaster_perk.lua
@@ -1,14 +1,14 @@
 --------------------------------------------------------------------------------------------------------
 --
 --		Hero: Beastmaster
---		Perk: Increases Beastmaster's Strength by 3 for every level put in Neutral abilities. 
+--      Perk: Increases Beastmaster's Strength by 3 for every level put in Neutral abilities. 
 --
 --------------------------------------------------------------------------------------------------------
 LinkLuaModifier( "modifier_npc_dota_hero_beastmaster_perk", "abilities/hero_perks/npc_dota_hero_beastmaster_perk.lua" ,LUA_MODIFIER_MOTION_NONE )
 --------------------------------------------------------------------------------------------------------
 if npc_dota_hero_beastmaster_perk == nil then npc_dota_hero_beastmaster_perk = class({}) end
 --------------------------------------------------------------------------------------------------------
---		Modifier: modifier_npc_dota_hero_beastmaster_perk			
+--		Modifier: modifier_npc_dota_hero_beastmaster_perk				
 --------------------------------------------------------------------------------------------------------
 if modifier_npc_dota_hero_beastmaster_perk == nil then modifier_npc_dota_hero_beastmaster_perk = class({}) end
 --------------------------------------------------------------------------------------------------------
@@ -17,37 +17,46 @@ function modifier_npc_dota_hero_beastmaster_perk:IsPassive()
 end
 --------------------------------------------------------------------------------------------------------
 function modifier_npc_dota_hero_beastmaster_perk:IsHidden()
-	return true
+	return false
 end
 --------------------------------------------------------------------------------------------------------
 -- Add additional functions
 --------------------------------------------------------------------------------------------------------
-function modifier_npc_dota_hero_beastmaster_perk:DeclareFunctions()
-	return { 
-	MODIFIER_PROPERTY_STATS_STRENGTH_BONUS  
-	}
-end
---------------------------------------------------------------------------------------------------------
-function modifier_npc_dota_hero_beastmaster_perk:OnCreated()
-	if IsServer() then
-		self.bonusPerLevel = 3
-		self.bonusAmount = 0
-	end
-	return true
-end
---------------------------------------------------------------------------------------------------------
-function modifier_npc_dota_hero_beastmaster_perk:GetModifierBonusStats_Strength(params)
-	local caster = self:GetCaster()
-	local bonusStrength = 0
 
-	for i = 0, 15 do
-		local ability = caster:GetAbilityByIndex(i)
-		if ability and ability:GetName() ~= "npc_dota_hero_beastmaster_perk" and ability:HasAbilityFlag("neutral") then
-			local level = ability:GetLevel()
-			bonusStrength = bonusStrength + (level * self.bonusPerLevel)	
+function modifier_npc_dota_hero_beastmaster_perk:DeclareFunctions()
+	local funcs = {
+		MODIFIER_PROPERTY_STATS_STRENGTH_BONUS,
+	}
+	return funcs
+end
+
+function modifier_npc_dota_hero_beastmaster_perk:OnCreated()
+	self.bonusPerLevel = 3
+	self.bonusStrength = 1
+	if IsServer() then
+		self:StartIntervalThink(0.1)
+	end
+end
+
+function modifier_npc_dota_hero_beastmaster_perk:OnIntervalThink()
+	if IsServer() then
+		local maiden = self:GetParent()
+		for i=0, maiden:GetAbilityCount() do
+			local skill = maiden:GetAbilityByIndex(i)
+			if skill and skill:HasAbilityFlag("neutral") then
+				if not skill.beastMasterPerkLvl then skill.beastMasterPerkLvl = skill:GetLevel() end
+				if skill:GetLevel() > skill.beastMasterPerkLvl then
+					local increase = (skill:GetLevel()  - skill.beastMasterPerkLvl)
+					increase = increase * self.bonusPerLevel
+					local stacks = self:GetStackCount()
+					self:SetStackCount(stacks + increase)
+					skill.beastMasterPerkLvl = skill:GetLevel()
+				end
+			end
 		end
 	end
-	self.bonusAmount = bonusStrength
-	return self.bonusAmount
 end
---------------------------------------------------------------------------------------------------------
+
+function modifier_npc_dota_hero_beastmaster_perk:GetModifierBonusStats_Strength()
+	return self.bonusStrength * self:GetStackCount()
+end
