e8456476beea29e3dbdf5d9478c08a24461404fa
Yahnich <y.slagmeulder@hotmail.com>
2016-09-29 02:19:23 +0200

    added ld perk

--- src/game/scripts/vscripts/abilities/hero_perks/npc_dota_hero_lone_druid_perk.lua
+++ src/game/scripts/vscripts/abilities/hero_perks/npc_dota_hero_lone_druid_perk.lua
@@ -1,10 +1,12 @@
 --------------------------------------------------------------------------------------------------------
 --
---		Hero: lone_druid
---		Perk: 
+--		Hero: Lone Druid
+--		Perk: Damage is redirected to Bear if available
 --
 --------------------------------------------------------------------------------------------------------
 LinkLuaModifier( "modifier_npc_dota_hero_lone_druid_perk", "abilities/hero_perks/npc_dota_hero_lone_druid_perk.lua" ,LUA_MODIFIER_MOTION_NONE )
+
+
 --------------------------------------------------------------------------------------------------------
 if npc_dota_hero_lone_druid_perk == nil then npc_dota_hero_lone_druid_perk = class({}) end
 --------------------------------------------------------------------------------------------------------
@@ -17,9 +19,41 @@ function modifier_npc_dota_hero_lone_druid_perk:IsPassive()
 end
 --------------------------------------------------------------------------------------------------------
 function modifier_npc_dota_hero_lone_druid_perk:IsHidden()
-	return true
+	return false
 end
 --------------------------------------------------------------------------------------------------------
 -- Add additional functions
 --------------------------------------------------------------------------------------------------------
+if IsServer() then
+	function modifier_npc_dota_hero_lone_druid_perk:OnCreated()
+		self.bear = self:GetCaster():FindAbilityByName("lone_druid_spirit_bear")
+		self.damageTaken = 0.5
+		self.damageRedirect = 1 - self.damageTaken
+		self.suicide = {item_bloodstone = true,
+						techies_suicide = true}
+	end
 
+	function modifier_npc_dota_hero_lone_druid_perk:DeclareFunctions()
+		local funcs = {
+			MODIFIER_EVENT_ON_TAKEDAMAGE,
+		}
+		return funcs
+	end
+
+	function modifier_npc_dota_hero_lone_druid_perk:OnTakeDamage(params)
+		if params.unit == self:GetParent() then
+			print(self.bear)
+			if params.inflictor and self.suicide[params.inflictor:GetName()] then return end
+			if self.bear then
+				for _,bear in pairs ( Entities:FindAllByName( "npc_dota_lone_druid_bear*")) do
+					print("foundbear", bear:GetName())
+					if bear:GetOwnerEntity() == self:GetParent() and bear:IsAlive() then
+						print("redirecting")
+						self:GetParent():SetHealth( self:GetParent():GetHealth() + params.damage*self.damageTaken )
+						bear:SetHealth( bear:GetHealth() - params.damage*self.damageRedirect )
+					end
+				end
+			end
+		end
+	end
+end
