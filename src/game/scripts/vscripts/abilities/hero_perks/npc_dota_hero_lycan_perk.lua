7d67ce3b3f3def4f3f22b625b1d987c048491062
TideSofDarK <tidesmain@gmail.com>
2016-09-30 17:08:55 +0500

    Lycan perk: Increase Shapeshift duration by 20 seconds when it's night

--- src/game/scripts/vscripts/abilities/hero_perks/npc_dota_hero_lycan_perk.lua
+++ src/game/scripts/vscripts/abilities/hero_perks/npc_dota_hero_lycan_perk.lua
@@ -1,3 +1,4 @@
+local Timers = require('easytimers')
 --------------------------------------------------------------------------------------------------------
 --
 --		Hero: lycan
@@ -22,4 +23,27 @@ end
 --------------------------------------------------------------------------------------------------------
 -- Add additional functions
 --------------------------------------------------------------------------------------------------------
+function modifier_npc_dota_hero_lycan_perk:DeclareFunctions()
+  local funcs = {
+    MODIFIER_EVENT_ON_ABILITY_FULLY_CAST
+  }
+  return funcs
+end
+--------------------------------------------------------------------------------------------------------
+function modifier_npc_dota_hero_lycan_perk:OnAbilityFullyCast(keys)
+  if IsServer() then
+    local hero = self:GetCaster()
+    local target = keys.target
+    local ability = keys.ability
+    if hero == keys.unit and ability and ability:GetName() == "lycan_shapeshift" then
+    	Timers:CreateTimer(function()
+    		if hero:HasModifier("modifier_lycan_shapeshift") and GameRules:IsDaytime() == true then
+    			local shapeshift = hero:FindModifierByName("modifier_lycan_shapeshift")
+    			shapeshift:SetDuration(shapeshift:GetDuration() + 20.0, true)
+    		end
 
+	        return
+	    end, DoUniqueString('modify_lycan_ult'), 1.55)
+    end
+  end
+end
