0bb63385ce30985fe33bda7109e7642b673140be
TideSofDarK <tidesmain@gmail.com>
2016-09-30 10:43:23 +0500

    Huskar perk: 1hp/s for every 10% of health he is missing

--- src/game/scripts/vscripts/abilities/hero_perks/npc_dota_hero_huskar_perk.lua
+++ src/game/scripts/vscripts/abilities/hero_perks/npc_dota_hero_huskar_perk.lua
@@ -1,7 +1,7 @@
 --------------------------------------------------------------------------------------------------------
 --
 --		Hero: huskar
---		Perk: 
+--		Perk: Regenerates 1hp/s for every 10% of health he is missing
 --
 --------------------------------------------------------------------------------------------------------
 LinkLuaModifier( "modifier_npc_dota_hero_huskar_perk", "abilities/hero_perks/npc_dota_hero_huskar_perk.lua" ,LUA_MODIFIER_MOTION_NONE )
@@ -11,6 +11,39 @@ if npc_dota_hero_huskar_perk == nil then npc_dota_hero_huskar_perk = class({}) e
 --		Modifier: modifier_npc_dota_hero_huskar_perk				
 --------------------------------------------------------------------------------------------------------
 if modifier_npc_dota_hero_huskar_perk == nil then modifier_npc_dota_hero_huskar_perk = class({}) end
+--------------------------------------------------------------------------------------------------------
+if IsServer() then
+    function modifier_npc_dota_hero_huskar_perk:OnCreated()
+        self:StartIntervalThink(1.0)
+        self:OnIntervalThink()
+    end
+
+    function modifier_npc_dota_hero_huskar_perk:OnIntervalThink()
+        local hero = self:GetParent()
+        local maxHealth = hero:GetMaxHealth()
+        local health = hero:GetHealth()
+
+        local stacks = 10 - math.floor((health / maxHealth) * 10)
+
+        self:SetStackCount(stacks)
+    end
+end
+--------------------------------------------------------------------------------------------------------
+function modifier_npc_dota_hero_huskar_perk:DeclareFunctions()
+    local funcs = {
+        MODIFIER_PROPERTY_HEALTH_REGEN_CONSTANT
+    }
+
+    return funcs
+end
+--------------------------------------------------------------------------------------------------------
+function modifier_npc_dota_hero_huskar_perk:GetModifierConstantHealthRegen()
+    return 1 * self:GetStackCount()
+end
+--------------------------------------------------------------------------------------------------------
+function modifier_npc_dota_hero_huskar_perk:GetAttributes()
+  return MODIFIER_ATTRIBUTE_PERMANENT + MODIFIER_ATTRIBUTE_IGNORE_INVULNERABLE
+end
 --------------------------------------------------------------------------------------------------------
 function modifier_npc_dota_hero_huskar_perk:IsPassive()
 	return true
