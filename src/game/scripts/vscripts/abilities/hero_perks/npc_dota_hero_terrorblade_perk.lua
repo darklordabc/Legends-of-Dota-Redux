9d19b4fb1db5c2910734ffdd091e0583fd64d221
SwordBacon <swordofbacon@gmail.com>
2016-10-02 13:27:32 -0600

    Add Terrorblade perk

--- src/game/scripts/vscripts/abilities/hero_perks/npc_dota_hero_terrorblade_perk.lua
+++ src/game/scripts/vscripts/abilities/hero_perks/npc_dota_hero_terrorblade_perk.lua
@@ -1,7 +1,7 @@
 --------------------------------------------------------------------------------------------------------
 --
---		Hero: terrorblade
---		Perk: 
+--		Hero: Terrorblade
+--		Perk: Terrorblade Illusions deal 15% more damage, but also take 15% more damage. 
 --
 --------------------------------------------------------------------------------------------------------
 LinkLuaModifier( "modifier_npc_dota_hero_terrorblade_perk", "abilities/hero_perks/npc_dota_hero_terrorblade_perk.lua" ,LUA_MODIFIER_MOTION_NONE )
@@ -17,9 +17,36 @@ function modifier_npc_dota_hero_terrorblade_perk:IsPassive()
 end
 --------------------------------------------------------------------------------------------------------
 function modifier_npc_dota_hero_terrorblade_perk:IsHidden()
-	return true
+	return false
+end
+--------------------------------------------------------------------------------------------------------
+function modifier_npc_dota_hero_terrorblade_perk:RemoveOnDeath()
+	return false
 end
 --------------------------------------------------------------------------------------------------------
 -- Add additional functions
 --------------------------------------------------------------------------------------------------------
-
+function modifier_npc_dota_hero_terrorblade_perk:DeclareFunctions()
+    return {
+        MODIFIER_PROPERTY_DAMAGEOUTGOING_PERCENTAGE, 
+        MODIFIER_PROPERTY_INCOMING_DAMAGE_PERCENTAGE
+    }
+    -- body
+end
+--------------------------------------------------------------------------------------------------------
+function modifier_npc_dota_hero_terrorblade_perk:GetModifierDamageOutgoing_Percentage(keys)
+    if keys.attacker and keys.attacker:IsIllusion() then
+        return 15
+    else 
+        return 0
+    end
+end
+--------------------------------------------------------------------------------------------------------
+function modifier_npc_dota_hero_terrorblade_perk:GetModifierIncomingDamage_Percentage(keys)
+    if keys.attacker and keys.attacker:IsIllusion() then
+        return 15
+    else 
+        return 0
+    end
+end
+--------------------------------------------------------------------------------------------------------
