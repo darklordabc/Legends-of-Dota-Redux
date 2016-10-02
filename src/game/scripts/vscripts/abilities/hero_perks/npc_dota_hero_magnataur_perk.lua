79f864c885d561a4322d5aea697efc5ed05eb8bf
SwordBacon <swordofbacon@gmail.com>
2016-10-01 23:53:06 -0600

    Transfer disruptor perk to magnus

--- src/game/scripts/vscripts/abilities/hero_perks/npc_dota_hero_magnataur_perk.lua
+++ src/game/scripts/vscripts/abilities/hero_perks/npc_dota_hero_magnataur_perk.lua
@@ -1,7 +1,7 @@
 --------------------------------------------------------------------------------------------------------
 --
---		Hero: magnataur
---		Perk: 
+--		Hero: Magnus
+--		Perk: When Magnus casts Enemy Moving abilities, they will have 25% mana refunded and cooldowns reduced by 25%.
 --
 --------------------------------------------------------------------------------------------------------
 LinkLuaModifier( "modifier_npc_dota_hero_magnataur_perk", "abilities/hero_perks/npc_dota_hero_magnataur_perk.lua" ,LUA_MODIFIER_MOTION_NONE )
@@ -20,6 +20,31 @@ function modifier_npc_dota_hero_magnataur_perk:IsHidden()
 	return true
 end
 --------------------------------------------------------------------------------------------------------
+function modifier_npc_dota_hero_magnataur_perk:OnCreated()
+  local manaRefund = 25
+  local cooldownReduction = 25
+
+  self.manaRefund = manaRefund * 0.01
+  self.cooldownReduction = 1 - (cooldownReduction * 0.01)
+  return true
+end
+--------------------------------------------------------------------------------------------------------
 -- Add additional functions
 --------------------------------------------------------------------------------------------------------
+function modifier_npc_dota_hero_magnataur_perk:DeclareFunctions()
+  local funcs = {
+    MODIFIER_EVENT_ON_ABILITY_FULLY_CAST,
+  }
+  return funcs
+end
+--------------------------------------------------------------------------------------------------------
+function modifier_npc_dota_hero_magnataur_perk:OnAbilityFullyCast(keys)
+  if IsServer() then
+    if keys.ability:HasAbilityFlag("enemyMoving") and keys.unit == self:GetParent() then
+      keys.ability:EndCooldown()
+      keys.ability:StartCooldown(keys.ability:GetCooldown(keys.ability:GetLevel()-1)*self.cooldownReduction)
+      self:GetParent():GiveMana(keys.ability:GetManaCost(keys.ability:GetLevel()-1)*self.manaRefund)
+    end
+  end
+end
 
