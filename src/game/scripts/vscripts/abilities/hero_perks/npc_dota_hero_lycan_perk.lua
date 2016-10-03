ea1659801aa50590ce597e36305ff8e3cf1d683d
darklordabc <curtisjdubois@hotmail.com>
2016-10-03 23:34:08 +1100

    fixed lycan perk

--- src/game/scripts/vscripts/abilities/hero_perks/npc_dota_hero_lycan_perk.lua
+++ src/game/scripts/vscripts/abilities/hero_perks/npc_dota_hero_lycan_perk.lua
@@ -1,8 +1,8 @@
 local Timers = require('easytimers')
 --------------------------------------------------------------------------------------------------------
 --
---		Hero: lycan
---		Perk: 
+--		Hero: Lycan
+--		Perk: Shapeshift lasts 20 seconds longer during the night when cast by Lycan. 
 --
 --------------------------------------------------------------------------------------------------------
 LinkLuaModifier( "modifier_npc_dota_hero_lycan_perk", "abilities/hero_perks/npc_dota_hero_lycan_perk.lua" ,LUA_MODIFIER_MOTION_NONE )
@@ -18,7 +18,11 @@ function modifier_npc_dota_hero_lycan_perk:IsPassive()
 end
 --------------------------------------------------------------------------------------------------------
 function modifier_npc_dota_hero_lycan_perk:IsHidden()
-	return true
+	return false
+end
+--------------------------------------------------------------------------------------------------------
+function modifier_npc_dota_hero_lycan_perk:RemoveOnDeath()
+	return false
 end
 --------------------------------------------------------------------------------------------------------
 -- Add additional functions
@@ -37,9 +41,13 @@ function modifier_npc_dota_hero_lycan_perk:OnAbilityFullyCast(keys)
     local ability = keys.ability
     if hero == keys.unit and ability and ability:GetName() == "lycan_shapeshift" then
     	Timers:CreateTimer(function()
-    		if hero:HasModifier("modifier_lycan_shapeshift") and GameRules:IsDaytime() == true then
+    		if hero:HasModifier("modifier_lycan_shapeshift") and GameRules:IsDaytime() == false then
     			local shapeshift = hero:FindModifierByName("modifier_lycan_shapeshift")
+				local shapeshift2 = hero:FindModifierByName("modifier_lycan_shapeshift_aura")
+				local shapeshift3 = hero:FindModifierByName("modifier_lycan_shapeshift_speed")
     			shapeshift:SetDuration(shapeshift:GetDuration() + 20.0, true)
+				shapeshift2:SetDuration(shapeshift2:GetDuration() + 20.0, true)
+				shapeshift3:SetDuration(shapeshift3:GetDuration() + 20.0, true)
     		end
 
 	        return
