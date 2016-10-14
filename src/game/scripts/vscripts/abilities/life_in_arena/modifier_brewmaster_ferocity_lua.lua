095cc7ad3105dd4017c6a61d3efa39f38bd48793
TideSofDarK <tidesmain@gmail.com>
2016-10-14 21:42:19 +0500

    Ferocity fix

--- src/game/scripts/vscripts/abilities/life_in_arena/modifier_brewmaster_ferocity_lua.lua
+++ src/game/scripts/vscripts/abilities/life_in_arena/modifier_brewmaster_ferocity_lua.lua
@@ -1,5 +1,17 @@
 modifier_brewmaster_ferocity_lua = class({})
 
+function modifier_brewmaster_ferocity_lua:DeclareFunctions()
+	local funcs = {
+		MODIFIER_PROPERTY_STATS_STRENGTH_BONUS
+	}
+ 
+	return funcs
+end
+
+function modifier_brewmaster_ferocity_lua:GetModifierBonusStats_Strength()
+	return self:GetStackCount()
+end
+
 function modifier_brewmaster_ferocity_lua:IsHidden()
 	--print(self:GetStackCount())
 	if self:GetStackCount() == 0 then
@@ -31,21 +43,18 @@ function modifier_brewmaster_ferocity_lua:OnIntervalThink(event)
 
 		local units = FindUnitsInRadius(self:GetParent():GetTeam(), 
 										self:GetParent():GetAbsOrigin(), 
-										nil, self.radius, 
+										nil, self:GetAbility():GetSpecialValueFor("radius"), 
 										DOTA_UNIT_TARGET_TEAM_BOTH, 
 										DOTA_UNIT_TARGET_CREEP + DOTA_UNIT_TARGET_HERO, 
 										DOTA_UNIT_TARGET_FLAG_INVULNERABLE + DOTA_UNIT_TARGET_FLAG_MAGIC_IMMUNE_ENEMIES, 
 										FIND_ANY_ORDER, 
 										false)
-	
+
 		self.bonus_strength_per_unit = self:GetAbility():GetSpecialValueFor("bonus_strength")
 		self.bonus_strength = self.bonus_strength_per_unit * (#units-1) -- отнимаем 1 чтобы исключить самого героя	
-	
-		self:GetParent():ModifyStrength(self.bonus_strength - previous_bonus_strength) 
-		self:GetParent():CalculateStatBonus()
 
 		self:SetStackCount(self.bonus_strength)
-		--self:ForceRefresh()
-	end
 
+		return 0.2
+	end
 end
