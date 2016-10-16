3b2cfaa6447159ee3c2df917f75ac58ee6e9d57a
TideSofDarK <tidesmain@gmail.com>
2016-10-16 19:29:47 +0500

    Black Arrow fix

--- src/game/scripts/vscripts/abilities/life_in_arena/blackarrow.lua
+++ src/game/scripts/vscripts/abilities/life_in_arena/blackarrow.lua
@@ -14,12 +14,16 @@ function BlackArrow(event)
 	local caster = event.caster
 	local lifetime = event.ability:GetSpecialValueFor("lifetime")
 
-	local creep = CreateUnitByName(target:GetUnitName(), target:GetAbsOrigin(), false, caster, caster, caster:GetTeamNumber())
+	local creep = CreateUnitByName(target:GetUnitName(), target:GetAbsOrigin(), true, caster, nil, caster:GetTeamNumber())
 	creep:SetControllableByPlayer(caster:GetPlayerID(), true)
 	creep:AddNewModifier(caster, event.ability, "modifier_kill", {duration = lifetime})
 	--creep:AddNewModifier(caster, event.ability, "modifier_illusion", nil)
-	event.ability:ApplyDataDrivenModifier(caster, target, "modifier_dark_ranger_black_arrow_unit", nil)
+	-- event.ability:ApplyDataDrivenModifier(caster, creep, "modifier_dark_ranger_black_arrow_unit", nil)
+	creep:AddNewModifier(caster, event.ability, "modifier_illusion", {duration = lifetime, outgoing_damage = 50, incoming_damage = 200})
 	creep:SetRenderColor(249, 127, 127)
+	local target_level = target:GetLevel()
+    for i = 1, target_level - 1 do
+        creep:HeroLevelUp(false)
+    end
 	creep:MakeIllusion()
-	creep:SetAcquisitionRange(800)
 end
