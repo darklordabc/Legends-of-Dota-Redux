be3ccc244207958d326c126e777cb08757a6110e
darklordabc <curtisjdubois@hotmail.com>
2016-10-20 05:39:39 +1100

    tweaked thirst for life

--- src/game/scripts/vscripts/abilities/life_in_arena/modifier_lust_for_life.lua
+++ src/game/scripts/vscripts/abilities/life_in_arena/modifier_lust_for_life.lua
@@ -16,27 +16,29 @@ function modifier_lust_for_life:IsHidden()
 	return true
 end
 
-if IsServer() then
+
     function modifier_lust_for_life:OnCreated()
-        self:StartIntervalThink(1.0)
-        self:OnIntervalThink()
+			self:StartIntervalThink(1.0)
+			self:OnIntervalThink()     
     end
 
     function modifier_lust_for_life:OnIntervalThink(event)
-		local caster = self:GetParent()
-		local ability = self:GetAbility()
-		local perChunk = self:GetAbility():GetSpecialValueFor("tolltip")
-		while(caster:HasModifier("modifier_lust_for_life_health_regen")) do
-			caster:RemoveModifierByName("modifier_lust_for_life_health_regen")
-		end
-		local modifierCount = 0
-		if caster:GetIntellect() ~= 0 then
+		if IsServer() then
+			local caster = self:GetParent()
+			local ability = self:GetAbility()				
+			local perChunk = self:GetAbility():GetSpecialValueFor("tolltip")
+			while(caster:HasModifier("modifier_lust_for_life_health_regen")) do
+				caster:RemoveModifierByName("modifier_lust_for_life_health_regen")
+			end
+			
+
 			modifierCount = math.ceil((100 - caster:GetHealthPercent())/perChunk)
+
+			
+			self:SetStackCount(modifierCount)
 		end
-		
-		self:SetStackCount(modifierCount)
 	end
-end
+
 
 function modifier_lust_for_life:GetModifierHealthRegenPercentage()
 	return self:GetAbility():GetSpecialValueFor("health_regen_percent") * self:GetStackCount()
