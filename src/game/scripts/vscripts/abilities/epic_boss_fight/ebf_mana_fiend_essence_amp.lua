4d6be2af5de60e7f2c9269b7792f7dffb2f48810
Yahnich <y.slagmeulder@hotmail.com>
2016-10-25 19:38:08 +0200

    fixed issues

--- src/game/scripts/vscripts/abilities/epic_boss_fight/ebf_mana_fiend_essence_amp.lua
+++ src/game/scripts/vscripts/abilities/epic_boss_fight/ebf_mana_fiend_essence_amp.lua
@@ -3,27 +3,36 @@ function EssenceAmp(filterTable)
     local attacker_index = filterTable["entindex_attacker_const"]
     local ability_index = filterTable["entindex_inflictor_const"]
     if not victim_index or not attacker_index or not ability_index then
-        return true
+        return filterTable
     end
     local attacker = EntIndexToHScript( attacker_index )
     local victim = EntIndexToHScript( victim_index )
     local ability = EntIndexToHScript( ability_index )
+	if attacker == victim then return filterTable end
 	local amp = attacker:FindAbilityByName("ebf_mana_fiend_essence_amp")
+	print(amp and amp:GetLevel() > 0, "ampcheck")
 	if amp and amp:GetLevel() > 0 then
-		local damageMult = amp:GetSpecialValueFor("crit_amp")
+		local damageMult = amp:GetSpecialValueFor("crit_amp") / 100
 		local manaburn = ability:GetManaCost(-1) * damageMult - ability:GetManaCost(-1)
 		local perc = amp:GetSpecialValueFor("crit_chance")
-		if RollPercentage(perc) and caster:GetMana() >= manaburn then
-			filterTable["damage"] = filterTable["damage"] * damageMult
-			local particle = ParticleManager:CreateParticle("particles/essence_amp_crit.vpcf",PATTACH_POINT_FOLLOW,caster)
-			caster:EmitSound("Hero_TemplarAssassin.Meld.Attack")
-			target:ShowPopup( {
-						PostSymbol = 4,
-						Color = Vector( 125, 125, 255 ),
-						Duration = 0.7,
-						Number = filterTable["damage"],
-						pfx = "spell_custom"} )
-			caster:SpendMana(manaburn, ability)
+		if attacker:GetMana() >= manaburn then
+			attacker.essenceCritPrng = attacker.essenceCritPrng or 0
+			if RollPercentage(perc-1+attacker.essenceCritPrng) then
+				filterTable["damage"] = filterTable["damage"] * damageMult
+				local particle = ParticleManager:CreateParticle("particles/essence_amp_crit.vpcf", PATTACH_POINT_FOLLOW, attacker)
+				attacker:EmitSound("Hero_TemplarAssassin.Meld.Attack")
+				victim:ShowPopup( {
+							PostSymbol = 4,
+							Color = Vector( 125, 125, 255 ),
+							Duration = 0.7,
+							Number = filterTable["damage"],
+							pfx = "spell_custom"} )
+				attacker:SpendMana(manaburn, ability)
+				attacker.essenceCritPrng = 0
+			else
+				attacker.essenceCritPrng = attacker.essenceCritPrng + 1
+			end
 		end
 	end
+	return filterTable
  end
--- src/game/scripts/vscripts/abilities/epic_boss_fight/ebf_troll_warlord_multistrike.lua
+++ src/game/scripts/vscripts/abilities/epic_boss_fight/ebf_troll_warlord_multistrike.lua
@@ -1,3 +1,160 @@
+TIMERS_THINK = 0.01
+
+if Timers == nil then
+  print ( '[Timers] creating Timers' )
+  Timers = {}
+  Timers.__index = Timers
+end
+
+function Timers:new( o )
+  o = o or {}
+  setmetatable( o, Timers )
+  return o
+end
+
+function Timers:start()
+  Timers = self
+  self.timers = {}
+  
+  local ent = Entities:CreateByClassname("info_target") -- Entities:FindByClassname(nil, 'CWorld')
+  ent:SetThink("Think", self, "timers", TIMERS_THINK)
+end
+
+function Timers:Think()
+  if GameRules:State_Get() >= DOTA_GAMERULES_STATE_POST_GAME then
+    return
+  end
+
+  -- Track game time, since the dt passed in to think is actually wall-clock time not simulation time.
+  local now = GameRules:GetGameTime()
+
+  -- Process timers
+  for k,v in pairs(Timers.timers) do
+    local bUseGameTime = true
+    if v.useGameTime ~= nil and v.useGameTime == false then
+      bUseGameTime = false
+    end
+    local bOldStyle = false
+    if v.useOldStyle ~= nil and v.useOldStyle == true then
+      bOldStyle = true
+    end
+
+    local now = GameRules:GetGameTime()
+    if not bUseGameTime then
+      now = Time()
+    end
+
+    if v.endTime == nil then
+      v.endTime = now
+    end
+    -- Check if the timer has finished
+    if now >= v.endTime then
+      -- Remove from timers list
+      Timers.timers[k] = nil
+      
+      -- Run the callback
+      local status, nextCall = pcall(v.callback, GameRules:GetGameModeEntity(), v)
+
+      -- Make sure it worked
+      if status then
+        -- Check if it needs to loop
+        if nextCall then
+          -- Change its end time
+
+          if bOldStyle then
+            v.endTime = v.endTime + nextCall - now
+          else
+            v.endTime = v.endTime + nextCall
+          end
+
+          Timers.timers[k] = v
+        end
+
+        -- Update timer data
+        --self:UpdateTimerData()
+      else
+        -- Nope, handle the error
+        Timers:HandleEventError('Timer', k, nextCall)
+      end
+    end
+  end
+
+  return TIMERS_THINK
+end
+
+function Timers:HandleEventError(name, event, err)
+  print(err)
+
+  -- Ensure we have data
+  name = tostring(name or 'unknown')
+  event = tostring(event or 'unknown')
+  err = tostring(err or 'unknown')
+
+  -- Tell everyone there was an error
+  --Say(nil, name .. ' threw an error on event '..event, false)
+  --Say(nil, err, false)
+
+  -- Prevent loop arounds
+  if not self.errorHandled then
+    -- Store that we handled an error
+    self.errorHandled = true
+  end
+end
+
+function Timers:CreateTimer(name, args)
+  if type(name) == "function" then
+    args = {callback = name}
+    name = DoUniqueString("timer")
+  elseif type(name) == "table" then
+    args = name
+    name = DoUniqueString("timer")
+  elseif type(name) == "number" then
+    args = {endTime = name, callback = args}
+    name = DoUniqueString("timer")
+  end
+  if not args.callback then
+    print("Invalid timer created: "..name)
+    return
+  end
+
+
+  local now = GameRules:GetGameTime()
+  if args.useGameTime ~= nil and args.useGameTime == false then
+    now = Time()
+  end
+
+  if args.endTime == nil then
+    args.endTime = now
+  elseif args.useOldStyle == nil or args.useOldStyle == false then
+    args.endTime = now + args.endTime
+  end
+
+  Timers.timers[name] = args 
+
+  return name
+end
+
+function Timers:RemoveTimer(name)
+  Timers.timers[name] = nil
+end
+
+function Timers:RemoveTimers(killAll)
+  local timers = {}
+
+  if not killAll then
+    for k,v in pairs(Timers.timers) do
+      if v.persist then
+        timers[k] = v
+      end
+    end
+  end
+
+  Timers.timers = timers
+end
+
+
+if not Timers.timers then Timers:start() end
+
 function Fervor(keys)
 	local caster = keys.caster
 	local target = keys.target
@@ -26,14 +183,13 @@ function Fervor(keys)
 						ability:ApplyDataDrivenModifier(caster, caster, modifier, {duration = duration})
 						caster:SetModifierStackCount(modifier, ability, stack_count + 1)
 						if chance*(stack_count + 1) > math.random(100 - ability.prng) then
-						caster.stuntattack = true
-						caster:StartGestureWithPlaybackRate(ACT_DOTA_ATTACK, (2*caster:GetAttacksPerSecond()))
-						Timers:CreateTimer(
-                        function()
-                            caster.stuntattack = true
-							caster:PerformAttack(target, true, true, true, false, true)	
-                        end, DoUniqueString('ebf_troll_warlord_multistrike'), caster:GetAttacksPerSecond() / 2)
-						ability.prng = 0
+							caster:StartGestureWithPlaybackRate(ACT_DOTA_ATTACK, (2*caster:GetAttacksPerSecond()))
+							Timers:CreateTimer(caster:GetAttacksPerSecond() / 2,
+							function()
+								caster.stuntattack = true
+								caster:PerformAttack(target, true, true, true, false, true)	
+							end, DoUniqueString('ebf_troll_warlord_multistrike'))
+							ability.prng = 0
 						end
 					else
 						caster.stuntattack = false
@@ -44,13 +200,13 @@ function Fervor(keys)
 						ability:ApplyDataDrivenModifier(caster, caster, modifier, {duration = duration})
 						caster:SetModifierStackCount(modifier, ability, stack_count)
 						if chance*stack_count > math.random(100 - ability.prng) then
-						caster:StartGestureWithPlaybackRate(ACT_DOTA_ATTACK, (2*caster:GetAttacksPerSecond()))
-						Timers:CreateTimer(
-                        function()
-                            caster.stuntattack = true
-							caster:PerformAttack(target, true, true, true, false, true)	
-                        end, DoUniqueString('ebf_troll_warlord_multistrike'), caster:GetAttacksPerSecond() / 2)
-						ability.prng = 0
+							caster:StartGestureWithPlaybackRate(ACT_DOTA_ATTACK, (2*caster:GetAttacksPerSecond()))
+							Timers:CreateTimer(caster:GetAttacksPerSecond() / 2,
+							function()
+								caster.stuntattack = true
+								caster:PerformAttack(target, true, true, true, false, true)	
+							end, DoUniqueString('ebf_troll_warlord_multistrike'))
+							ability.prng = 0
 						end
 					else
 						caster.stuntattack = false
