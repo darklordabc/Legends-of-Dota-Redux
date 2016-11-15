d5da97766474c8315e259e3c91064506733a2c49
Yahnich <y.slagmeulder@hotmail.com>
2016-11-15 15:03:30 +0100

    Update angel_arena_archmage_anomaly.lua

--- src/game/scripts/vscripts/abilities/angel_arena_reborn/angel_arena_archmage_anomaly.lua
+++ src/game/scripts/vscripts/abilities/angel_arena_reborn/angel_arena_archmage_anomaly.lua
@@ -23,6 +23,9 @@ function modifier_angel_arena_archmage_anomaly_thinker:OnCreated( kv )
 		self.FXIndex = ParticleManager:CreateParticle( "particles/econ/items/faceless_void/faceless_void_mace_of_aeons/fv_chronosphere_aeons.vpcf", PATTACH_ABSORIGIN_FOLLOW, self:GetParent() )
 		ParticleManager:SetParticleControl( self.FXIndex, 0, self:GetParent():GetOrigin() )
 		ParticleManager:SetParticleControl( self.FXIndex, 1, Vector( self.aura_radius, 0, 0 ) )
+		ParticleManager:SetParticleControl( self.FXIndex, 4, self:GetParent():GetOrigin() )
+		ParticleManager:SetParticleControl( self.FXIndex, 6, self:GetParent():GetOrigin() )
+		ParticleManager:SetParticleControl( self.FXIndex, 10, self:GetParent():GetOrigin() )
 	end
 	self.AlreadyHit = {}
 	EmitSoundOn("Hero_Warlock.ShadowWord", self:GetParent())
