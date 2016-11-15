06c2b3421096dd928e1f4f9f8c57c7d0a7efc041
SwordBacon <swordofbacon@gmail.com>
2016-11-15 11:21:11 -0700

    Hide perk buff when on cooldown.

--- src/game/scripts/vscripts/abilities/hero_perks/npc_dota_hero_phoenix_perk.lua
+++ src/game/scripts/vscripts/abilities/hero_perks/npc_dota_hero_phoenix_perk.lua
@@ -18,7 +18,7 @@ function modifier_npc_dota_hero_phoenix_perk:IsPassive()
 end
 --------------------------------------------------------------------------------------------------------
 function modifier_npc_dota_hero_phoenix_perk:IsHidden()
-    return false
+    return self:GetCaster():HasModifier("modifier_npc_dota_hero_phoenix_perk_delay")
 end
 --------------------------------------------------------------------------------------------------------
 function modifier_npc_dota_hero_phoenix_perk:IsPurgable()
