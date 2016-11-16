f27745220fb7775ec43fed0db45c8aba11173f89
K1llMan <k1llmanws@yandex.ru>
2016-11-16 19:26:33 +0300

    Fix animation on hero screen

--- src/content/panorama/scripts/custom_game/game_setup/game_setup_hero.js
+++ src/content/panorama/scripts/custom_game/game_setup/game_setup_hero.js
@@ -3,10 +3,23 @@
 function setHeroName( name ) {
     $.GetContextPanel().SetAttributeString('heroName', name);
     $('#heroImage').heroname = name;
-    $('#heroMovie').heroname = name;
     $('#heroName').text = $.Localize(name);
 }
 
 (function() {
     $.GetContextPanel().setHeroName = setHeroName;
+	$.GetContextPanel().SetPanelEvent('onmouseover', function(){
+		if ($('#heroMovie') != null)
+			return;
+
+		var movie = $.CreatePanel( 'DOTAHeroMovie', $.GetContextPanel(), 'heroMovie' );
+		$.GetContextPanel().MoveChildBefore(movie, $('#heroImage'));
+    	$('#heroMovie').heroname = $.GetContextPanel().GetAttributeString('heroName', '');
+	});
+
+	$.GetContextPanel().SetPanelEvent('onmouseout', function(){
+		if ($('#heroMovie') == null)
+			return;		
+		$('#heroMovie').DeleteAsync(0);
+	});
 })();
