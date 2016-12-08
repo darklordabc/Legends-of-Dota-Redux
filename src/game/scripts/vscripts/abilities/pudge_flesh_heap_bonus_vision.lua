eb19b004a65c51ca91a6d88068cabcebb626e3d4
Wouterz90 <wouter.zegers@gmail.com>
2016-12-08 11:38:00 +0100

    Create pudge_flesh_heap_bonus_vision.lua

---
+++ src/game/scripts/vscripts/abilities/pudge_flesh_heap_bonus_vision.lua
@@ -0,0 +1,127 @@
+pudge_flesh_heap_bonus_vision = class({})
+
+LinkLuaModifier( "modifier_flesh_heap_bonus_vision", "abilities/pudge_flesh_heap_bonus_vision.lua" ,LUA_MODIFIER_MOTION_NONE )
+
+
+function pudge_flesh_heap_bonus_vision:GetIntrinsicModifierName()
+	return "modifier_flesh_heap_bonus_vision"
+end
+
+--------------------------------------------------------------------------------
+
+function pudge_flesh_heap_bonus_vision:OnHeroDiedNearby( hVictim, hKiller, kv )
+	if hVictim == nil or hKiller == nil then
+		return	
+	end
+	if hVictim:IsIllusion() then
+		return
+	end
+
+	if hVictim:GetTeamNumber() ~= self:GetCaster():GetTeamNumber() and self:GetCaster():IsAlive() then
+		self.fleshHeapRange = self:GetLevelSpecialValueFor( "flesh_heap_range", 0 )
+		local vToCaster = self:GetCaster():GetOrigin() - hVictim:GetOrigin()
+		local flDistance = vToCaster:Length2D() - (self:GetCaster():GetCollisionPadding() + hVictim:GetCollisionPadding())
+		if hKiller == self:GetCaster() or self.fleshHeapRange >= flDistance then
+			if self.nKills == nil then
+				self.nKills = 0
+			end
+
+			self.nKills = self.nKills + 1
+
+			local hBuff = self:GetCaster():FindModifierByName( "modifier_flesh_heap_bonus_vision" )
+			if hBuff ~= nil then
+				hBuff:SetStackCount( self.nKills )
+				self:GetCaster():CalculateStatBonus()
+			else
+				self:GetCaster():AddNewModifier( self:GetCaster(), self,  "modifier_flesh_heap_attack_range" , {} )
+			end
+
+			local nFXIndex = ParticleManager:CreateParticle( "particles/units/heroes/hero_pudge/pudge_fleshheap_count.vpcf", PATTACH_OVERHEAD_FOLLOW, self:GetCaster() )
+			ParticleManager:SetParticleControl( nFXIndex, 1, Vector( 1, 0, 0 ) )
+			ParticleManager:ReleaseParticleIndex( nFXIndex )
+		end
+	end
+end
+
+--------------------------------------------------------------------------------
+
+function pudge_flesh_heap_bonus_vision:GetFleshHeapKills()
+	if self.nKills == nil then
+		self.nKills = 0
+	end
+	return self.nKills
+end
+ 
+--------------------------------------------------------------------------------
+
+--Taken from the spelllibrary, credits go to valve
+
+modifier_flesh_heap_bonus_vision = class({})
+
+
+--------------------------------------------------------------------------------
+
+function modifier_flesh_heap_bonus_vision:IsHidden()
+    if self:GetAbility():GetLevel() == 0 then
+        return true
+    end
+    return false
+end
+
+--------------------------------------------------------------------------------
+
+function modifier_flesh_heap_bonus_vision:RemoveOnDeath()
+    return false
+end
+
+--------------------------------------------------------------------------------
+
+function modifier_flesh_heap_bonus_vision:OnCreated( kv )
+	if not self:GetAbility() then
+		self:GetParent():RemoveModifierByName("modifier_flesh_heap_bonus_vision")
+		self:GetParent():CalculateStatBonus()
+		return
+	end
+	self.flesh_heap_bonus_vision_buff_amount = self:GetAbility():GetSpecialValueFor( "flesh_heap_bonus_vision_buff_amount" ) or 0
+	if IsServer() then
+		self:SetStackCount( self:GetAbility():GetFleshHeapKills() )
+		self:GetParent():CalculateStatBonus()
+	end
+end
+
+--------------------------------------------------------------------------------
+
+function modifier_flesh_heap_bonus_vision:OnRefresh( kv )
+	if not self:GetAbility() then
+		self:GetParent():RemoveModifierByName("modifier_flesh_heap_bonus_vision")
+		self:GetParent():CalculateStatBonus()
+		return
+	end
+	self.flesh_heap_bonus_vision_buff_amount = self:GetAbility():GetSpecialValueFor( "flesh_heap_bonus_vision_buff_amount" ) or 0
+	if IsServer() then
+		self:SetStackCount( self:GetAbility():GetFleshHeapKills() )
+		self:GetParent():CalculateStatBonus()
+	end
+end
+
+--------------------------------------------------------------------------------
+
+function modifier_flesh_heap_bonus_vision:DeclareFunctions()
+	local funcs = {
+		MODIFIER_PROPERTY_BONUS_DAY_VISION,
+    MODIFIER_PROPERTY_BONUS_NIGHT_VISION,
+	}
+
+	return funcs
+end
+
+--------------------------------------------------------------------------------
+
+
+function modifier_flesh_heap_bonus_vision:	GetBonusDayVision( params )
+		return self:GetStackCount() * self.flesh_heap_bonus_vision_buff_amount
+end
+
+function modifier_flesh_heap_bonus_vision:	GetBonusNightVision( params )
+		return self:GetStackCount() * self.flesh_heap_bonus_vision_buff_amount
+end
