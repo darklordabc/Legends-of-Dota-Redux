faaf27ae8c91c6d89d890453a351006af57dde28
Wouterz90 <wouter.zegers@gmail.com>
2016-12-16 13:51:01 +0100

    Remove default abilities instead of hiding

--- src/game/scripts/vscripts/skillmanager.lua
+++ src/game/scripts/vscripts/skillmanager.lua
@@ -259,7 +259,8 @@ function skillManager:RemoveAllSkills(hero)
     -- Remove all old skills
     for k,v in pairs(currentSkillList[hero]) do
         if hero:HasAbility(v) then
-            hero:FindAbilityByName(v):SetHidden(true)
+	    hero:RemoveAbility(v)
+            --hero:FindAbilityByName(v):SetHidden(true)
 		end
     end
 end
