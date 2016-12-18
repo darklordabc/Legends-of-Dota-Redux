c673c2c14f76c9a8bcfcd8e8db2f6d79b244198d
Wouterz90 <wouter.zegers@gmail.com>
2016-12-18 10:18:32 +0100

    staging error restored

--- src/game/scripts/vscripts/abilities/hero_perks/npc_dota_hero_rubick_perk.lua
+++ src/game/scripts/vscripts/abilities/hero_perks/npc_dota_hero_rubick_perk.lua
@@ -40,12 +40,6 @@ function modifier_npc_dota_hero_rubick_perk:OnCreated(keys)
             nullField:UpgradeAbility(false)
         else 
             nullField = caster:AddAbility("rubick_null_field")
-	    for i=0,10 do
-            	local abil = caster:GetAbilityByIndex(i)
-                if not abil then
-		    nullField:SetAbilityIndex(i)
-		end
-     	    end
             nullField:SetHidden(true)
             nullField:SetLevel(1)	
         end
