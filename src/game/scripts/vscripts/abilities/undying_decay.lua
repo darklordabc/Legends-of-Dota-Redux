c4f95e47147f289f71e6c202e6fba1c285ada1fd
Yahnich <y.slagmeulder@hotmail.com>
2016-12-18 04:54:09 +0100

    decays done, needs particle work but decay crashes on me

--- src/game/scripts/vscripts/abilities/undying_decay.lua
+++ src/game/scripts/vscripts/abilities/undying_decay.lua
@@ -1,22 +1,44 @@
 function ScepterCheck( keys )
 	local caster = keys.caster
 	local ability = keys.ability
+	local target = keys.target
 	
+	EmitSoundOn("Hero_Undying.Decay.Transfer", target)
+	EmitSoundOn("Hero_Undying.Decay.Target", target)
 	local scepterNameModifier = ""
-	if caster:HasScepter() then scepterNameModifier = "_scepter"
+	local stacks = ability:GetSpecialValueFor("agi_steal")
+	if caster:HasScepter() then 
+		scepterNameModifier = "_scepter"
+		stacks = ability:GetSpecialValueFor("agi_steal_scepter")
+	end
 	ability:ApplyDataDrivenModifier(caster, caster, keys.modifierAlly..scepterNameModifier, {duration = ability:GetSpecialValueFor("decay_duration")})
+	ability:ApplyDataDrivenModifier(caster, caster, keys.modifierCounter, {duration = ability:GetSpecialValueFor("decay_duration")})
+	caster:SetModifierStackCount(keys.modifierCounter, caster, caster:GetModifierStackCount(keys.modifierCounter, caster) + stacks)
 	ability:ApplyDataDrivenModifier(caster, target, keys.modifierEnemy..scepterNameModifier, {duration = ability:GetSpecialValueFor("decay_duration")})
-	caster:SetModelScale( caster:GetModelScale()*(1+ability:GetSpecialValueFor("stat_scale_up")/100) )
 	local decayLink = ParticleManager:CreateParticle(keys.particleLink, PATTACH_POINT_FOLLOW, target)
-			ParticleManager:SetParticleControlEnt(goldFountain, 0, target, PATTACH_POINT_FOLLOW, "attach_hitloc", target:GetAbsOrigin(), true)
-			ParticleManager:SetParticleControlEnt(goldFountain, 1, caster, PATTACH_POINT_FOLLOW, "attach_hitloc", caster:GetAbsOrigin(), true)
-			ParticleManager:SetParticleControlEnt(goldFountain, 3, caster, PATTACH_POINT_FOLLOW, "attach_hitloc", caster:GetAbsOrigin(), true)
-	-- local decayRadius =
+			ParticleManager:SetParticleControlEnt(decayLink, 0, target, PATTACH_POINT_FOLLOW, "attach_hitloc", target:GetAbsOrigin(), true)
+			ParticleManager:SetParticleControlEnt(decayLink, 1, caster, PATTACH_POINT_FOLLOW, "attach_hitloc", caster:GetAbsOrigin(), true)
+			ParticleManager:SetParticleControlEnt(decayLink, 3, caster, PATTACH_POINT_FOLLOW, "attach_hitloc", (caster:GetAbsOrigin() + target:GetAbsOrigin())/2, true)
 end
 
 function DecreaseModelSize( keys )
 	local caster = keys.caster
 	local ability = keys.ability
 
-	caster:SetModelScale( caster:GetModelScale()/(1+ability:GetSpecialValueFor("stat_scale_up")/100) )
+	caster:SetModifierStackCount(keys.modifierCounter, caster, caster:GetModifierStackCount(keys.modifierCounter, caster) - keys.stacks)
+	if caster:GetModifierStackCount(keys.modifierCounter, caster) >= 0 then
+		caster:RemoveModifierByName(keys.modifierCounter)
+	end
 end 
+
+function ApplyParticles(keys)
+	local caster = keys.caster
+	local ability = keys.ability
+	local target = keys.target_points[1]
+	local radius = ability:GetSpecialValueFor("radius")
+	
+	local decayRadius = ParticleManager:CreateParticle(keys.particleRadius, PATTACH_POINT, caster)
+			ParticleManager:SetParticleControl(decayRadius, 0, target)
+			ParticleManager:SetParticleControl(decayRadius, 1, Vector(radius, radius, radius))
+			ParticleManager:SetParticleControl(decayRadius, 2, target)
+end
