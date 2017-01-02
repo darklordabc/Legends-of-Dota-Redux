f3d3a43c6508738cd96b0487e1d4d4392f7b605c
Wouterz90 <wouter.zegers@gmail.com>
2017-01-02 16:37:29 +0100

    syntax error fix
    im an idiot

--- src/game/scripts/vscripts/abilities/skywrath_concussive_break.lua
+++ src/game/scripts/vscripts/abilities/skywrath_concussive_break.lua
@@ -55,31 +55,12 @@ function skywrath_mage_concussive_break:OnSpellStart()
         iVisionTeamNumber = caster:GetTeamNumber(),
         iSourceAttachment = DOTA_PROJECTILE_ATTACHMENT_ATTACK_1
       }
-    caster:EmitSound("Hero_SkywrathMage.ConcussiveShot.Cast")
     ProjectileManager:CreateTrackingProjectile( projTable )
 
-    -- 2nd target for scepter
-    if caster:HasScepter() then
-      local unitsScepter = FindUnitsInRadius(caster:GetTeamNumber(), v:GetAbsOrigin(), caster, 700, targetTeam, targetType, targetFlag, FIND_ANY_ORDER, false)
-      for _,unit in pairs(unitsScepter) do
-        if #unitsScepter > 0 then
-          local projTable = {
-            EffectName = "particles/skywrath_mage_concussive_break/skywrath_mage_concussive_break.vpcf",
-            Ability = ability,
-            Target = unit,
-            Source = caster,
-            bDodgeable = false,
-            bProvidesVision = true,
-            vSpawnOrigin = caster:GetAbsOrigin(),
-            iMoveSpeed = speed,
-            iVisionRadius = radius,
-            iVisionTeamNumber = caster:GetTeamNumber(),
-            iSourceAttachment = DOTA_PROJECTILE_ATTACHMENT_ATTACK_1
-          }
-          ProjectileManager:CreateTrackingProjectile( projTable )
-          break
-        else 
-          unitsScepter = FindUnitsInRadius(caster:GetTeamNumber(), v:GetAbsOrigin(), caster, 700, targetTeam, DOTA_UNIT_TARGET_BASIC, targetFlag, FIND_ANY_ORDER, false)
+      -- 2nd target for scepter
+      if caster:HasScepter() then
+        local unitsScepter = FindUnitsInRadius(caster:GetTeamNumber(), v:GetAbsOrigin(), caster, 700, targetTeam, targetType, targetFlag, FIND_ANY_ORDER, false)
+        for _,unit in pairs(unitsScepter) do
           if #unitsScepter > 0 then
             local projTable = {
               EffectName = "particles/skywrath_mage_concussive_break/skywrath_mage_concussive_break.vpcf",
@@ -95,11 +76,30 @@ function skywrath_mage_concussive_break:OnSpellStart()
               iSourceAttachment = DOTA_PROJECTILE_ATTACHMENT_ATTACK_1
             }
             ProjectileManager:CreateTrackingProjectile( projTable )
+            return
+          else 
+            unitsScepter = FindUnitsInRadius(caster:GetTeamNumber(), v:GetAbsOrigin(), caster, 700, targetTeam, DOTA_UNIT_TARGET_BASIC, targetFlag, FIND_ANY_ORDER, false)
+            if #unitsScepter > 0 then
+              local projTable = {
+                EffectName = "particles/skywrath_mage_concussive_break/skywrath_mage_concussive_break.vpcf",
+                Ability = ability,
+                Target = unit,
+                Source = caster,
+                bDodgeable = false,
+                bProvidesVision = true,
+                vSpawnOrigin = caster:GetAbsOrigin(),
+                iMoveSpeed = speed,
+                iVisionRadius = radius,
+                iVisionTeamNumber = caster:GetTeamNumber(),
+                iSourceAttachment = DOTA_PROJECTILE_ATTACHMENT_ATTACK_1
+              }
+              ProjectileManager:CreateTrackingProjectile( projTable )
+            end
+            return
           end
-          break
         end
+      break
       end
-    break
     end
   end
 end
