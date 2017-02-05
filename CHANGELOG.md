b72f760f003b45df77d06585dca645ca0ee793a0
darklordabc <curtisjdubois@hotmail.com>
2017-02-05 14:49:59 +1100

    add 180 inverted map (its mandatory just for the short-term, its a prank)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,7 @@ All notable changes to this project will be documented in this file.
 ### Bug Fixing/New Features
 - Fixed Jingu Mastery being unselectable
 - Fixed Bots picking invis abilities when invis ban mode 2 is choosen
+- Added new inverted map
 
 ### Balance
 - Added respawn time increaser, at 40 minutes respawn times will become minimum 50% of original respawn times. 
--- src/game/scripts/vscripts/pregame.lua
+++ src/game/scripts/vscripts/pregame.lua
@@ -322,7 +322,7 @@ function Pregame:init()
     end
 
     -- Custom -- set preset
-    if mapName == 'custom' or mapName == 'custom_bot' or mapName == 'custom_700' or mapName == '10_vs_10' then
+    if mapName == 'custom' or mapName == 'custom_bot' or mapName == 'dota_180' or mapName == 'custom_700' or mapName == '10_vs_10' then
         self:setOption('lodOptionGamemode', 1)
     end
 
@@ -337,7 +337,7 @@ function Pregame:init()
     self:setOption('lodOptionBanningMaxHeroBans', 0)
 
     -- Bot match
-    if mapName == 'custom_bot' or mapName == 'custom_700' or mapName == '10_vs_10' then
+    if mapName == 'custom_bot' or mapName == 'custom_700' or mapName == 'dota_180' or mapName == '10_vs_10' then
         self.enabledBots = true
     end
 
@@ -5457,7 +5457,7 @@ function Pregame:addExtraTowers()
         ListenToGameEvent('entity_hurt', function(keys)
             -- Grab the entity that was hurt
             local ent = EntIndexToHScript(keys.entindex_killed)
-            local attacker = EntIndexToHScript( keys.entindex_attacker )
+            --local attacker = EntIndexToHScript( keys.entindex_attacker )
             
             -- Check for tower connections
             if ent:GetHealth() <= 0 and this.towerConnectors[ent] then
