e04d36ba9d3356630b87fa3bf96bb8ccad0d66b4
CarpeSwag <james@garbagnati.com>
2017-02-05 17:27:22 -0800

    Moved tips to the top right of the ingame hero builder.

--- src/content/panorama/scripts/custom_game/ingame_hero_builder.js
+++ src/content/panorama/scripts/custom_game/ingame_hero_builder.js
@@ -24,6 +24,7 @@ function showIngameBuilder() {
         // heroBuilderPanel.SetHasClass('phase_ingame', true);
         heroBuilderPanel.SetHasClass('phase_selection_selected', true);
         heroBuilderPanel.SetHasClass('phase_selection', true);
+		heroBuilderPanel.SetHasClass('ingame_menu', true);
 
         heroBuilderPanel.balanceMode = balanceMode;
         heroBuilderPanel.FindChildTraverse("balanceModeFilter").SetHasClass("balanceModeDisabled", true);
@@ -51,7 +52,7 @@ function showIngameBuilder() {
         $('#heroBuilderDisplay').visible = !$('#heroBuilderDisplay').visible;
     }
 
-    heroBuilderPanel.doActualTeamUpdate();
+    //heroBuilderPanel.doActualTeamUpdate();
 }
 
 (function() {
