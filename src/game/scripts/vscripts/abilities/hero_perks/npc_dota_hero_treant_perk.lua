58908e09a501761bde0aff8a74eba7eed4ac9173
darklordabc <curtisjdubois@hotmail.com>
2017-02-19 14:52:28 +1100

    changed Treant perk

--- src/game/scripts/vscripts/abilities/hero_perks/npc_dota_hero_treant_perk.lua
+++ src/game/scripts/vscripts/abilities/hero_perks/npc_dota_hero_treant_perk.lua
@@ -1,10 +1,11 @@
 --------------------------------------------------------------------------------------------------------
 --
 --		Hero: Treant
---		Perk: Treant self-casts Living Armor and Nature's Guise when casting them on allies.
+--		Perk: Treant receives 3 charges of living armor. 
 --
 --------------------------------------------------------------------------------------------------------
 LinkLuaModifier( "modifier_npc_dota_hero_treant_perk", "abilities/hero_perks/npc_dota_hero_treant_perk.lua" ,LUA_MODIFIER_MOTION_NONE )
+LinkLuaModifier("modifier_charges", "abilities/modifiers/modifier_charges.lua", LUA_MODIFIER_MOTION_NONE)
 --------------------------------------------------------------------------------------------------------
 if npc_dota_hero_treant_perk ~= "" then npc_dota_hero_treant_perk = class({}) end
 --------------------------------------------------------------------------------------------------------
@@ -16,6 +17,10 @@ function modifier_npc_dota_hero_treant_perk:IsPassive()
 	return true
 end
 --------------------------------------------------------------------------------------------------------
+function modifier_npc_dota_hero_treant_perk:IsPurgable()
+	return false
+end
+--------------------------------------------------------------------------------------------------------
 function modifier_npc_dota_hero_treant_perk:IsHidden()
 	return false
 end
@@ -24,12 +29,41 @@ function modifier_npc_dota_hero_treant_perk:RemoveOnDeath()
 	return false
 end
 --------------------------------------------------------------------------------------------------------
-function modifier_npc_dota_hero_treant_perk:IsPurgable()
-	return false
-end
---------------------------------------------------------------------------------------------------------
 -- Add additional functions
 --------------------------------------------------------------------------------------------------------
+function modifier_npc_dota_hero_treant_perk:OnCreated()
+	if IsServer() then
+		self.hook = self:GetCaster():FindAbilityByName("treant_living_armor")
+		if self.hook then
+			self:StartIntervalThink(0.1)
+		end
+	end
+end
+
+function modifier_npc_dota_hero_treant_perk:OnIntervalThink()
+	if not self.activated then
+		if self.hook:GetLevel() > 0 then
+			self:GetCaster():AddNewModifier(self:GetCaster(), self.hook, "modifier_charges",
+				{
+					max_count = 3,
+					start_count = 1,
+					replenish_time = self.hook:GetCooldown(-1)
+				}
+			)
+			self.activated = true
+		end
+	end
+end
+
+function modifier_npc_dota_hero_treant_perk:OnRefresh()
+	if IsServer() then
+		--self.damagecooldown = self.hook:GetCooldown(-1) -- Time before hook does damage again.
+		local modifier = self:GetParent():FindModifierByName("modifier_charges")
+		if modifier and modifier.kv.replenish_time ~= self.hook:GetCooldown(-1) then
+			modifier.kv.replenish_time = self.hook:GetCooldown(-1)
+		end
+	end
+end
 
 function modifier_npc_dota_hero_treant_perk:DeclareFunctions()
 	local funcs = {
@@ -38,19 +72,11 @@ function modifier_npc_dota_hero_treant_perk:DeclareFunctions()
 	return funcs
 end
 
-local Timers = require('easytimers')
 
 function modifier_npc_dota_hero_treant_perk:OnAbilityFullyCast(params)
-	if params.caster == self:GetParent() then
-		if params.ability:GetName() == "treant_living_armor" then
-			local armor = params.ability
-			local duration = armor:GetSpecialValueFor("duration")
-			self:GetParent():AddNewModifier(self:GetParent(), armor, "modifier_treant_living_armor", {duration = duration})
-		end
-		if params.ability:GetName() == "treant_natures_guise" then
-			local guise = params.ability
-			local duration = guise:GetSpecialValueFor("duration")
-			self:GetParent():AddNewModifier(self:GetParent(), guise, "modifier_treant_natures_guise", {duration = duration})
+	if params.unit == self:GetParent() then
+		if params.ability == self.hook then
+			self:ForceRefresh()
 		end
 	end
 end
