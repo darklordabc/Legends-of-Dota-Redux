e61c89ef0ff6b6c80b3b355d320f152dddb7d382
DrTeaSpoon <otso.ilmari.turpeinen@gmail.com>
2017-02-27 18:15:00 +0200

    ShadowShaman perk as intended.

--- src/game/scripts/vscripts/abilities/hero_perks/npc_dota_hero_shadow_shaman_perk.lua
+++ src/game/scripts/vscripts/abilities/hero_perks/npc_dota_hero_shadow_shaman_perk.lua
@@ -1,7 +1,7 @@
 --------------------------------------------------------------------------------------------------------
 --
 --    Hero: Shadow Shaman
---    Perk: Casting targeted spells have 5% chance of hexing the target.
+--    Perk: When targeted by a spell, Hex the caster for 3 seconds. Has 60 second cooldown.
 --
 --------------------------------------------------------------------------------------------------------
 LinkLuaModifier( "modifier_npc_dota_hero_shadow_shaman_perk", "abilities/hero_perks/npc_dota_hero_shadow_shaman_perk.lua" ,LUA_MODIFIER_MOTION_NONE )
@@ -30,13 +30,16 @@ end
 --------------------------------------------------------------------------------------------------------
 function modifier_npc_dota_hero_shadow_shaman_perk:OnCreated()
   if IsServer() then
---    local cooldownPercentReduction = 20
-    local hexChance = 5
---    self.cooldownReduction = 1 - (cooldownPercentReduction / 100)
-    self.hexChance = 1 - (hexChance / 100)
-    self.aoeEnabled = false;
+    self.cooldownTime = 60
+    self.hexDuration = 3
+
+    self.cooldownReady = true
   end
   return true
+end
+
+function modifier_npc_dota_hero_shadow_shaman_perk:DestroyOnExpire ()
+  return false
 end
 --------------------------------------------------------------------------------------------------------
 -- Add additional functions
@@ -44,62 +47,31 @@ end
 
 function modifier_npc_dota_hero_shadow_shaman_perk:DeclareFunctions()
   local funcs = {
-  --  MODIFIER_EVENT_ON_ABILITY_FULLY_CAST,
-    MODIFIER_EVENT_ON_ABILITY_EXECUTED
+    MODIFIER_PROPERTY_ABSORB_SPELL
   }
   return funcs
 end
-function modifier_npc_dota_hero_shadow_shaman_perk:OnAbilityExecuted(keys)
+
+function modifier_npc_dota_hero_shadow_shaman_perk:GetAbsorbSpell(keys)
   if IsServer() then
-    local hero = self:GetCaster()
-    local target = keys.target
-    local ability = keys.ability
-
-    if hero == keys.unit and ability and not ability:HasAbilityFlag("hex") then
-      if self.aoeEnabled and bit.band(ability:GetBehavior(),DOTA_ABILITY_BEHAVIOR_AOE) == DOTA_ABILITY_BEHAVIOR_AOE then
-        local aoe_flags = DOTA_UNIT_TARGET_ALL
-        local radius = 150
-        if ability.GetAOERadius then radius = ability:GetAOERadius() end
-        local teamNumber = hero:GetTeamNumber()
-      --  local position = keys.point
-        local tTargets = FindUnitsInRadius(teamNumber, ability:GetCursorPosition(), nil, radius, DOTA_UNIT_TARGET_TEAM_ENEMY, aoe_flags, DOTA_UNIT_TARGET_FLAG_NONE, FIND_ANY_ORDER,false)
-        if #tTargets > 0 then
-          for i = 1, #tTargets do
-            self:HexTarget(tTargets[i],ability)
-          end
-        end
-      elseif target and target:GetTeamNumber() ~= hero:GetTeamNumber() and math.random() > self.hexChance then
-        target:AddNewModifier(hero, ability, "modifier_shadow_shaman_voodoo", {duration = 3})
-      end
-      --[[
-
-      local cooldown = ability:GetCooldownTimeRemaining() * self.cooldownReduction
-      ability:RefundManaCost()
-      ability:EndCooldown()
-      ability:StartCooldown(cooldown)
-      ]]--
-
+    if self.cooldownReady and keys.caster:GetTeamNumber() ~= self:GetParent():GetTeamNumber() then
+      self:HexCaster(keys.caster,keys.ability)
     end
   end
+  return false
 end
 
-function modifier_npc_dota_hero_shadow_shaman_perk:HexTarget(target,ability)
+function modifier_npc_dota_hero_shadow_shaman_perk:HexCaster (target,ability)
+  target:AddNewModifier(self:GetParent(),ability,"modifier_shadow_shaman_voodoo",{duration = self.hexDuration})
+  self:SetDuration(self.cooldownTime, true)
+  self:StartIntervalThink(self.cooldownTime)
+  self.cooldownReady = false
+end
+
+function modifier_npc_dota_hero_shadow_shaman_perk:OnIntervalThink ()
   if IsServer() then
-    target:AddNewModifier(self:GetParent(), ability, "modifier_shadow_shaman_voodoo", {duration = 3})
+    self.cooldownReady = true
+    self:SetDuration(-1,true)
+    self:StartIntervalThink(-1)
   end
 end
---[[
-function modifier_npc_dota_hero_shadow_shaman_perk:OnAbilityFullyCast(keys)
-  if IsServer() then
-    local hero = self:GetCaster()
-    local target = keys.target
-    local ability = keys.ability
-    if hero == keys.unit and ability and ability:HasAbilityFlag("hex") then
-      local cooldown = ability:GetCooldownTimeRemaining() * self.cooldownReduction
-      ability:RefundManaCost()
-      ability:EndCooldown()
-      ability:StartCooldown(cooldown)
-    end
-  end
-end
-]]--
