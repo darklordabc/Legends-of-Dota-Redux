29b45b47c9e2c52c67436fb0ac6f691eb58e2ec1
Wouter <wouter.zegers@gmail.com>
2017-02-27 09:37:11 +0100

    Created the abilities.
    Still needs tooltips,icons and them to be set as level 1 abilities.

---
+++ src/game/scripts/vscripts/abilities/keeper_of_the_light_innate.lua
@@ -0,0 +1,33 @@
+keeper_of_the_light_innate_redux = class({})
+LinkLuaModifier("modifier_keeper_of_the_light_innate_redux","abilities/keeper_of_the_light_innate.lua",LUA_MODIFIER_MOTION_NONE)
+modifier_keeper_of_the_light_innate_redux = class({})
+
+function keeper_of_the_light_innate_redux:GetIntrinsicModifierName()
+  return "modifier_keeper_of_the_light_innate_redux"
+end
+
+function modifier_keeper_of_the_light_innate_redux:IsPassive() 
+  return true
+end
+
+function modifier_keeper_of_the_light_innate_redux:IsPurgable()
+  return false
+end
+
+function modifier_keeper_of_the_light_innate_redux:RemoveOnDeath()
+  return false
+end
+
+function modifier_keeper_of_the_light_innate_redux:OnCreated()
+  if IsServer() then
+    self:StartIntervalThink(1/32)
+  end
+end
+
+function modifier_keeper_of_the_light_innate_redux:OnIntervalThink()
+  local caster = self:GetParent()
+  local ability = self:GetAbility()
+  if caster:HasScepter() and GameRules:IsDaytime() then
+    AddFOWViewer(caster:GetTeamNumber(),caster:GetAbsOrigin(),ability:GetSpecialValueFor("vision_radius"),2/32,false)
+  end
+end
---
+++ src/game/scripts/vscripts/abilities/night_stalker_innate.lua
@@ -0,0 +1,33 @@
+night_stalker_innate_redux = class({})
+LinkLuaModifier("modifier_night_stalker_innate_redux","abilities/night_stalker_innate.lua",LUA_MODIFIER_MOTION_NONE)
+modifier_night_stalker_innate_redux = class({})
+
+function night_stalker_innate_redux:GetIntrinsicModifierName()
+  return "modifier_night_stalker_innate_redux"
+end
+
+function modifier_night_stalker_innate_redux:IsPassive() 
+  return true
+end
+
+function modifier_night_stalker_innate_redux:IsPurgable()
+  return false
+end
+
+function modifier_night_stalker_innate_redux:RemoveOnDeath()
+  return false
+end
+
+function modifier_night_stalker_innate_redux:OnCreated()
+  if IsServer() then
+    self:StartIntervalThink(1/32)
+  end
+end
+
+function modifier_night_stalker_innate_redux:OnIntervalThink()
+  local caster = self:GetParent()
+  local ability = self:GetAbility()
+  if caster:HasScepter() and not GameRules:IsDaytime() then
+    AddFOWViewer(caster:GetTeamNumber(),caster:GetAbsOrigin(),ability:GetSpecialValueFor("vision_radius"),2/32,false)
+  end
+end
