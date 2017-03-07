4cc22849f5c7e7884fb7eda9ad6f5f46537a8c60
darklordabc <curtisjdubois@hotmail.com>
2017-03-07 17:18:26 +1100

    Revert "added some test chat commands to see if it works"
    This reverts commit f14f81bd0ee5c9363f60ef7bf97f7b9fa19aab4c.

--- src/game/scripts/vscripts/pregame.lua
+++ src/game/scripts/vscripts/pregame.lua
@@ -37,8 +37,6 @@ local challenge = require('challenge')
 local ingame = require('ingame')
 local localStorage = require("ModDotaLib.LocalStorage")
 require('lib/wearables')
-require('split')
-require('chatcommand')
 
 -- This should alone be used if duels are on.
 --require('lib/util_aar')
@@ -60,12 +58,6 @@ LinkLuaModifier("modifier_neutral_power", "abilities/modifiers/modifier_neutral_
 
 local Pregame = class({})
 local buildBackups = {}
-
-ChatCommand:LinkCommand("-test", "MyFunction")
-
-function MyFunction(keys)
-    print("hello world")
-end
 
 -- Init pregame stuff
 function Pregame:init()
--- src/game/scripts/vscripts/split.lua
+++
@@ -1,7 +0,0 @@
-function split(s, delimiter)
-    result = {};
-    for match in (s..delimiter):gmatch("(.-)"..delimiter) do
-        table.insert(result, match);
-    end
-    return result;
-end
