f18ffcf06b1977cbcfe3bc01b38a3c62bf8a4c39
ark120202 <ark120202@gmail.com>
2017-05-14 22:30:24 +0500

    Added sorting by creation date to skill builds

--- src/content/panorama/scripts/custom_game/stats_client.js
+++ src/content/panorama/scripts/custom_game/stats_client.js
@@ -1,12 +1,12 @@
-var ServerAddress = (Game.IsInToolsMode() && false ? "http://127.0.0.1:3333" : "https://lodr-ark120202.rhcloud.com") + "/lodServer/"
+var ServerAddress = (Game.IsInToolsMode() && false ? 'http://127.0.0.1:3333' : 'https://lodr-ark120202.rhcloud.com') + '/lodServer/';
 
 function GetDataFromServer(path, params, resolve, reject) {
-	var encodedParams = params == null ? "" : "?" + Object.keys(params).map(function(key) {
-	    return encodeURIComponent(key) + "=" + encodeURIComponent(params[key]);
-	}).join("&");
+	var encodedParams = params == null ? '' : '?' + Object.keys(params).map(function(key) {
+	    return encodeURIComponent(key) + '=' + encodeURIComponent(params[key]);
+	}).join('&');
 	//return new Promise(function(resolve, reject) {
 	$.AsyncWebRequest(ServerAddress + path + encodedParams, {
-		type: "GET",
+		type: 'GET',
 		success: function(data) {
 			if (resolve) resolve(data || {});
 		},
@@ -18,46 +18,44 @@ function GetDataFromServer(path, params, resolve, reject) {
 }
 
 function CreateSkillBuild(title, description) {
-	GameEvents.SendCustomGameEventToServer("stats_client_create_skill_build", {
+	GameEvents.SendCustomGameEventToServer('stats_client_create_skill_build', {
 		title: title,
 		description: description
-	})
-}
-
-function LoadBuilds(skip) {
-	var req = {steamID: Game.GetLocalPlayerInfo().player_steamid};
-	if (skip) req.skip = skip;
-	GetDataFromServer("getSkillBuilds", req, function(builds) {
-		if (builds && builds.length > 0) {
-			for (var i = 0; i < builds.length; i++) {
-				addRecommendedBuild(builds[i]);
-			}
-
-			LoadFavBuilds();
-		}
-
-		$("#buildLoadingIndicator").visible = false;
-		$("#pickingPhaseRecommendedBuildContainer").GetParent().visible = true;
-	}, function() {
-		$("#buildLoadingSpinner").visible = false;
-		$("#buildLoadingIndicatorText").text = $.Localize("#unableLoadingBuilds");
 	});
 }
 
-function SaveFavBuilds(builds) {
-	GameEvents.SendCustomGameEventToServer("stats_client_save_fav_builds", {builds: builds})
+function LoadBuilds(cont, skip) {
+	cont = cont || $pickingPhaseRecommendedBuildContainer()
+	var req = {steamID: Game.GetLocalPlayerInfo().player_steamid};
+	if (skip) req.skip = skip;
+	if (cont) req.sorting = cont[1];
+	GetDataFromServer('getSkillBuilds', req, function(builds) {
+		if (builds && builds.length > 0) {
+			for (var i = 0; i < builds.length; i++) {
+				addRecommendedBuild(cont[0], builds[i]);
+			}
+
+			LoadFavBuilds(cont[0]);
+		}
+
+		$('#buildLoadingIndicator').visible = false;
+		cont[0].GetParent().visible = true;
+	}, function() {
+		$('#buildLoadingSpinner').visible = false;
+		$('#buildLoadingIndicatorText').text = $.Localize('#unableLoadingBuilds');
+	});
 }
 
-function LoadFavBuilds() {
+function LoadFavBuilds(rootPanel) {
 	if (!Game.GetLocalPlayerInfo()) {
-		$.Schedule(0.1, LoadFavBuilds)
+		$.Schedule(0.1, LoadFavBuilds);
 	} else {
-		GetDataFromServer("getPlayerData", {steamID: Game.GetLocalPlayerInfo().player_steamid}, function(data) {
-			var con = $("#pickingPhaseRecommendedBuildContainer");
-			var favoriteBuilds = Object.keys(data.favoriteBuilds || {}).map(function (key) { return data.favoriteBuilds[key]; });
+		GetDataFromServer('getPlayerData', {steamID: Game.GetLocalPlayerInfo().player_steamid}, function(data) {
+			var con = rootPanel;
+			var favoriteBuilds = Object.keys(data.favoriteBuilds || {}).map(function(key) { return data.favoriteBuilds[key]; });
 			$.Each(con.Children(), function(child) {
-				child.setFavorite(favoriteBuilds.indexOf(child.buildID) != -1);
-			})
-		})
+				child.setFavorite(favoriteBuilds.indexOf(child.buildID) !== -1);
+			});
+		});
 	}
 }
