bc542b34b7110765e1affa3eee1b3a46ff166f80
SwordBacon <swordofbacon@gmail.com>
2017-05-21 13:01:24 -0600

    attempt to fix armageddon

--- src/game/scripts/vscripts/abilities/mega_ultimates/armageddon.lua
+++ src/game/scripts/vscripts/abilities/mega_ultimates/armageddon.lua
@@ -1,9 +1,10 @@
+local Timers = require('easytimers')
+
 function StartArmageddon(keys)
 	local caster = keys.caster
 	local ability = keys.ability
 
-	caster.armageddon = CreateUnitByName("npc_armageddon", caster:GetAbsOrigin(), true, caster, nil, caster:GetTeam())
-	local armageddon_unit = caster.armageddon
+	local armageddon_unit = CreateUnitByName("npc_armageddon", caster:GetAbsOrigin(), true, caster, nil, caster:GetTeam())
 	armageddon_unit:SetControllableByPlayer(caster:GetPlayerOwnerID(),false)
 	armageddon_unit:SetOwner(caster)
 
@@ -18,10 +19,10 @@ function StartArmageddon(keys)
 	armageddon_unit:AddNewModifier(caster, nil, "modifier_kill", {Duration = duration})
 	armageddon_unit:AddNewModifier(caster,nil,"modifier_invulnerable", {})
 
-	Timers:CreateTimer(0, function()
+	Timers:CreateTimer(function()
 		time = time + interval
 		if time < duration then
-			local randomLocation = RandomVector(math.random(1500, 9000))
+			local randomLocation = RandomVector(RandomInt(1500, 9000))
 
 			armageddon_unit:SetAbsOrigin(randomLocation)
 			armageddon_unit:SetCursorPosition(randomLocation + RandomVector(1400))
@@ -30,7 +31,7 @@ function StartArmageddon(keys)
 		else 
 			return nil
 		end
-	end)
+	end, DoUniqueString('zulf_typhhoon'), interval)
 end 
 
 
