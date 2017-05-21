0d0a3a452b82cdbcc2696fcac6de693d15b17b54
TideSofDarK <tidesmain@gmail.com>
2017-05-21 22:49:59 +0500

    Green modifier + ignore wards + fix shared exp

--- src/game/scripts/vscripts/abilities/global_mutators/battle_thirst.lua
+++ src/game/scripts/vscripts/abilities/global_mutators/battle_thirst.lua
@@ -57,10 +57,10 @@ function modifier_battle_thirst_effect:OnIntervalThink(keys)
 		end
 		
 		for _,v in pairs(FindUnitsInRadius( parentTeam, parent:GetAbsOrigin(), nil, 2000.0, DOTA_UNIT_TARGET_TEAM_ENEMY, DOTA_UNIT_TARGET_ALL, DOTA_UNIT_TARGET_FLAG_NONE+DOTA_UNIT_TARGET_FLAG_INVULNERABLE+DOTA_UNIT_TARGET_FLAG_MAGIC_IMMUNE_ENEMIES, FIND_ANY_ORDER, false )) do
-			if IsValidEntity(v) and v:IsNull() == false and v.GetPlayerOwnerID and not v:IsClone() and not v:HasModifier("modifier_arc_warden_tempest_double") then
+			if IsValidEntity(v) and v:IsNull() == false and v.GetPlayerOwnerID and not v:IsClone() and not v:HasModifier("modifier_arc_warden_tempest_double") and not string.match(v:GetUnitName(), "ward") then
 		        if v:GetTeamNumber() == tonumber(enemyTeam) then
 		        	if v:CanEntityBeSeenByMyTeam(parent) then
-		        		parent:AddExperience(8,0,false,false)
+		        		parent:AddExperience(8,1,false,false)
 		        		parent:ModifyGold(4,false,0)
 
 		        		self:SetStackCount(0)
@@ -88,4 +88,8 @@ end
 ----------------------------------------------------------------------------------------------------------
 function modifier_battle_thirst_effect:GetTexture()
 	return "custom_games_xp_coin"
+end
+----------------------------------------------------------------------------------------------------------
+function modifier_battle_thirst_effect:IsDebuff()
+	return false
 end
