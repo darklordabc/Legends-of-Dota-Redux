c3f8ef2d501801940bea1cc235b7e503fe384fe1
Wouter <wouter.zegers@gmail.com>
2017-07-04 18:06:22 +0200

    Added MS ability

---
+++ src/game/scripts/vscripts/abilities/basic_movespeed_bonus.lua
@@ -0,0 +1,40 @@
+basic_movespeed_bonus=class({})
+basic_movespeed_bonus_op=class({})
+
+modifier_basic_movespeed_bonus = class({})
+modifier_basic_movespeed_bonus_op = class({})
+
+LinkLuaModifier("modifier_basic_movespeed_bonus","abilities/basic_movespeed_bonus.lua",LUA_MODIFIER_MOTION_NONE)
+LinkLuaModifier("modifier_basic_movespeed_bonus_op","abilities/basic_movespeed_bonus.lua",LUA_MODIFIER_MOTION_NONE)
+
+function basic_movespeed_bonus:GetIntrinsicModifierName()
+ return "modifier_basic_health_bonus"
+end
+
+function basic_movespeed_bonus_op:GetIntrinsicModifierName()
+ return "modifier_basic_health_bonus_op"
+end
+
+function modifier_basic_movespeed_bonus:IsPermanent() return true end
+function modifier_basic_movespeed_bonus:IsHidden() return true end
+
+function modifier_basic_movespeed_bonus:DeclareFunctions() 
+  return {MODIFIER_PROPERTY_MOVESPEED_BONUS_CONSTANT}
+end
+
+function modifier_basic_movespeed_bonus:GetModifierMoveSpeedBonus_Constant()
+  if self:GetCaster():PassivesDisabled() then return 0 end
+  return self:GetAbility():GetSpecialValueFor("speed_bonus")
+end
+
+function modifier_basic_movespeed_bonus_op:IsPermanent() return true end
+function modifier_basic_movespeed_bonus_op:IsHidden() return true end
+
+function modifier_basic_movespeed_bonus_op:DeclareFunctions() 
+  return {MODIFIER_PROPERTY_MOVESPEED_BONUS_CONSTANT}
+end
+
+function modifier_basic_movespeed_bonus_op:GetModifierMoveSpeedBonus_Constant()
+  if self:GetCaster():PassivesDisabled() then return 0 end
+  return self:GetAbility():GetSpecialValueFor("speed_bonus")
+end
