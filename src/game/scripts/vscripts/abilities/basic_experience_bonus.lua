c1b8d9d5d33fa18bed1e50d69ed211a349f38975
darklordabc <curtisjdubois@hotmail.com>
2017-07-04 23:22:01 +1000

    finished tweaking

--- src/game/scripts/vscripts/abilities/basic_experience_bonus.lua
+++ src/game/scripts/vscripts/abilities/basic_experience_bonus.lua
@@ -1,10 +1,18 @@
 basic_xpm_bonus=class({})
+basic_xpm_bonus_op=class({})
 modifier_basic_xpm_bonus = class({})
+modifier_basic_xpm_bonus_op = class({})
 LinkLuaModifier("modifier_basic_xpm_bonus","abilities/basic_experience_bonus.lua",LUA_MODIFIER_MOTION_NONE)
+LinkLuaModifier("modifier_basic_xpm_bonus_op","abilities/basic_experience_bonus.lua",LUA_MODIFIER_MOTION_NONE)
 
 function basic_xpm_bonus:OnUpgrade()
  self:GetCaster():RemoveModifierByName("modifier_basic_xpm_bonus")
  self:GetCaster():AddNewModifier(self:GetCaster(),self,"modifier_basic_xpm_bonus",{})
+end
+
+function basic_xpm_bonus_op:OnUpgrade()
+ self:GetCaster():RemoveModifierByName("modifier_basic_xpm_bonus_op")
+ self:GetCaster():AddNewModifier(self:GetCaster(),self,"modifier_basic_xpm_bonus_op",{})
 end
 
 function modifier_basic_xpm_bonus:IsPermanent() return true end
@@ -19,4 +27,18 @@ end
 function modifier_basic_xpm_bonus:OnIntervalThink()
   self:GetCaster():AddExperience(1,DOTA_ModifyXP_Unspecified,false,true)
 end
+
+function modifier_basic_xpm_bonus_op:IsPermanent() return true end
+function modifier_basic_xpm_bonus_op:IsHidden() return true end
+
+function modifier_basic_xpm_bonus_op:OnCreated() 
+  if IsServer() then
+    self:StartIntervalThink(60/self:GetAbility():GetSpecialValueFor("experience_bonus"))
+  end
+end
+
+function modifier_basic_xpm_bonus_op:OnIntervalThink()
+  self:GetCaster():AddExperience(1,DOTA_ModifyXP_Unspecified,false,true)
+end
+
 
