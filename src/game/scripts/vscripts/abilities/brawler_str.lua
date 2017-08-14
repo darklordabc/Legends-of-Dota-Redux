60dc31a2ebbe0c066ddc0bf6629b5db734e59198
01der <01der@f.oib.com>
2017-08-15 01:38:31 +0200

    Update brawler_str.lua

--- src/game/scripts/vscripts/abilities/brawler_str.lua
+++ src/game/scripts/vscripts/abilities/brawler_str.lua
@@ -4,8 +4,7 @@
 	Check number of units every interval
 	Note: Might be possible to do entirely in datadriven, however, I seem to crash everytime I tried
 	to do so, insteads, I just use simple script
-  
-  Reversed by 01der
+
 ]]
 function brawler_detection( keys )
 	local caster = keys.caster
