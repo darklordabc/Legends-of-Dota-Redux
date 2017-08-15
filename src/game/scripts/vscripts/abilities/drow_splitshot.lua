b6766e4dde55c905e4edc582f0deb812c8fae116
DankBudd <evilsword54@yahoo.com>
2017-08-15 08:28:14 -0400

    Add files via upload

---
+++ src/game/scripts/vscripts/abilities/drow_splitshot.lua
@@ -0,0 +1,110 @@
+LinkLuaModifier("modifier_drow_splitshot", "abilities/drow_splitshot.lua", LUA_MODIFIER_MOTION_NONE)
+
+
+drow_splitshot = class({})
+
+function drow_splitshot:GetIntrinsicModifierName()
+	return "modifier_drow_splitshot"
+end
+
+function drow_splitshot:OnProjectileHit( hTarget, vLocation )
+	if not IsServer() or not hTarget or hTarget:IsNull() then return end
+
+	local bUseCastAttackOrb = true
+	local bProcessProcs = true
+	local bSkipCooldown = false
+	local bIgnoreInvis = false
+	local bUseProjectile = false
+	local bFakeAttack = false
+	local bNeverMiss = false
+
+	self.mod.reduceAttackDamage = true
+
+		PerformAttack(hTarget, bUseCastAttackOrb, bProcessProcs, bSkipCooldown, bIgnoreInvis, bUseProjectile, bFakeAttack, bNeverMiss)
+	
+	self.mod.reduceAttackDamage = false
+end
+
+
+modifier_drow_splitshot = class({
+	IsHidden = function(self) return true end,
+	IsPurgable = function(self) return false end,
+	IsPermanent = function(self) return true end,
+	RemoveOnDeath = function(self) return false end,
+	GetAttributes = function(self) return MODIFIER_ATTRIBUTE_IGNORE_INVULNERABLE+MODIFIER_ATTRIBUTE_PERMANENT end,
+	DeclareFunctions = function(self) return {MODIFIER_EVENT_ON_ATTACK_LANDED, MODIFIER_PROPERTY_BASEDAMAGEOUTGOING_PERCENTAGE,} end,
+})
+
+function modifier_drow_splitshot:OnCreated( kv )
+
+	--i believe drow ult actually works with illusions, so i guess this works with illusions?
+	--if self:GetParent():IsIllusion() then self:Destroy() return end
+
+	self:GetAbility().mod = self
+
+	self.splitCount = self:GetAbility():GetSpecialValueFor("split_count")
+	self.searchRadius = self:GetAbility():GetSpecialValueFor("search_radius")
+	self.damageReduction = self:GetAbility():GetSpecialValueFor("damage_reduction") * 0.01
+
+	self.projSpeed = self:GetParent():GetProjectileSpeed()
+	self.projectile = self:GetParent():GetRangedProjectileName()
+
+	print("DROW SHIT: "..tostring(self.projectile), self.projSpeed)
+
+	-- for melee heroes
+	if not self.projectile then
+		self.projectile = "particles/units/heroes/hero_drow/drow_base_attack.vpcf"
+	end
+	if self.projSpeed <= 0 then
+		self.projSpeed = 1250
+	end
+end
+
+function modifier_drow_splitshot:OnAttackLanded( keys )
+	if IsServer() and keys.attacker == self:GetParent() then
+
+		--According to the wiki, "Splintering arrows are not disabled [by break] and fully work."
+		--prevent infinite loop, break check
+		if self.reduceAttackDamage --[[or self:GetParent():PassivesDisabled()]] then return end
+
+		if not self:GetAbility():IsFullyCastable() then return end
+
+		local units = FindUnitsInRadius(self:GetParent():GetTeamNumber(), keys.victim:GetAbsOrigin(), nil, self.searchRadius, self:GetAbility():GetAbilityTargetTeam(), self:GetAbility():GetAbilityTargetType(), self:GetAbility():GetAbilityTargetFlags(), FIND_ANY_ORDER, false)
+		if #units <= 1 then return end
+
+		--dont fire projectiles at the primary target
+		local pos = vlua.find(units, keys.victim)
+		if pos then
+			table.remove(units, pos)
+		end	
+
+		local info = {
+			EffectName = self.projectile,
+		--	Target = nil,
+			Source = keys.victim,
+			Ability = self:GetAbility(),
+			bDodgeable = true,
+			iMoveSpeed = self.projSpeed,
+			iSourceAttachment = DOTA_PROJECTILE_ATTACHMENT_ATTACK_1,
+			bProvidesVision = false,
+		--	iVisionRadius = 0,
+		--	iVisionTeamNumber = self:GetParent():GetTeamNumber(),
+		}
+
+		for i=1,self.splitCount do
+			if units[i] then
+				info.Target = units[i]
+				ProjectileManager:CreateTrackingProjectile(info)
+			end
+		end
+
+		self:GetAbility():UseResources(true, false, true)
+	end
+end
+
+--not sure if this is the correct modifier property for this
+function modifier_drow_splitshot:GetModifierBaseDamageOutgoing_Percentage()
+	if IsServer() and self.reduceAttackDamage then
+		return self.damageReduction
+	end
+end
