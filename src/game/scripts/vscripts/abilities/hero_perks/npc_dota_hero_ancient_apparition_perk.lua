2fd4ac960d7d85e84cb7536c7659f0b38d368ffc
DankBudd <evilsword54@yahoo.com>
2017-08-17 21:31:26 -0400

    Update npc_dota_hero_ancient_apparition_perk.lua

--- src/game/scripts/vscripts/abilities/hero_perks/npc_dota_hero_ancient_apparition_perk.lua
+++ src/game/scripts/vscripts/abilities/hero_perks/npc_dota_hero_ancient_apparition_perk.lua
@@ -73,6 +73,7 @@ function perkAncientApparition(filterTable)
   if ability then
     if caster:HasModifier("modifier_npc_dota_hero_ancient_apparition_perk") then
       if ability:HasAbilityFlag("ice") then
+	Timers:CreateTimer(function() trackModifier(filterTable) end)
         local modifierDuration = filterTable["duration"]
 	if modifierDuration == -1 then
 	  modifierDuration = 3
@@ -83,4 +84,44 @@ function perkAncientApparition(filterTable)
       end
     end  
   end
+end
+
+function trackModifier( filterTable )
+  local parentIndex = filterTable["entindex_parent_const"]
+  local casterIndex = filterTable["entindex_caster_const"]
+  if not parentIndex or not casterIndex then
+    return
+  end
+  local parent = EntIndexToHScript( parentIndex )
+  local caster = EntIndexToHScript( casterIndex )
+  local modifierName = filterTable["name_const"]
+  local duration = filterTable["duration"]
+
+  print("start tracking on: "..modifierName)
+  print("for duration: "..tostring(duration))
+
+  --track modifier
+  Timers:CreateTimer(0.1, function()
+  local modifier = parent:FindModifierByNameAndCaster(modifierName, caster)
+  if not modifier or modifier:IsNull() then print("abort tracking") return end
+    local dur = modifier:GetDuration()
+    local remaining = modifier:GetRemainingTime()
+    local elapsed = modifier:GetElapsedTime()
+
+    modifier.prevElapsed = modifier.prevElapsed or elapsed
+    if modifier.prevElapsed > elapsed then
+      print("modifier refreshed")
+      -- call any functions that need to interact with modifiers on refresh here
+      parent:AddNewModifier(caster,nil,"modifier_npc_dota_hero_ancient_apparition_perk_heal_freeze",{duration = modifierDuration})
+    end
+
+    print("remaining: "..tostring(remaining), "elapsed: "..tostring(elapsed))
+
+    if elapsed >= dur then
+      print("end tracking on: "..modifierName)
+      print()
+      return
+    end
+    return 0.1
+  end)
 end
