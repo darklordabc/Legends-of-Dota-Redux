e83fb741670f5db2a0fc7b51612a0bfd3d4b12d5
DankBudd <evilsword54@yahoo.com>
2017-08-23 10:26:51 -0400

    Update cooldown.lua

--- src/game/scripts/vscripts/abilities/spell_lab/survivor/cooldown.lua
+++ src/game/scripts/vscripts/abilities/spell_lab/survivor/cooldown.lua
@@ -13,13 +13,13 @@ end
 
 function spell_lab_survivor_cooldown_modifier:DeclareFunctions()
 	local funcs = {
-		MODIFIER_PROPERTY_COOLDOWN_PERCENTAGE_STACKING,
+		MODIFIER_PROPERTY_COOLDOWN_PERCENTAGE,
     MODIFIER_EVENT_ON_DEATH
 	}
 	return funcs
 end
 
-function spell_lab_survivor_cooldown_modifier:GetModifierPercentageCooldownStacking()
+function spell_lab_survivor_cooldown_modifier:GetModifierPercentageCooldown()
 if self:GetParent():PassivesDisabled() then return 0 end
 return self:GetStackCount()
 end
