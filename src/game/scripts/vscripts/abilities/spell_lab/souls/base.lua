6a1d220c2235f31edebd8815097255decc11e618
Otso Turpeinen <otso.ilmari.turpeinen@gmail.com>
2018-02-22 20:46:12 +0200

    -Reduced time from 5 mins to 3

--- src/game/scripts/vscripts/abilities/spell_lab/souls/base.lua
+++ src/game/scripts/vscripts/abilities/spell_lab/souls/base.lua
@@ -121,7 +121,7 @@ end
 function spell_lab_souls_base_modifier:OnIntervalThink()
 	if IsServer() then
 		self.fTime = self.fTime + self.fInterval
-		if (self.fTime > 300) then
+		if (self.fTime > 180) then
 			if (not self.bPickedup) then
 				ParticleManager:DestroyParticle(self.nFXIndex,false)
 			end
