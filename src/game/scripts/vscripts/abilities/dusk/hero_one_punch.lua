2d4eaa9c8755607383ae42794033b0588f94f92a
DankBudd <evilsword54@yahoo.com>
2018-03-25 03:49:47 -0400

    Update hero_one_punch.lua

--- src/game/scripts/vscripts/abilities/dusk/hero_one_punch.lua
+++ src/game/scripts/vscripts/abilities/dusk/hero_one_punch.lua
@@ -163,3 +163,17 @@ function InflictDamage(target,attacker,ability,damage,damage_type,flags)
 	    ability = ability
   	})
 end
+
+function FindEnemies(caster,point,radius,targets,flags)
+  local targets = targets or DOTA_UNIT_TARGET_HERO+DOTA_UNIT_TARGET_CREEP
+  local flags = flags or DOTA_UNIT_TARGET_FLAG_NONE
+  return FindUnitsInRadius( caster:GetTeamNumber(),
+                            point,
+                            nil,
+                            radius,
+                            DOTA_UNIT_TARGET_TEAM_ENEMY,
+                            targets,
+                            flags,
+                            FIND_CLOSEST,
+                            false)
+end
