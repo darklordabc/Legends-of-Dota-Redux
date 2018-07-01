82d9f1b603c10c5b978bad2eecedf65e3ab51248
darklordabc <curtisjdubois@hotmail.com>
2018-07-01 22:49:01 +1000

    disabled leveling up heroes when giving them bonus ability items

--- src/game/scripts/vscripts/pregame.lua
+++ src/game/scripts/vscripts/pregame.lua
@@ -7810,11 +7810,9 @@ function Pregame:giveAbilityUsageBonuses(playerID)
         if hero then
             if newAbilities > 0 then
                 hero:AddItemByName('item_new_ability_bonus'):SetCurrentCharges(newAbilities)
-                hero:HeroLevelUp(true)
             end
             if newGlobalAbilities > 0 then
                 hero:AddItemByName('item_new_global_ability_bonus'):SetCurrentCharges(newGlobalAbilities)
-                hero:HeroLevelUp(true)
             end
             if pregame.optionStore["lodOptionBalancedBuildBonusGold"] > 0 and passiveAbilities <= 3 then
                 hero:AddItemByName('item_balanced_build_bonus')
