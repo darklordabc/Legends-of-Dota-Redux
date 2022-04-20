49ef1bba141390cc5adac10cf63ee612190dc93d
scarredwithcrayons <56701186+scarredwithcrayons@users.noreply.github.com>
2022-04-20 20:47:22 +0100

    various
    fixed concussive break spell effect
    fixed talent issues for omniknight
    fixed talent issues for ogre magi
    fixed talent issues for nyx
    fixed talent issues for bloodseeker
    fixed talent issues for clinkz
    fixed talent issues for clockwerk
    fixed talent issues for juggernaut
    fixed talent issues for legion commander
    fixed talent issues for medusa
    fixed talent issues for riki
    fixed talent issues for slark
    fixed talent issues for silencer
    fixed talent issues for snapfire
    fixed talent issues for terrorblade
    fixed talent issues for troll warlord
    fixed talent issues for sven
    fixed talent issues for windrunner
    returned forest_troll_high_priest_heal to valve version
    returned tusk_walrus_kick to valve version
    added bloodseeker_blood_mist
    added skywrath_mage_shield_of_the_scion
    enabled treant_eyes_in_the_forest

--- src/game/scripts/vscripts/abilities/pudge_flesh_heap_cooldown_reduction.lua
+++ src/game/scripts/vscripts/abilities/pudge_flesh_heap_cooldown_reduction.lua
@@ -94,13 +94,13 @@ end
 function modifier_flesh_heap_cooldown_reduction:OnCreated( kv )
   if not self:GetAbility() then
     self:GetParent():RemoveModifierByName("modifier_flesh_heap_cooldown_reduction")
-    self:GetParent():CalculateStatBonus()
+    self:GetParent():CalculateStatBonus(true)
     return
   end
   self.flesh_heap_value_buff_amount = self:GetAbility():GetSpecialValueFor( "flesh_heap_value_buff_amount" ) or 0
   if IsServer() then
     self:SetStackCount( self:GetFleshHeapKills() )
-    self:GetParent():CalculateStatBonus()
+    self:GetParent():CalculateStatBonus(true)
   end
 end
 
@@ -109,13 +109,13 @@ end
 function modifier_flesh_heap_cooldown_reduction:OnRefresh( kv )
   if not self:GetAbility() then
     self:GetParent():RemoveModifierByName("modifier_flesh_heap_cooldown_reduction")
-    self:GetParent():CalculateStatBonus()
+    self:GetParent():CalculateStatBonus(true)
     return
   end
   self.flesh_heap_value_buff_amount = self:GetAbility():GetSpecialValueFor( "flesh_heap_value_buff_amount" ) or 0
   if IsServer() then
     self:SetStackCount( self:GetFleshHeapKills() )
-    self:GetParent():CalculateStatBonus()
+    self:GetParent():CalculateStatBonus(true)
   end
 end
 
@@ -169,7 +169,7 @@ function modifier_flesh_heap_cooldown_reduction:OnDeath(keys)
       local hBuff = self:GetCaster():FindModifierByName( "modifier_flesh_heap_cooldown_reduction" )
       if hBuff ~= nil then
         hBuff:SetStackCount( self.nKills )
-        self:GetCaster():CalculateStatBonus()
+        self:GetCaster():CalculateStatBonus(true)
       else
         self:GetCaster():AddNewModifier( self:GetCaster(), self,  "modifier_flesh_heap_cooldown_reduction" , {} )
       end
