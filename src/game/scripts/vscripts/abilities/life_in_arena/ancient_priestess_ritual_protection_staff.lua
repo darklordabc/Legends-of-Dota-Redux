600e0ada8400f7f107256573d2460ac943af3627
asuminichie <104118145+asuminichie@users.noreply.github.com>
2023-01-04 14:55:53 +0800

    Update ancient_priestess_ritual_protection_staff.lua

--- src/game/scripts/vscripts/abilities/life_in_arena/ancient_priestess_ritual_protection_staff.lua
+++ src/game/scripts/vscripts/abilities/life_in_arena/ancient_priestess_ritual_protection_staff.lua
@@ -11,7 +11,7 @@ function ancient_priestess_ritual_protection_staff:OnSpellStart()
 
 	for _,unit in pairs(targets) do 
 		unit:AddNewModifier(caster, self, "modifier_ancient_priestess_ritual_protection", {duration = duration})
-		ParticleManager:CreateParticle("particles/mirana_moonlight_ray_custom.vpcf", PATTACH_ABSORIGIN_FOLLOW, unit)
+		ParticleManager:CreateParticle("particles/items_fx/wand_of_the_brine_buff.vpcf", PATTACH_ABSORIGIN_FOLLOW, unit)
 	end
 
 	EmitSoundOn("Ability.MoonlightShadow", caster)
