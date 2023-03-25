de0eb1eab37b86e42e0c7270c16e38d979e8eadc
asuminichie <104118145+asuminichie@users.noreply.github.com>
2023-03-25 12:20:16 +0800

    Update modifier_dark_blade.lua

--- src/game/scripts/vscripts/abilities/overflow/dark_blade/modifier_dark_blade.lua
+++ src/game/scripts/vscripts/abilities/overflow/dark_blade/modifier_dark_blade.lua
@@ -88,7 +88,7 @@ function modifier_dark_blade:OnDestroy()
 				EmitSoundOn( "Hero_Nightstalker.Void.Nihility", hTarget )
 			--end
 	
-			local nFXIndex = ParticleManager:CreateParticle( "particles/econ/items/dazzle/dazzle_ti9/dazzle_shadow_wave_ti9.vpcf", PATTACH_CUSTOMORIGIN, nil );
+			local nFXIndex = ParticleManager:CreateParticle( "particles/black_laguna.vpcf", PATTACH_CUSTOMORIGIN, nil );
 			ParticleManager:SetParticleControlEnt( nFXIndex, 0, self:GetParent(), PATTACH_POINT_FOLLOW, "attach_hitloc", self:GetParent():GetOrigin() + Vector( 0, 0, 96 ), true );
 			ParticleManager:SetParticleControlEnt( nFXIndex, 1, hTarget, PATTACH_POINT_FOLLOW, "attach_hitloc", hTarget:GetOrigin(), true );
 			local Colour = {150,50,150}
